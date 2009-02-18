9cebafb371f5889691df59dd419e47ab92d13134
cdumoulin <cdumoulin@d68fad7f-2b57-0410-97a2-f40a37629c47>
2009-02-18 16:00:38 +0000

    Move the editor declaration in its own plugin. This plugin can depend on GMF. The core plugin should not.

---
+++ plugins/core/org.eclipse.papyrus.editor/src/org/eclipse/papyrus/editor/PapyrusMultiEditor.java
@@ -0,0 +1,28 @@
+/*****************************************************************************
+ * Copyright (c) 2008 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Cedric Dumoulin  Cedric.dumoulin@lifl.fr - Initial API and implementation
+ *
+  *****************************************************************************/
+
+package org.eclipse.papyrus.editor;
+
+import org.eclipse.papyrus.core.editor.SashMultiDiagramEditor;
+
+
+/**
+ * Papyrus main MultiEditor.
+ * This class add GMF adaptation dedicated to Papyrus. 
+ * @author dumoulin
+ *
+ */
+public class PapyrusMultiEditor extends SashMultiDiagramEditor {
+
+}
