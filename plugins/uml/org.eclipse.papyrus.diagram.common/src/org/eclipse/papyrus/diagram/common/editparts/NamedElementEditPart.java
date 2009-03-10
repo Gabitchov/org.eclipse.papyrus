06566fbd9846e453edbc820871f5603c2eacf3e5
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2009-03-10 11:14:47 +0000

    RESOLVED - bug 267824: Adding abstract Editparts for stereotypes and qualified Management management https://bugs.eclipse.org/bugs/show_bug.cgi?id=267824

---
+++ plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/editparts/NamedElementEditPart.java
@@ -0,0 +1,73 @@
+/*****************************************************************************
+ * Copyright (c) 2008 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.diagram.common.editparts;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EMap;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.papyrus.diagram.common.figure.node.NodeNamedElementFigure;
+import org.eclipse.papyrus.editor.PapyrusConstant;
+import org.eclipse.uml2.uml.NamedElement;
+
+/**
+ * this editpart manage the display of qualifiedName
+ * 
+ */
+public abstract class NamedElementEditPart extends UmlNodeEditPart {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public NamedElementEditPart(View view) {
+		super(view);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * get the depth to display for the qualified name from a eannotation in the view
+	 * 
+	 * @return the depth
+	 */
+	public int getQualifiedNamedepth() {
+		EAnnotation stereotypeDisplayKind = ((View) getModel()).getEAnnotation(PapyrusConstant.QUALIFIED_NAME);
+		if (stereotypeDisplayKind != null) {
+			EMap<String, String> entries = stereotypeDisplayKind.getDetails();
+
+			if (entries != null) {
+				String depthString = entries.get(PapyrusConstant.QUALIFIED_NAME_DEPTH);
+				if (!depthString.isEmpty()) {
+					Integer i = new Integer(depthString);
+					return i.intValue();
+				}
+			}
+		}
+		return 0;
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	protected void handleNotificationEvent(Notification event) {
+		super.handleNotificationEvent(event);
+
+		// set the figure active when the feature of the of a class is true
+		if (resolveSemanticElement() != null) {
+			((NodeNamedElementFigure) getPrimaryShape()).setDepth(getQualifiedNamedepth());
+			((NodeNamedElementFigure) getPrimaryShape()).setQualifiedName(((NamedElement) resolveSemanticElement()).getQualifiedName());
+			refreshVisuals();
+		}
+	}
+}
---
+++ plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/editparts/UmlNodeEditPart.java
@@ -0,0 +1,110 @@
+/*****************************************************************************
+ * Copyright (c) 2008 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.diagram.common.editparts;
+
+import java.util.StringTokenizer;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EMap;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.papyrus.diagram.common.figure.node.NodeNamedElementFigure;
+import org.eclipse.papyrus.editor.Activator;
+import org.eclipse.papyrus.editor.PapyrusConstant;
+
+public abstract class UmlNodeEditPart extends org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeNodeEditPart {
+
+	public UmlNodeEditPart(View view) {
+		super(view);
+	}
+
+	public abstract NodeNamedElementFigure getPrimaryShape();
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 */
+	protected void handleNotificationEvent(Notification event) {
+		super.handleNotificationEvent(event);
+
+		// set the figure active when the feature of the of a class is true
+		if (resolveSemanticElement() != null) {
+			if (stereotypesToDisplay() != "") {
+				((NodeNamedElementFigure) getPrimaryShape()).setStereotypes(stereotypesToDisplay());
+			}
+			refreshVisuals();
+		}
+	}
+
+	/**
+	 * get the list of stereotype to display from the eannotation
+	 * 
+	 * @return the list of stereotypes to display
+	 */
+	public String stereotypesToDisplay() {
+		EAnnotation stereotypeDisplayKind = ((View) getModel()).getEAnnotation(PapyrusConstant.STEREOTYPE_ANNOTATION);
+		if (stereotypeDisplayKind != null) {
+			EMap<String, String> entries = stereotypeDisplayKind.getDetails();
+
+			String stereotypesToDisplay = entries.get(PapyrusConstant.STEREOTYPE_LIST);
+			String stereotypespresentationKind = entries.get(PapyrusConstant.STEREOTYPE_PRESENTATION_KIND);
+			if (PapyrusConstant.STEREOTYPE_TEXT_VERTICAL_PRESENTATION.equals(stereotypespresentationKind)) {
+				return stereotypesToDisplay("\n", stereotypesToDisplay);
+			} else {
+				return stereotypesToDisplay(" ", stereotypesToDisplay);
+
+			}
+		}
+		return "";
+	}
+
+	public String stereotypesToDisplay(String separator, String stereotypesToDisplay) {
+
+		// AL Changes Feb. 07 - Beg
+		// Style Handling for STEREOTYPE_NAME_APPEARANCE from ProfileApplicationPreferencePage
+		// Stereotype displayed according to UML standard (first letter forced to lower case) - default -
+		// or kept as entered by user (user controlled)
+
+		// Get the preference from PreferenceStore
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		String sNameAppearance = store.getString(PapyrusConstant.P_STEREOTYPE_NAME_APPEARANCE);
+
+		StringTokenizer strqualifiedName = new StringTokenizer(stereotypesToDisplay, ",");
+		String out = "";
+		while (strqualifiedName.hasMoreElements()) {
+
+			// property value contains qualifiedName ==> extract name from it
+			StringTokenizer strToken = new StringTokenizer(strqualifiedName.nextToken(), "::");
+			String name = "";
+			while (strToken.hasMoreTokens()) {
+				name = strToken.nextToken();
+			}
+			// AL Changes Feb. 07 - Beg
+			// Handling STEREOTYPE_NAME_APPEARANCE preference (from ProfileApplicationPreferencePage)
+			// Previously lowercase forced onto first letter (standard UML)
+			// stereotypesToDisplay = stereotypesToDisplay+name.substring(0, 1).toLowerCase()+name.substring(1, name.length())+","+separator;
+
+			if (sNameAppearance.equals(PapyrusConstant.P_STEREOTYPE_NAME_DISPLAY_USER_CONTROLLED)) {
+				out = out + name + "," + separator;
+			} else if (sNameAppearance.equals(PapyrusConstant.P_STEREOTYPE_NAME_DISPLAY_UML_CONFORM)) {
+				out = out + name.substring(0, 1).toLowerCase() + name.substring(1, name.length()) + "," + separator;
+			} else { // should not happen since radio button are used to set choice
+				out = out + name.substring(0, 1).toLowerCase() + name.substring(1, name.length()) + "," + separator;
+			}
+		}
+		return stereotypesToDisplay;
+	}
+
+}
