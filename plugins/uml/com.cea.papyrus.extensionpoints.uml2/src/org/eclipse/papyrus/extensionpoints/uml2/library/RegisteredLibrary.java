754c30e9d0962739b8037b50b3cbccbb734dd0aa
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2009-03-27 11:07:40 +0000

    share project "com.cea.papyrus.extensionpoints.uml2" dans "svn+ssh://dev.eclipse.org/svnroot/modeling/org.eclipse.mdt.papyrus"

---
+++ plugins/uml/com.cea.papyrus.extensionpoints.uml2/src/org/eclipse/papyrus/extensionpoints/uml2/library/RegisteredLibrary.java
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ * Copyright (c) 2006 CEA List.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     CEA List - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.papyrus.extensionpoints.uml2.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.papyrus.extensionpoints.uml2.Activator;
+import org.eclipse.papyrus.extensionpoints.uml2.standard.ExtensionIds;
+import org.eclipse.papyrus.extensionpoints.uml2.standard.RegisteredElementExtensionPoint;
+
+
+/**
+ * Class that manages registered library extension point 
+ */
+public class RegisteredLibrary extends RegisteredElementExtensionPoint{
+	
+	/** name of the extension point (main element name) in the DTD */
+	private static final String TAG_LIBRARY = "library";
+	
+	/** attribute that acts as a cache for  the list of registered libraries */
+	private static RegisteredLibrary[] RegisteredLibraries;
+	
+	/**
+	 * Main constructor.
+	 * @param configElt the configuration element that defines the given library
+	 * @param ordinal the place in the list of registered libraries 
+	 */
+	public RegisteredLibrary(IConfigurationElement configElt, int ordinal) {
+		super(configElt, ordinal);
+	}
+	
+	/**
+	 * Returns the list of registered libraries in the platform, using the 
+	 * Papyrus extension point.
+	 * @return the list of registered libraries in the platform
+	 */
+	public static RegisteredLibrary[] getRegisteredLibraries() {
+		
+		// if the list was already computed, returns it (like a cache)
+		if (RegisteredLibraries != null) {
+			return RegisteredLibraries;
+		}
+		
+		// the list of libraries was not already computed. Read configuration elements
+		List<RegisteredLibrary> libraries = new ArrayList<RegisteredLibrary>();
+		
+//		// Default elements : UML, Ecore, Java, XML PrimitiveTypes model library
+//		RegisteredLibrary UmlPrimitiveTypes
+//				= new RegisteredLibrary("UMLPrimitiveTypes", URI.createURI(UMLResource.UML_PRIMITIVE_TYPES_LIBRARY_URI));
+//		libraries.add(UmlPrimitiveTypes);
+//		RegisteredLibrary EcorePrimitiveTypes
+//				= new RegisteredLibrary("EcorePrimitiveTypes", URI.createURI(UMLResource.ECORE_PRIMITIVE_TYPES_LIBRARY_URI));
+//		libraries.add(EcorePrimitiveTypes);
+//		RegisteredLibrary JavaPrimitiveTypes
+//				= new RegisteredLibrary("JavaPrimitiveTypes", URI.createURI(UMLResource.JAVA_PRIMITIVE_TYPES_LIBRARY_URI));
+//		libraries.add(JavaPrimitiveTypes);
+//		RegisteredLibrary XMLPrimitiveTypes
+//				= new RegisteredLibrary("XMLPrimitiveTypes", URI.createURI(UMLResource.XML_PRIMITIVE_TYPES_LIBRARY_URI));
+//		libraries.add(XMLPrimitiveTypes);	
+		
+		// Look for the rest in plugins
+		IConfigurationElement[] configElements = Platform.getExtensionRegistry().getConfigurationElementsFor(ExtensionIds.LIBRARY_EXTENSION_ID);
+		
+		// Read configuration elements for the current extension
+		for (int j = 0 ; j < configElements.length ; j++) {
+			RegisteredLibrary proxy = parseLibraryExtension(configElements[j], libraries.size());
+
+			if (proxy != null) {
+				libraries.add(proxy);
+			}
+		} // end of configElements loop	
+
+		RegisteredLibraries = libraries.toArray(new RegisteredLibrary[libraries.size()]);
+		return RegisteredLibraries;
+	}
+	
+	/**
+	 * Parse data from extensions.
+	 * @param ordinal 
+	 * @param configElt 
+	 * @return 
+	 */
+	private static RegisteredLibrary parseLibraryExtension(IConfigurationElement configElt, int ordinal) {
+		if (!TAG_LIBRARY.equals(configElt.getName())) {
+			return null;
+		}
+		try {
+			return new RegisteredLibrary(configElt, ordinal);
+		} catch (Exception e) {
+			
+			String name = configElt.getAttribute(ExtensionIds.ATT_NAME);
+			if (name == null) {
+				name = "[missing name attribute]";
+			}
+			String msg = "Failed to load library named "
+							+ name + " in "
+							+ configElt
+								.getDeclaringExtension()
+								.getExtensionPointUniqueIdentifier();
+			Activator.log(msg);
+			return null;
+		}
+	}
+}
---
+++ plugins/uml/com.cea.papyrus.extensionpoints.uml2/src/org/eclipse/papyrus/extensionpoints/uml2/library/RegisteredProfileAsLibrarySelectionDialog.java
@@ -0,0 +1,201 @@
+/*******************************************************************************
+ * Copyright (c) 2006 CEA List.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     CEA List - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.papyrus.extensionpoints.uml2.library;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.papyrus.extensionpoints.uml2.Activator;
+import org.eclipse.papyrus.extensionpoints.uml2.profile.RegisteredProfile;
+import org.eclipse.papyrus.extensionpoints.uml2.standard.ExtensionLabelProvider;
+import org.eclipse.papyrus.extensionpoints.uml2.utils.Util;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.PackageImport;
+import org.eclipse.uml2.uml.UMLFactory;
+
+
+/**
+ * 
+ */
+public class RegisteredProfileAsLibrarySelectionDialog extends ElementListSelectionDialog {
+
+	/**
+	 * 
+	 */
+	private EList importedProfiles;
+	
+	/**
+	 * 
+	 */
+	private List<String> importedProfilesNames;
+	
+	/**
+	 * 
+	 */
+	private RegisteredProfile[] regProfiles;
+	
+	/**
+	 * 
+	 */
+	private Package currentPackage;
+	
+	
+	/**
+	 * 
+	 * 
+	 * @param umlPackage 
+	 * @param parent 
+	 */
+	public RegisteredProfileAsLibrarySelectionDialog(Composite parent, Package umlPackage) {
+		super(parent.getShell(), new ExtensionLabelProvider());
+		
+		currentPackage			= umlPackage;
+		importedProfiles		= umlPackage.getPackageImports();
+		importedProfilesNames	= getImportedProfileNames(importedProfiles);
+		
+		// Retrieve registered profiles
+		regProfiles				= RegisteredProfile.getRegisteredProfiles();
+		// remove already applied profiles from the list
+		regProfiles				= removeAlreadyImportedFromSelection();
+		
+		this.setTitle("Apply profiles from Papyrus repository :");
+		this.setElements(regProfiles);
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @return 
+	 */
+	public boolean run() {
+		this.open();
+		return this.treatSelection();
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param appliedProfiles 
+	 * 
+	 * @return 
+	 */
+	private List<String> getImportedProfileNames(EList appliedProfiles) {
+		
+		List<String> Libraries = new ArrayList<String>();
+		Iterator importedIt =  importedProfiles.iterator();
+		
+		while(importedIt.hasNext()) {
+			org.eclipse.uml2.uml.PackageImport currentImport
+								= (org.eclipse.uml2.uml.PackageImport) importedIt.next();
+			String currentName	= currentImport.getImportedPackage().getName();
+			Libraries.add(currentName);
+		}
+		
+		return Libraries;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @return 
+	 */
+	private RegisteredProfile[] removeAlreadyImportedFromSelection() {
+		
+		List<RegisteredProfile> profiles = new ArrayList<RegisteredProfile>();		
+		
+		for (int i = 0 ; i < regProfiles.length ; i++) {
+			
+			String currentName = regProfiles[i].name;
+			if (!importedProfilesNames.contains(currentName)) {
+				profiles.add(regProfiles[i]);
+			}
+		}
+		
+		RegisteredProfile[] cleandList;
+		cleandList = profiles.toArray(new RegisteredProfile[profiles.size()]);
+		
+		return cleandList;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @return 
+	 */
+	private boolean treatSelection() {
+		
+		// User selection
+		Object[] selection		= this.getResult();
+		boolean hasChanged      = false;
+		
+		if (selection == null) { // Cancel was selected
+			return hasChanged;
+		}
+		
+		for (int i = 0 ; i < selection.length ; i++) {
+			
+			RegisteredProfile currentProfile	= (RegisteredProfile) (selection[i]);			
+			URI modelUri						= currentProfile.uri;
+				
+			PackageImport pi = getModelLibraryImportFromURI(modelUri);
+			
+			if (pi != null) {
+				currentPackage.getPackageImports().add(pi);
+			}
+		}
+		return hasChanged;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param uri 
+	 * 
+	 * @return 
+	 */
+	public PackageImport getModelLibraryImportFromURI(URI uri) {
+		
+		ResourceSet resourceSet	= Util.getResourceSet(currentPackage);
+		Resource modelResource	= resourceSet.getResource(uri,true);
+		
+		if (modelResource.getContents().size() <= 0) {
+			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "No element found in model from URI "+uri.toString()));
+			return null;
+		}
+		
+		// Try to reach model
+		Element root = (Element) modelResource.getContents().get(0);
+		
+		if (root instanceof Package) {
+			
+			// Import model library
+			Package libToImport = (Package) (modelResource.getContents().get(0));
+			// create import package
+			PackageImport modelLibImport = UMLFactory.eINSTANCE.createPackageImport();
+			modelLibImport.setImportedPackage(libToImport);
+			
+			return modelLibImport;
+		}
+		Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "The selected uri ("+ uri.toString() +") does not contain any model library !"));
+		return null;
+	}
+}
+
