a1a5c007fd22a86ceb8e213e8bc3906e5fe3a949
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2009-07-08 10:16:10 +0000

    fix problem about displaying end of associations

--- plugins/uml/org.eclipse.papyrus.diagram.clazz/src/org/eclipse/papyrus/diagram/clazz/edit/parts/AssociationEditPart.java
+++ plugins/uml/org.eclipse.papyrus.diagram.clazz/src/org/eclipse/papyrus/diagram/clazz/edit/parts/AssociationEditPart.java
@@ -17,6 +17,7 @@ import org.eclipse.draw2d.Connection;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ITreeBranchEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.notation.View;
@@ -31,8 +32,7 @@ import org.eclipse.uml2.uml.Property;
 /**
  * @generated
  */
-public class AssociationEditPart extends UMLConnectionNodeEditPart implements
-		ITreeBranchEditPart {
+public class AssociationEditPart extends UMLConnectionNodeEditPart implements ITreeBranchEditPart {
 
 	/**
 	 * @generated
@@ -58,10 +58,8 @@ public class AssociationEditPart extends UMLConnectionNodeEditPart implements
 	 * @generated NOT
 	 */
 	protected void addAssociationEndListeners() {
-		EObject sourceEnd = ((Association) resolveSemanticElement())
-				.getMemberEnds().get(0);
-		EObject targetEnd = ((Association) resolveSemanticElement())
-				.getMemberEnds().get(1);
+		EObject sourceEnd = ((Association) resolveSemanticElement()).getMemberEnds().get(0);
+		EObject targetEnd = ((Association) resolveSemanticElement()).getMemberEnds().get(1);
 		addListenerFilter("AssociationEndListenersSource", this, sourceEnd); //$NON-NLS-1$
 		addListenerFilter("AssociationEndListenersTarget", this, targetEnd); //$NON-NLS-1$
 	}
@@ -81,34 +79,27 @@ public class AssociationEditPart extends UMLConnectionNodeEditPart implements
 	 */
 	protected boolean addFixedChild(EditPart childEditPart) {
 		if (childEditPart instanceof AppliedStereotypeAssociationEditPart) {
-			((AppliedStereotypeAssociationEditPart) childEditPart)
-					.setLabel(getPrimaryShape()
-							.getAppliedStereotypeAssociationLabel());
+			((AppliedStereotypeAssociationEditPart) childEditPart).setLabel(getPrimaryShape().getAppliedStereotypeAssociationLabel());
 			return true;
 		}
 		if (childEditPart instanceof AssociationName2EditPart) {
-			((AssociationName2EditPart) childEditPart)
-					.setLabel(getPrimaryShape().getAssociationNameLabel());
+			((AssociationName2EditPart) childEditPart).setLabel(getPrimaryShape().getAssociationNameLabel());
 			return true;
 		}
 		if (childEditPart instanceof AssociationName3EditPart) {
-			((AssociationName3EditPart) childEditPart)
-					.setLabel(getPrimaryShape().getRoleSourceLabel());
+			((AssociationName3EditPart) childEditPart).setLabel(getPrimaryShape().getRoleSourceLabel());
 			return true;
 		}
 		if (childEditPart instanceof AssociationName4EditPart) {
-			((AssociationName4EditPart) childEditPart)
-					.setLabel(getPrimaryShape().getMultiplicitySourceLabel());
+			((AssociationName4EditPart) childEditPart).setLabel(getPrimaryShape().getMultiplicitySourceLabel());
 			return true;
 		}
 		if (childEditPart instanceof AssociationName5EditPart) {
-			((AssociationName5EditPart) childEditPart)
-					.setLabel(getPrimaryShape().getMultiplicityTargetLabel());
+			((AssociationName5EditPart) childEditPart).setLabel(getPrimaryShape().getMultiplicityTargetLabel());
 			return true;
 		}
 		if (childEditPart instanceof AssociationName6EditPart) {
-			((AssociationName6EditPart) childEditPart)
-					.setLabel(getPrimaryShape().getRoleTargetLabel());
+			((AssociationName6EditPart) childEditPart).setLabel(getPrimaryShape().getRoleTargetLabel());
 			return true;
 		}
 		return false;
@@ -117,8 +108,7 @@ public class AssociationEditPart extends UMLConnectionNodeEditPart implements
 	/**
 	 * Creates figure for this edit part.
 	 * 
-	 * Body of this method does not depend on settings in generation model so
-	 * you may safely remove <i>generated</i> tag and modify it.
+	 * Body of this method does not depend on settings in generation model so you may safely remove <i>generated</i> tag and modify it.
 	 * 
 	 * @generated
 	 */
@@ -132,11 +122,8 @@ public class AssociationEditPart extends UMLConnectionNodeEditPart implements
 	 */
 	protected void createDefaultEditPolicies() {
 		super.createDefaultEditPolicies();
-		installEditPolicy(EditPolicyRoles.SEMANTIC_ROLE,
-				new AssociationItemSemanticEditPolicy());
-		installEditPolicy(
-				AppliedStereotypeLabelDisplayEditPolicy.STEREOTYPE_LABEL_POLICY,
-				new AppliedStereotypeLabelDisplayEditPolicy());
+		installEditPolicy(EditPolicyRoles.SEMANTIC_ROLE, new AssociationItemSemanticEditPolicy());
+		installEditPolicy(AppliedStereotypeLabelDisplayEditPolicy.STEREOTYPE_LABEL_POLICY, new AppliedStereotypeLabelDisplayEditPolicy());
 	}
 
 	/**
@@ -173,10 +160,24 @@ public class AssociationEditPart extends UMLConnectionNodeEditPart implements
 	 * @generated NOT
 	 */
 	protected void refreshVisuals() {
-		Property source = (Property) ((Association) resolveSemanticElement())
-				.getMembers().get(0);
-		Property target = (Property) ((Association) resolveSemanticElement())
-				.getMembers().get(1);
+		if (getSource() == null || getTarget() == null) {
+			return;
+		}
+		if (((GraphicalEditPart) getSource()).resolveSemanticElement() == null || ((GraphicalEditPart) getTarget()).resolveSemanticElement() == null) {
+			return;
+		}
+
+		Property source = null;
+		Property target = null;
+
+		if (((Property) (((Association) getUMLElement()).getMemberEnds().get(0))).getType().equals(((GraphicalEditPart) getSource()).resolveSemanticElement())) {
+			source = ((Property) (((Association) getUMLElement()).getMemberEnds().get(0)));
+			target = ((Property) (((Association) getUMLElement()).getMemberEnds().get(1)));
+		} else {
+			source = ((Property) (((Association) getUMLElement()).getMemberEnds().get(1)));
+			target = ((Property) (((Association) getUMLElement()).getMemberEnds().get(0)));
+		}
+
 		int sourceType = 0;
 		int targetType = 0;
 		// owned?
