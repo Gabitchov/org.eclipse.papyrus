445e3f97a323632cbeb59a1d2892d2919cf4213a
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2009-07-09 10:22:29 +0000

    change filter of appearance property section

---
+++ plugins/uml/org.eclipse.papyrus.tabbedproperties.appearance/src/org/eclipse/papyrus/tabbedproperties/appearance/NodeNamedElementFilter.java
@@ -0,0 +1,20 @@
+package org.eclipse.papyrus.tabbedproperties.appearance;
+
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeEditPart;
+import org.eclipse.jface.viewers.IFilter;
+import org.eclipse.uml2.uml.NamedElement;
+
+
+public class NodeNamedElementFilter implements IFilter {
+
+
+    public boolean select(Object object) {
+        if (object instanceof GraphicalEditPart && ((GraphicalEditPart)object).getNotationView().getElement() instanceof NamedElement)
+            return true;
+
+        return false;
+    }
+}
+
+
