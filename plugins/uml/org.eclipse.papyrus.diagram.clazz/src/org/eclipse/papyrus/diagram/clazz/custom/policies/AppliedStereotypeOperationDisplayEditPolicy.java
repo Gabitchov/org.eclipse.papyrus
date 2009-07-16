bd3d0ce7835f1e17ad2a405cd4b9f76c209e8cdc
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2009-07-16 13:19:52 +0000

    adds support of custom display to property labels (to be continued) and operation labels

---
+++ plugins/uml/org.eclipse.papyrus.diagram.clazz/src/org/eclipse/papyrus/diagram/clazz/custom/policies/AppliedStereotypeOperationDisplayEditPolicy.java
@@ -0,0 +1,42 @@
+/*****************************************************************************
+ * Copyright (c) 2009 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Remi Schnekenburger (CEA LIST) remi.schnekenburger@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.diagram.clazz.custom.policies;
+
+import org.eclipse.gef.GraphicalEditPart;
+import org.eclipse.papyrus.diagram.common.editpolicies.AbstractAppliedStereotypeDisplayEditPolicy;
+import org.eclipse.uml2.uml.Operation;
+
+/**
+ * Specific edit policy for label displaying stereotypes and their properties for edges representing UML elements.
+ */
+public class AppliedStereotypeOperationDisplayEditPolicy extends AbstractAppliedStereotypeDisplayEditPolicy {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected Operation getUMLElement() {
+		return (Operation) super.getUMLElement();
+	};
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void refreshDisplay() {
+		// calls the helper for this edit Part
+		OperationLabelHelper.refreshEditPartDisplay((GraphicalEditPart) getHost());
+	}
+
+}
