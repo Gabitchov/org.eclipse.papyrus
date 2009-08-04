1b443743ba14afcb92de17965c62a406785981a7
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2009-08-04 12:32:24 +0000

    adding tabbed properties for profile management of edges and compartment

--- plugins/others/org.eclipse.papyrus.profile/src/org/eclipse/papyrus/profile/utils/UmlElementFilter.java
+++ plugins/others/org.eclipse.papyrus.profile/src/org/eclipse/papyrus/profile/utils/UmlElementFilter.java
@@ -1,18 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2009 CEA LIST.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     CEA LIST - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.papyrus.profile.utils;
 
-import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.jface.viewers.IFilter;
 import org.eclipse.uml2.uml.Element;
-import org.eclipse.uml2.uml.NamedElement;
 
-
+/**
+ * Filter that returns <code>true</code> if the element associated to the object is a UML
+ * {@link Element}.
+ */
 public class UmlElementFilter implements IFilter {
 
-	 public boolean select(Object object) {
-	        if (object instanceof GraphicalEditPart && ((GraphicalEditPart)object).getNotationView().getElement() instanceof Element)
-	            return true;
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean select(Object object) {
+		// test element is a node edit part and its controlled element has an associated UML element
+		if (object instanceof IGraphicalEditPart
+				&& ((IGraphicalEditPart) object).getNotationView().getElement() instanceof Element)
+			return true;
 
-	        return false;
-	    }
+		if (object instanceof Element) {
+			return true;
+		}
+
+		return false;
+	}
 
 }
