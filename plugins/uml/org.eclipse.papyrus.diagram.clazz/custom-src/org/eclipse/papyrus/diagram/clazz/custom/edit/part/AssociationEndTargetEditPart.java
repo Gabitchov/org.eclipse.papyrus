279689f284a681d041b037928f309c98aaedcae5
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2009-12-07 17:58:37 +0000

    RESOLVED - bug 297098: [Class Diagram] Papyrus shall enable to edit association end directly on the diagram https://bugs.eclipse.org/bugs/show_bug.cgi?id=297098

--- plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/edit/part/AssociationEndTargetEditPart.java
+++ plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/edit/part/AssociationEndTargetEditPart.java
@@ -1,34 +1,49 @@
+/*****************************************************************************
+ * Copyright (c) 2009 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
 package org.eclipse.papyrus.diagram.clazz.custom.edit.part;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.transaction.RecordingCommand;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.gef.Request;
-import org.eclipse.gef.requests.DirectEditRequest;
-import org.eclipse.gmf.runtime.diagram.ui.requests.RequestConstants;
-import org.eclipse.gmf.runtime.diagram.ui.tools.TextDirectEditManager;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.jface.window.Window;
 import org.eclipse.papyrus.diagram.clazz.custom.helper.AssociationEndSourceLabelHelper;
-import org.eclipse.papyrus.diagram.clazz.edit.parts.AssociationName3EditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.AssociationName5EditPart;
-import org.eclipse.papyrus.diagram.common.editpolicies.IDirectEdition;
-import org.eclipse.papyrus.extensionpoints.editors.Activator;
-import org.eclipse.papyrus.extensionpoints.editors.ui.ExtendedDirectEditionDialog;
-import org.eclipse.papyrus.extensionpoints.editors.utils.DirectEditorsUtil;
-import org.eclipse.papyrus.extensionpoints.editors.utils.IDirectEditorsIds;
-import org.eclipse.ui.PlatformUI;
 
 
+/**
+ * The Class AssociationEndTargetEditPart.
+ * Ensure the edition of ends in the diagram
+ */
 public class AssociationEndTargetEditPart extends AssociationName5EditPart {
 
+	/**
+	 * Instantiates a new association end target edit part.
+	 * 
+	 * @param view
+	 *        the view
+	 */
 	public AssociationEndTargetEditPart(View view) {
 		super(view);
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	 public EObject resolveSemanticElement() {
-		 return AssociationEndSourceLabelHelper.getInstance().getUMLElement(this);
-	 }
+
+
+	/**
+	 * @see org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart#resolveSemanticElement()
+	 * 
+	 * @return
+	 */
+
+	public EObject resolveSemanticElement() {
+		return AssociationEndSourceLabelHelper.getInstance().getUMLElement(this);
+	}
 }
