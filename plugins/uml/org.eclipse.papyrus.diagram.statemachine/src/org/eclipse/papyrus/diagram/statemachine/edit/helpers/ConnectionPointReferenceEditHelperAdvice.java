d77f1606ebb1f9cecf8815c71c4a5fccfe869247
dservat <dservat@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-02-03 13:35:16 +0000

    Remove old source code

--- plugins/uml/org.eclipse.papyrus.diagram.statemachine/src/org/eclipse/papyrus/diagram/statemachine/edit/helpers/ConnectionPointReferenceEditHelperAdvice.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.papyrus.diagram.statemachine.edit.helpers;
-
-import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
-
-/**
- * @generated
- */
-public class ConnectionPointReferenceEditHelperAdvice extends AbstractEditHelperAdvice {
-}
