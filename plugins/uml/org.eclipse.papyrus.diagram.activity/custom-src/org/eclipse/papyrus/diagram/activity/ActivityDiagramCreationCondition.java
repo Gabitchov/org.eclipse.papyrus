cf59e368d81f9b414ae3800a90cecdfd2c422d6e
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-04-09 15:18:22 +0000

    NEW - bug 308652: [General] pb of several id for Diagram creation Command https://bugs.eclipse.org/bugs/show_bug.cgi?id=308652

--- plugins/uml/org.eclipse.papyrus.diagram.activity/custom-src/org/eclipse/papyrus/diagram/activity/ActivityDiagramCreationCondition.java
+++ plugins/uml/org.eclipse.papyrus.diagram.activity/custom-src/org/eclipse/papyrus/diagram/activity/ActivityDiagramCreationCondition.java
@@ -12,6 +12,7 @@ package org.eclipse.papyrus.diagram.activity;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.papyrus.core.extension.commands.ICreationCondition;
+import org.eclipse.papyrus.core.extension.commands.PerspectiveContextDependence;
 import org.eclipse.uml2.uml.Activity;
 import org.eclipse.uml2.uml.BehavioredClassifier;
 import org.eclipse.uml2.uml.Package;
@@ -20,17 +21,23 @@ import org.eclipse.uml2.uml.Package;
  * ActivityDiagramCreationCondition class allows to check if an Activity diagram can be added to the
  * selected element.
  */
-public class ActivityDiagramCreationCondition implements ICreationCondition {
+public class ActivityDiagramCreationCondition  extends PerspectiveContextDependence implements ICreationCondition {
 
 	/**
 	 * @return whether the diagram can be created.
 	 */
 	public boolean create(EObject selectedElement) {
-		// FIXME creation under BehavioredClassifier disabled for now (until bug 348 fixed)
-		if(selectedElement instanceof Activity && selectedElement.eContainer() instanceof BehavioredClassifier) {
-			return false;
+
+		if(super.create(selectedElement)){
+
+			// FIXME creation under BehavioredClassifier disabled for now (until bug 348 fixed)
+			if(selectedElement instanceof Activity && selectedElement.eContainer() instanceof BehavioredClassifier) {
+				return false;
+			}
+			return selectedElement instanceof Activity || selectedElement instanceof Package;// || selectedElement instanceof BehavioredClassifier;
 		}
-		return selectedElement instanceof Activity || selectedElement instanceof Package;// || selectedElement instanceof BehavioredClassifier;
+		return false;
 	}
+
 
 }
