6a0bd6668816fcf575e8578f83c8ecb7ada2940f
tlandre <tlandre@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-04-20 08:42:48 +0000

    RESOLVED - bug 309772: [Wizards] Create UML diagram should be specific to UML https://bugs.eclipse.org/bugs/show_bug.cgi?id=309772

--- plugins/uml/org.eclipse.papyrus.diagram.statemachine/custom-src/org/eclipse/papyrus/diagram/statemachine/StateMachineDiagramCreationCondition.java
+++ plugins/uml/org.eclipse.papyrus.diagram.statemachine/custom-src/org/eclipse/papyrus/diagram/statemachine/StateMachineDiagramCreationCondition.java
@@ -1,34 +1,31 @@
 /*****************************************************************************
- * Copyright (c) 2009 Atos Origin.
- *
- *    
+ * Copyright (c) 2010 Atos Origin.
+ *  
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * Contributors:
+ *  Atos Origin - Initial API and implementation
  *****************************************************************************/
-package org.eclipse.papyrus.diagram.activity;
+package org.eclipse.papyrus.diagram.statemachine;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.papyrus.core.extension.commands.ICreationCondition;
 import org.eclipse.papyrus.core.extension.commands.PerspectiveContextDependence;
-import org.eclipse.uml2.uml.Activity;
-import org.eclipse.uml2.uml.BehavioredClassifier;
-import org.eclipse.uml2.uml.Package;
-
+import org.eclipse.uml2.uml.Element;
 /**
- * ActivityDiagramCreationCondition class allows to check if an Activity diagram can be added to the
+ * StateMachineDiagramCreationCondition class allows to check if a State Machine diagram can be added to the
  * selected element.
  */
-public class ActivityDiagramCreationCondition  extends PerspectiveContextDependence {
+public class StateMachineDiagramCreationCondition  extends PerspectiveContextDependence {
 
 	/**
 	 * @return whether the diagram can be created.
 	 */
 	public boolean create(EObject selectedElement) {
 		if(super.create(selectedElement)){
-			return selectedElement instanceof Activity || selectedElement instanceof Package || selectedElement instanceof BehavioredClassifier;
+			// TODO : to specify according to the spec
+			return selectedElement instanceof Element;
 		}
 		return false;
 	}
