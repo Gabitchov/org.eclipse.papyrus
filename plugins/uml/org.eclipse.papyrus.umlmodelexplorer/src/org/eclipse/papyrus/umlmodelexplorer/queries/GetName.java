3f0a5928459c48aea7a559a6ffe8a20e70ac89ad
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-04-21 13:31:30 +0000

    first integration

---
+++ plugins/uml/org.eclipse.papyrus.umlmodelexplorer/src/org/eclipse/papyrus/umlmodelexplorer/queries/GetName.java
@@ -0,0 +1,14 @@
+package org.eclipse.papyrus.umlmodelexplorer.queries;
+import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.gmt.modisco.infra.query.core.exception.ModelQueryExecutionException;
+import org.eclipse.gmt.modisco.infra.query.core.java.IJavaModelQuery;
+import org.eclipse.gmt.modisco.infra.query.core.java.ParameterValueList;
+
+/** get the name + the list of stereotypes from a named Element */
+public class GetName implements IJavaModelQuery<NamedElement, String> {
+	public String evaluate(final NamedElement context, final ParameterValueList parameterValues)
+			throws ModelQueryExecutionException {
+		// TODO Auto-generated method stub
+		return context.getName();
+	}
+}
