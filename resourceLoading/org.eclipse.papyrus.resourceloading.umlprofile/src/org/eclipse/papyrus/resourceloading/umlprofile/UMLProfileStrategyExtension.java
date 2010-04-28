fdc3926307a87587c837c3a49bb8efbda7fd1a7f
eperico <eperico@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-04-28 13:45:11 +0000

    manage profile loading

--- resourceLoading/org.eclipse.papyrus.resourceloading.umlprofile/src/org/eclipse/papyrus/resourceloading/umlprofile/UMLProfileStrategyExtension.java
+++ resourceLoading/org.eclipse.papyrus.resourceloading.umlprofile/src/org/eclipse/papyrus/resourceloading/umlprofile/UMLProfileStrategyExtension.java
@@ -10,17 +10,23 @@
  * Contributors:
  *  Emilien Perico (Atos Origin) emilien.perico@atosorigin.com - Initial API and implementation
  *
-  *****************************************************************************/
+ *****************************************************************************/
 package org.eclipse.papyrus.resourceloading.umlprofile;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.papyrus.core.resourceloading.ILoadingStrategyExtension;
 import org.eclipse.papyrus.core.utils.DiResourceSet;
+import org.eclipse.papyrus.core.utils.caches.TypeCacheAdapter;
 import org.eclipse.uml2.uml.Package;
-import org.eclipse.uml2.uml.ProfileApplication;
 import org.eclipse.uml2.uml.UMLPackage;
 
 
@@ -29,33 +35,60 @@ import org.eclipse.uml2.uml.UMLPackage;
  */
 public class UMLProfileStrategyExtension implements ILoadingStrategyExtension {
 
+	private static final String UML_FILE_EXTENSION = "uml";
+
 	/**
 	 * {@inheritDoc}
+	 * Load a profile resource if it used in the model
 	 */
 	public boolean loadResource(DiResourceSet diResourceSet, URI uri) {
-		// TODO use type cache adapter
-		boolean result = false;
-		Resource modelResource = diResourceSet.getModelResource();
-//		TypeCacheAdapter adapter = TypeCacheAdapter.getExistingtypeCacheAdapter(diResourceSet);
-//		Collection<EObject> applications = adapter.getReachableObjectsOfType(null,UMLPackage.Literals.PROFILE_APPLICATION);
-		if(modelResource.getContents().size() > 0) {
-			EObject root = modelResource.getContents().get(0);
-			if (root instanceof Package) {
-				Package rootPackage = (Package)root;
-				for (ProfileApplication profile : rootPackage.getProfileApplications()) {
-					EObject proxy = (EObject)profile.eGet(UMLPackage.Literals.PROFILE_APPLICATION__APPLIED_PROFILE,false);
-					if (proxy != null && proxy.eIsProxy())
-					{
-						InternalEObject internal = (InternalEObject)proxy ;
-						if (uri.trimFragment().equals(internal.eProxyURI()))
-						{
-							return true ;
+		if(UML_FILE_EXTENSION.equals(diResourceSet.getModelFileExtension())) {
+			TypeCacheAdapter adapter = null;
+			for(Adapter a : diResourceSet.eAdapters()) {
+				if(a instanceof TypeCacheAdapter) {
+					adapter = (TypeCacheAdapter)a;
+					break;
+				}
+			}
+			if(adapter == null) {
+				// throw ...
+			}
+			Resource modelResource = diResourceSet.getModelResource();
+			if(modelResource != null && !modelResource.getContents().isEmpty()) {
+				if(adapter != null) {
+					Collection<EObject> applications = null;
+					if(!adapter.isAlreadyComputed(UMLPackage.Literals.PROFILE_APPLICATION)) {
+						applications = new LinkedList<EObject>();
+						// a profile application can only be stored in a package
+						for(int i = 0; i < diResourceSet.getResources().size(); i++) {
+							for(TreeIterator<EObject> j = EcoreUtil.getAllProperContents(diResourceSet.getResources().get(i), false); j.hasNext();) {
+								EObject e = j.next();
+								if(e instanceof Package) {
+									applications.addAll(((Package)e).getProfileApplications());
+								} else {
+									j.prune();
+								}
+							}
+						}
+						adapter.fillFirstEntryCache(UMLPackage.Literals.PROFILE_APPLICATION, applications);
+					} else {
+						applications = adapter.getReachableObjectsOfType(modelResource.getContents().get(0), UMLPackage.Literals.PROFILE_APPLICATION);
+					}
+					if(applications != null) {
+						for(EObject profileApp : applications) {
+							EObject proxy = (EObject)profileApp.eGet(UMLPackage.Literals.PROFILE_APPLICATION__APPLIED_PROFILE, false);
+							if(proxy != null && proxy.eIsProxy()) {
+								InternalEObject internal = (InternalEObject)proxy;
+								if(uri.trimFragment().equals(internal.eProxyURI().trimFragment())) {
+									return true;
+								}
+							}
 						}
 					}
 				}
 			}
 		}
-		return result;
+		return false;
 	}
 
 }
