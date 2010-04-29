291b562e1dcdf1e85941e82ba83db0bdb2cfad0b
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-04-29 10:04:13 +0000

    renamed basic tab to UML updated the test for the tab descriptor  provider

--- plugins/core/org.eclipse.papyrus.properties.tabbed.core/src/org/eclipse/papyrus/properties/tabbed/core/provider/XMLBasedTabDescriptorProvider.java
+++ plugins/core/org.eclipse.papyrus.properties.tabbed.core/src/org/eclipse/papyrus/properties/tabbed/core/provider/XMLBasedTabDescriptorProvider.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.papyrus.core.editor.CoreMultiDiagramEditor;
-import org.eclipse.papyrus.core.utils.EditorUtils;
 import org.eclipse.papyrus.properties.tabbed.core.Activator;
 import org.eclipse.papyrus.properties.tabbed.core.view.PropertyServiceUtil;
 import org.eclipse.ui.IWorkbenchPart;
@@ -42,7 +41,7 @@ public class XMLBasedTabDescriptorProvider implements ITabDescriptorProvider {
 	 * {@inheritDoc}
 	 */
 	public ITabDescriptor[] getTabDescriptors(IWorkbenchPart part, ISelection selection) {
-		if(descriptors == null && part != null) {
+		if(descriptors == null && part instanceof CoreMultiDiagramEditor && selection != null) {
 			List<ITabDescriptor> descriptors = new ArrayList<ITabDescriptor>();
 
 			List<List<ITabDescriptor>> list = PropertyServiceUtil.getTabDescriptors();
@@ -50,7 +49,7 @@ public class XMLBasedTabDescriptorProvider implements ITabDescriptorProvider {
 				descriptors.addAll((List<ITabDescriptor>)sub);
 			}
 			// get all tab descriptors for the registered extension points
-			TabbedPropertyRegistry registry = TabbedPropertyRegistryFactory.getInstance().createRegistry((CoreMultiDiagramEditor)EditorUtils.getMultiDiagramEditor());
+			TabbedPropertyRegistry registry = TabbedPropertyRegistryFactory.getInstance().createRegistry((CoreMultiDiagramEditor)part);
 
 			// invoke dynamically on the tab registry, as method is private :/
 			// problem of implementation of tabbed properties tabbed registry. Either contribution using extension points, either a tabprovider
@@ -61,7 +60,9 @@ public class XMLBasedTabDescriptorProvider implements ITabDescriptorProvider {
 				ITabDescriptor[] registeredTabDesriptors;
 
 				registeredTabDesriptors = (ITabDescriptor[])method.invoke(registry);
-				descriptors.addAll(Arrays.asList(registeredTabDesriptors));
+				if(registeredTabDesriptors != null) {
+					descriptors.addAll(Arrays.asList(registeredTabDesriptors));
+				}
 			} catch (IllegalArgumentException e) {
 				Activator.log.error(e);
 			} catch (IllegalAccessException e) {
