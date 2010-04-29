57095443527f80d104373f5c75c4cc5c367f1055
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-04-29 11:57:22 +0000

    change name

--- plugins/uml/org.eclipse.papyrus.uml.perspective.configuration/src/org/eclipse/papyrus/uml/ui/perspectiveconfiguration/Activator.java
+++ plugins/uml/org.eclipse.papyrus.uml.perspective.configuration/src/org/eclipse/papyrus/uml/ui/perspectiveconfiguration/Activator.java
@@ -1,4 +1,4 @@
-package org.eclipse.papyrus.uml.perspective.configuration;
+package org.eclipse.papyrus.uml.ui.perspectiveconfiguration;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
