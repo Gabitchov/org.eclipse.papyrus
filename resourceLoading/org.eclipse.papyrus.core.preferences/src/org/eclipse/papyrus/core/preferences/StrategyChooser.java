d4341bcce49f64b8128b75071943c80a61369daf
eperico <eperico@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-04-30 09:17:39 +0000

    initial checking: manage preferences for loading strategies

---
+++ resourceLoading/org.eclipse.papyrus.core.preferences/src/org/eclipse/papyrus/core/preferences/StrategyChooser.java
@@ -0,0 +1,53 @@
+/*****************************************************************************
+ * Copyright (c) 2010 Atos Origin.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Emilien Perico (Atos Origin) emilien.perico@atosorigin.com - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.core.preferences;
+
+import org.eclipse.papyrus.core.resourceloading.IStrategyChooser;
+
+
+/**
+ * The Class StrategyChooser.
+ */
+public class StrategyChooser implements IStrategyChooser {
+
+	/** The current strategy. */
+	private static int currentStrategy = -1;
+
+	/**
+	 * Instantiates a new strategy chooser.
+	 */
+	public StrategyChooser() {
+	}
+
+	/**
+	 * @see org.eclipse.papyrus.core.resourceloading.IStrategyChooser#getCurrentStrategy()
+	 */
+	public int getCurrentStrategy() {
+		if(currentStrategy == -1) {
+			// set the current strategy at the first time
+			currentStrategy = CorePlugin.getDefault().getPreferenceStore().getInt(ICorePreferenceConstants.PREF_CORE_DEFINE_LOADING_STRATEGY);
+		}
+		return currentStrategy;
+	}
+
+	/**
+	 * Sets the current strategy.
+	 * 
+	 * @param strategy the new current strategy ID
+	 */
+	public static void setCurrentStrategy(int strategy) {
+		currentStrategy = strategy;
+	}
+
+}
