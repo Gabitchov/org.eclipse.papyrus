d4c69cb9211fc3d281d33442e4bca9cb6eec4d20
cdumoulin <cdumoulin@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-05-06 14:39:22 +0000

    ASSIGNED - bug 311896: [Sash Editor] Allow refresh of tabs name and icons https://bugs.eclipse.org/bugs/show_bug.cgi?id=311896
    Add a common ancestor to nested GMF editors.
    This ancestors take cares to listen to diagram's name change and to throw event.

---
+++ plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/part/UmlGmfDiagramEditor.java
@@ -0,0 +1,127 @@
+/**
+ * 
+ */
+package org.eclipse.papyrus.diagram.common.part;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
+import org.eclipse.papyrus.core.services.ServiceException;
+import org.eclipse.papyrus.core.services.ServicesRegistry;
+
+
+/**
+ * Common ancestor of GMF based editors for UML.
+ * This class allows to declare stuff commons to all this kind of editors.
+ * 
+ * @author cedric dumoulin
+ *
+ */
+public class UmlGmfDiagramEditor extends DiagramDocumentEditor {
+
+
+	/**
+	 * The associated Diagram.
+	 */
+	private Diagram diagram;
+	
+	/**
+	 * Object used to synchronize the name of the editor with the name of the diagram.
+	 */
+	private PartNameSynchronizer partNameSynchronizer;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param servicesRegistry
+	 * @param diagram
+	 * @throws ServiceException
+	 */
+	public UmlGmfDiagramEditor(ServicesRegistry servicesRegistry, Diagram diagram) throws ServiceException {
+		super(true);
+		this.diagram = diagram;
+		
+		// Install synchronizer
+		partNameSynchronizer = new PartNameSynchronizer(diagram);
+	}
+
+	/**
+	 * Set the associated Diagram.
+	 */
+	public void setDiagram(Diagram diagram) {
+		this.diagram = diagram;
+		partNameSynchronizer.setDiagram(diagram);
+	}
+
+	/**
+	 * Get the associated Diagram
+	 */
+	public Diagram getDiagram() {
+		return diagram;
+	}
+
+	/**
+	 * A class taking in charge the synchronization of the partName and the diagram name.
+	 * When diagram name change, the other is automatically updated.
+	 * @author cedric dumoulin
+	 *
+	 */
+	public class PartNameSynchronizer {
+		
+		Diagram diagram;
+		
+		/**
+		 * Listener on diagram name change.
+		 */
+		private Adapter diagramNameListener = new Adapter() {
+	
+			public void notifyChanged(Notification notification) {
+				if( notification.getFeatureID(Diagram.class) == NotationPackage.DIAGRAM__NAME && notification.getNotifier() == diagram) {
+					setPartName(diagram.getName());
+				}
+				
+			}
+	
+			public Notifier getTarget() {
+				return null;
+			}
+	
+			public void setTarget(Notifier newTarget) {
+			}
+	
+			public boolean isAdapterForType(Object type) {
+				return false;
+			}
+			
+		};
+		/**
+		 * 
+		 * Constructor.
+		 *
+		 * @param diagram
+		 */
+		PartNameSynchronizer( Diagram diagram ) {
+			setDiagram(diagram);
+		}
+		
+		/**
+		 * Change the associated diagram.
+		 * @param diagram
+		 */
+		public void setDiagram( Diagram diagram) {
+			// Remove from old diagram, if any
+			if(this.diagram != null)
+				diagram.eAdapters().remove(diagramNameListener);
+			
+			// Set new Diagram
+			this.diagram = diagram;
+			// Set editor name
+			setPartName(diagram.getName());
+			// Listen to name change
+			diagram.eAdapters().add(diagramNameListener);
+		}
+	}
+}
