c859c40b66c14993811be14dc576884463760527
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-06-02 00:33:53 +0000

    added an action to remove a controller from a container

--- plugins/core/org.eclipse.papyrus.properties.tabbed.customization/src/org/eclipse/papyrus/properties/tabbed/customization/state/ControllerDescriptorState.java
+++ plugins/core/org.eclipse.papyrus.properties.tabbed.customization/src/org/eclipse/papyrus/properties/tabbed/customization/state/ControllerDescriptorState.java
@@ -16,13 +16,23 @@ import java.beans.PropertyChangeSupport;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuCreator;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.papyrus.properties.runtime.controller.descriptor.IPropertyEditorControllerDescriptor;
+import org.eclipse.papyrus.properties.tabbed.customization.Activator;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 
 
 /**
  * State for controller descriptors
  */
-public class ControllerDescriptorState extends AbstractState {
+public class ControllerDescriptorState extends AbstractState implements IMenuCreator {
 
 	/** identifier for the controller descriptor editor dialog */
 	private static final String CONTROLLER_DESCRIPTOR_EDITOR_DLG = "ControllerDescriptorEditorDialog";
@@ -35,6 +45,9 @@ public class ControllerDescriptorState extends AbstractState {
 
 	/** change support for this bean */
 	private PropertyChangeSupport changeSupport;
+
+	/** menu manager used to create elements */
+	private MenuManager manager;
 
 	/**
 	 * Creates a new ControllerDescriptorState.
@@ -112,4 +125,76 @@ public class ControllerDescriptorState extends AbstractState {
 	public List<ITraversableModelElement> getChildren() {
 		return Collections.emptyList();
 	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Menu getMenu(final Control parent) {
+		if(manager == null) {
+			manager = new MenuManager();
+		}
+		Menu menu = manager.getMenu();
+		if(menu != null) {
+			menu.dispose();
+			menu = null;
+		}
+		manager.removeAll();
+		menu = manager.createContextMenu(parent);
+		IAction removeAction = new Action("Remove Controller", Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "/icons/delete.gif")) {
+
+			/**
+			 * {@inheritDoc}
+			 */
+			@Override
+			public void run() {
+				// remove this section descriptor state from its parent
+				if(parent instanceof Tree) {
+					TreeItem[] selectedItems = ((Tree)parent).getSelection();
+					if(selectedItems.length < 1) {
+						Activator.log.warn("Impossible to find the current selection in the tree");
+						return;
+					}
+					TreeItem selectedItem = selectedItems[0];
+					TreeItem parentItem = selectedItem.getParentItem();
+					// parent item should be the section descriptor set 
+					if(parentItem == null) {
+						Activator.log.warn("Impossible to find the parent for current selection in the tree ");
+						return;
+					}
+					Object parent = parentItem.getData();
+					// test the parent is a FragmentDescriptorState
+					if((parent instanceof ContainerDescriptorState)) {
+						((ContainerDescriptorState)parent).removePropertyEditorControllerState(ControllerDescriptorState.this);
+					}
+				}
+			}
+
+		};
+		manager.add(removeAction);
+		manager.add(new Separator(ADD_GROUP));
+
+		return menu;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void dispose() {
+		if(manager != null) {
+			Menu menu = manager.getMenu();
+			if(menu != null) {
+				menu.dispose();
+				menu = null;
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Menu getMenu(Menu parent) {
+		// nothing to do here
+		return null;
+	}
+
 }
