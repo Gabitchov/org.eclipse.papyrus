7cf74e911197260da09a4432db8feeb079387c15
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-06-15 15:52:29 +0000

    315969     	[Wizard] Separate Model and Diagram initialization commands

---
+++ plugins/sysml/org.eclipse.papyrus.sysml.diagram.ui/src/org/eclipse/papyrus/sysml/diagram/ui/NewSysMLModelWizard.java
@@ -0,0 +1,105 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Tatiana Fesenko (CEA LIST) - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.sysml.diagram.ui;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.papyrus.wizards.CreateModelWizard;
+import org.eclipse.papyrus.wizards.SelectDiagramCategoryPage;
+import org.eclipse.papyrus.wizards.SelectDiagramKindPage;
+import org.eclipse.papyrus.sysml.diagram.common.commands.CreateSysMLModelCommand;
+import org.eclipse.ui.IWorkbench;
+
+
+/**
+ * The Class NewSysMLModelWizard.
+ */
+public class NewSysMLModelWizard extends CreateModelWizard {
+
+	/**
+	 * @see org.eclipse.papyrus.wizards.CreateModelWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
+	 * 
+	 * @param workbench
+	 * @param selection
+	 */
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		super.init(workbench, selection);
+		setWindowTitle("New SysML Model");
+	}
+
+	/**
+	 * Instantiates a new new sys ml model wizard.
+	 */
+	public NewSysMLModelWizard() {
+		super();
+	}
+
+	/**
+	 * Instantiates a new new sys ml model wizard.
+	 * 
+	 * @param domainModelURI
+	 *        the domain model uri
+	 */
+	public NewSysMLModelWizard(URI domainModelURI) {
+		super(domainModelURI);
+	}
+
+	/**
+	 * @see org.eclipse.jface.wizard.Wizard#getNextPage(org.eclipse.jface.wizard.IWizardPage)
+	 * 
+	 * @param page
+	 * @return
+	 */
+	@Override
+	public IWizardPage getNextPage(IWizardPage page) {
+		IWizardPage next = super.getNextPage(page);
+		if(next != null && SelectDiagramCategoryPage.PAGE_ID.equals(next.getName())) {
+			return super.getNextPage(next);
+		}
+		return next;
+	}
+
+	/**
+	 * @see org.eclipse.papyrus.wizards.NewPapyrusProjectWizard#addPages()
+	 *
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+		if(newModelFilePage != null) {
+			newModelFilePage.setTitle("Papyrus SysML Project");
+			newModelFilePage.setDescription("Create a New Papyrus SysML Project");
+		}
+	}
+
+	/**
+	 * @see org.eclipse.papyrus.wizards.CreateModelWizard#getSelectDiagramKindPage()
+	 * 
+	 * @return
+	 */
+	@Override
+	protected SelectDiagramKindPage getSelectDiagramKindPage() {
+		return new SelectDiagramKindPage() {
+
+			@Override
+			protected String getDiagramCategory() {
+				return CreateSysMLModelCommand.COMMAND_ID;
+			}
+		};
+	}
+
+}
