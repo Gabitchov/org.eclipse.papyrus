f3274e7fb305f65638ecae6a0ac3a4452f56d477
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-07-07 12:44:44 +0000

    318000 -  [Class Diagram] Refactor the creation / move / drag and drop behavior of the ContainmentLink - Delete command

--- plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/policies/itemsemantic/CustomPackageItemSemanticEditPolicy.java
+++ plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/policies/itemsemantic/CustomPackageItemSemanticEditPolicy.java
@@ -1,10 +1,16 @@
 package org.eclipse.papyrus.diagram.clazz.custom.policies.itemsemantic;
 
+import java.util.Iterator;
+
+import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.requests.ReconnectRequest;
 import org.eclipse.gmf.runtime.common.core.command.ICompositeCommand;
+import org.eclipse.gmf.runtime.diagram.core.commands.DeleteCommand;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.emf.commands.core.command.CompositeTransactionalCommand;
+import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyReferenceCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyReferenceRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ReorientReferenceRelationshipRequest;
@@ -12,8 +18,23 @@ import org.eclipse.gmf.runtime.notation.Edge;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.diagram.clazz.custom.command.ContainmentLinkReorientCommand;
 import org.eclipse.papyrus.diagram.clazz.custom.helper.ContainmentHelper;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.AbstractionEditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.AddedLinkEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.CommentAnnotatedElementEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.ConnectorDurationObservationEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.ConnectorTimeObservationEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.ConstraintConstrainedElementEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.DependencyBranchEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.DependencyEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.ElementImportEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.PackageImportEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.PackageMergeEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.ProfileApplicationEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.RealizationEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.TemplateBindingEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.UsageEditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.policies.PackageItemSemanticEditPolicy;
+import org.eclipse.papyrus.diagram.clazz.part.UMLVisualIDRegistry;
 
 public class CustomPackageItemSemanticEditPolicy extends PackageItemSemanticEditPolicy {
 
@@ -63,12 +84,79 @@ public class CustomPackageItemSemanticEditPolicy extends PackageItemSemanticEdit
 		return super.getDestroyReferenceCommand(req);
 	}
 
+	protected Command getDestroyElementCommandGen(DestroyElementRequest req) {
+		View view = (View)getHost().getModel();
+		CompositeTransactionalCommand cmd = new CompositeTransactionalCommand(getEditingDomain(), null);
+		cmd.setTransactionNestingEnabled(false);
+
+		for(Iterator<?> it = view.getTargetEdges().iterator(); it.hasNext();) {
+			Edge incomingLink = (Edge)it.next();
+			switch(UMLVisualIDRegistry.getVisualID(incomingLink)) {
+			case CommentAnnotatedElementEditPart.VISUAL_ID:
+			case ConstraintConstrainedElementEditPart.VISUAL_ID:
+			case ConnectorTimeObservationEditPart.VISUAL_ID:
+			case ConnectorDurationObservationEditPart.VISUAL_ID:
+				DestroyReferenceRequest destroyRefReq = new DestroyReferenceRequest(incomingLink.getSource().getElement(), null, incomingLink.getTarget().getElement(), false);
+				cmd.add(new DestroyReferenceCommand(destroyRefReq));
+				cmd.add(new DeleteCommand(getEditingDomain(), incomingLink));
+				break;
+			case RealizationEditPart.VISUAL_ID:
+			case AbstractionEditPart.VISUAL_ID:
+			case UsageEditPart.VISUAL_ID:
+			case DependencyEditPart.VISUAL_ID:
+			case DependencyBranchEditPart.VISUAL_ID:
+			case ElementImportEditPart.VISUAL_ID:
+			case PackageImportEditPart.VISUAL_ID:
+			case PackageMergeEditPart.VISUAL_ID:
+			case TemplateBindingEditPart.VISUAL_ID:
+//			case AddedLinkEditPart.VISUAL_ID:
+				DestroyElementRequest destroyEltReq = new DestroyElementRequest(incomingLink.getElement(), false);
+				cmd.add(new DestroyElementCommand(destroyEltReq));
+				cmd.add(new DeleteCommand(getEditingDomain(), incomingLink));
+				break;
+			}
+		}
+
+		for(Iterator<?> it = view.getSourceEdges().iterator(); it.hasNext();) {
+			Edge outgoingLink = (Edge)it.next();
+			switch(UMLVisualIDRegistry.getVisualID(outgoingLink)) {
+			case RealizationEditPart.VISUAL_ID:
+			case AbstractionEditPart.VISUAL_ID:
+			case UsageEditPart.VISUAL_ID:
+			case DependencyEditPart.VISUAL_ID:
+			case DependencyBranchEditPart.VISUAL_ID:
+			case ElementImportEditPart.VISUAL_ID:
+			case PackageImportEditPart.VISUAL_ID:
+			case PackageMergeEditPart.VISUAL_ID:
+			case ProfileApplicationEditPart.VISUAL_ID:
+			case TemplateBindingEditPart.VISUAL_ID:
+//			case AddedLinkEditPart.VISUAL_ID:
+				DestroyElementRequest destroyEltReq = new DestroyElementRequest(outgoingLink.getElement(), false);
+				cmd.add(new DestroyElementCommand(destroyEltReq));
+				cmd.add(new DeleteCommand(getEditingDomain(), outgoingLink));
+				break;
+			}
+		}
+		EAnnotation annotation = view.getEAnnotation("Shortcut"); //$NON-NLS-1$
+		if(annotation == null) {
+			// there are indirectly referenced children, need extra commands: false
+			addDestroyChildNodesCommand(cmd);
+			addDestroyShortcutsCommand(cmd, view);
+			// delete host element
+			cmd.add(new DestroyElementCommand(req));
+		} else {
+			cmd.add(new DeleteCommand(getEditingDomain(), view));
+		}
+		return getGEFWrapper(cmd.reduce());
+	}
+
 	/**
 	 * 
 	 * {@inheritDoc}
 	 */
 	protected Command getDestroyElementCommand(DestroyElementRequest req) {
-		ICommandProxy command = (ICommandProxy)super.getDestroyElementCommand(req);
+		
+		ICommandProxy command = (ICommandProxy)getDestroyElementCommandGen(req);
 		CompositeTransactionalCommand cmd = new CompositeTransactionalCommand(getEditingDomain(), null);
 		cmd.add(command.getICommand());
 		
