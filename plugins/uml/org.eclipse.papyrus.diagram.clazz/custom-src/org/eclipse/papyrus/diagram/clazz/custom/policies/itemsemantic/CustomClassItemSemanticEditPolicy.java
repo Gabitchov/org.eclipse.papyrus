2ce314b6d3c443e2611bf9855c12889e21f4a318
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-07-07 12:50:18 +0000

    318000 -  [Class Diagram] Refactor the creation / move / drag and drop behavior of the ContainmentLink - Delete command

--- plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/policies/itemsemantic/CustomClassItemSemanticEditPolicy.java
+++ plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/policies/itemsemantic/CustomClassItemSemanticEditPolicy.java
@@ -13,14 +13,21 @@
  *****************************************************************************/
 package org.eclipse.papyrus.diagram.clazz.custom.policies.itemsemantic;
 
+import java.util.Iterator;
+
+import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.UnexecutableCommand;
 import org.eclipse.gef.requests.ReconnectRequest;
 import org.eclipse.gmf.runtime.common.core.command.ICompositeCommand;
+import org.eclipse.gmf.runtime.diagram.core.commands.DeleteCommand;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.emf.commands.core.command.CompositeTransactionalCommand;
+import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyReferenceCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateRelationshipRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyReferenceRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ReorientReferenceRelationshipRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ReorientRelationshipRequest;
 import org.eclipse.gmf.runtime.notation.Edge;
@@ -31,11 +38,27 @@ import org.eclipse.papyrus.diagram.clazz.custom.command.ContainmentLinkReorientC
 import org.eclipse.papyrus.diagram.clazz.custom.helper.ContainmentHelper;
 import org.eclipse.papyrus.diagram.clazz.edit.commands.CommentAnnotatedElementReorientCommand;
 import org.eclipse.papyrus.diagram.clazz.edit.commands.ConstraintConstrainedElementReorientCommand;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.AbstractionEditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.AddedLinkEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.AssociationBranchEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.AssociationClass2EditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.AssociationEditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.CommentAnnotatedElementEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.ConnectorDurationObservationEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.ConnectorTimeObservationEditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.ConstraintConstrainedElementEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.DependencyBranchEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.DependencyEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.ElementImportEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.GeneralizationEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.InterfaceRealizationEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.PackageImportEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.RealizationEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.SubstitutionEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.TemplateBindingEditPart;
+import org.eclipse.papyrus.diagram.clazz.edit.parts.UsageEditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.policies.ClassItemSemanticEditPolicy;
+import org.eclipse.papyrus.diagram.clazz.part.UMLVisualIDRegistry;
 import org.eclipse.papyrus.diagram.clazz.providers.UMLElementTypes;
 
 /**
@@ -130,11 +153,94 @@ public class CustomClassItemSemanticEditPolicy extends ClassItemSemanticEditPoli
 	}
 
 	/**
+	 * Gets the destroy element command gen.
+	 *
+	 * @param req the req
+	 * @return the destroy element command gen
+	 */
+	protected Command getDestroyElementCommandGen(DestroyElementRequest req) {
+		// Temprary copy of super.getDestroyElementCommand()
+		// made in order to avoid NPE with AddedLinkEditPArt and properly process delete of other links
+		// TODO: AddedLink is a reference Link, but it looks like it was generated as a link with class
+		//properly re-generate the code
+		View view = (View)getHost().getModel();
+		CompositeTransactionalCommand cmd = new CompositeTransactionalCommand(getEditingDomain(), null);
+		cmd.setTransactionNestingEnabled(false);
+
+		for(Iterator<?> it = view.getTargetEdges().iterator(); it.hasNext();) {
+			Edge incomingLink = (Edge)it.next();
+			switch(UMLVisualIDRegistry.getVisualID(incomingLink)) {
+			case CommentAnnotatedElementEditPart.VISUAL_ID:
+			case ConstraintConstrainedElementEditPart.VISUAL_ID:
+			case ConnectorTimeObservationEditPart.VISUAL_ID:
+			case ConnectorDurationObservationEditPart.VISUAL_ID:
+				DestroyReferenceRequest destroyRefReq = new DestroyReferenceRequest(incomingLink.getSource().getElement(), null, incomingLink.getTarget().getElement(), false);
+				cmd.add(new DestroyReferenceCommand(destroyRefReq));
+				cmd.add(new DeleteCommand(getEditingDomain(), incomingLink));
+				break;
+			case AssociationClass2EditPart.VISUAL_ID:
+			case AssociationEditPart.VISUAL_ID:
+			case AssociationBranchEditPart.VISUAL_ID:
+			case GeneralizationEditPart.VISUAL_ID:
+			case SubstitutionEditPart.VISUAL_ID:
+			case RealizationEditPart.VISUAL_ID:
+			case AbstractionEditPart.VISUAL_ID:
+			case UsageEditPart.VISUAL_ID:
+			case DependencyEditPart.VISUAL_ID:
+			case DependencyBranchEditPart.VISUAL_ID:
+			case ElementImportEditPart.VISUAL_ID:
+			case TemplateBindingEditPart.VISUAL_ID:
+//			case AddedLinkEditPart.VISUAL_ID:
+				DestroyElementRequest destroyEltReq = new DestroyElementRequest(incomingLink.getElement(), false);
+				cmd.add(new DestroyElementCommand(destroyEltReq));
+				cmd.add(new DeleteCommand(getEditingDomain(), incomingLink));
+				break;
+			}
+		}
+
+		for(Iterator<?> it = view.getSourceEdges().iterator(); it.hasNext();) {
+			Edge outgoingLink = (Edge)it.next();
+			switch(UMLVisualIDRegistry.getVisualID(outgoingLink)) {
+			case AssociationClass2EditPart.VISUAL_ID:
+			case AssociationEditPart.VISUAL_ID:
+			case AssociationBranchEditPart.VISUAL_ID:
+			case GeneralizationEditPart.VISUAL_ID:
+			case InterfaceRealizationEditPart.VISUAL_ID:
+			case SubstitutionEditPart.VISUAL_ID:
+			case RealizationEditPart.VISUAL_ID:
+			case AbstractionEditPart.VISUAL_ID:
+			case UsageEditPart.VISUAL_ID:
+			case DependencyEditPart.VISUAL_ID:
+			case DependencyBranchEditPart.VISUAL_ID:
+			case ElementImportEditPart.VISUAL_ID:
+			case PackageImportEditPart.VISUAL_ID:
+			case TemplateBindingEditPart.VISUAL_ID:
+//			case AddedLinkEditPart.VISUAL_ID:
+				DestroyElementRequest destroyEltReq = new DestroyElementRequest(outgoingLink.getElement(), false);
+				cmd.add(new DestroyElementCommand(destroyEltReq));
+				cmd.add(new DeleteCommand(getEditingDomain(), outgoingLink));
+				break;
+			}
+		}
+		EAnnotation annotation = view.getEAnnotation("Shortcut"); //$NON-NLS-1$
+		if(annotation == null) {
+			// there are indirectly referenced children, need extra commands: false
+			addDestroyChildNodesCommand(cmd);
+			addDestroyShortcutsCommand(cmd, view);
+			// delete host element
+			cmd.add(new DestroyElementCommand(req));
+		} else {
+			cmd.add(new DeleteCommand(getEditingDomain(), view));
+		}
+		return getGEFWrapper(cmd.reduce());
+	}
+
+	/**
 	 * 
 	 * {@inheritDoc}
 	 */
 	protected Command getDestroyElementCommand(DestroyElementRequest req) {
-		ICommandProxy command = (ICommandProxy)super.getDestroyElementCommand(req);
+		ICommandProxy command = (ICommandProxy)getDestroyElementCommandGen(req);
 		CompositeTransactionalCommand cmd = new CompositeTransactionalCommand(getEditingDomain(), null);
 		cmd.add(command.getICommand());
 
