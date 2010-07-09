40da367fb1ee0af58375b016cf551a60a0a52157
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-07-09 11:06:16 +0000

    NEW - bug 313985: [Composite Structure Diagram] port becomes invisible after moving https://bugs.eclipse.org/bugs/show_bug.cgi?id=313985

--- plugins/uml/org.eclipse.papyrus.diagram.profile/src/org/eclipse/papyrus/diagram/profile/edit/parts/ProfileDiagramEditPart.java
+++ plugins/uml/org.eclipse.papyrus.diagram.profile/src/org/eclipse/papyrus/diagram/profile/edit/parts/ProfileDiagramEditPart.java
@@ -16,6 +16,9 @@ package org.eclipse.papyrus.diagram.profile.edit.parts;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.draw2d.IClippingStrategy;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.gef.GraphicalEditPart;
@@ -26,6 +29,7 @@ import org.eclipse.gef.handles.MoveHandle;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.NonResizableLabelEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.figures.BorderedNodeFigure;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.diagram.common.editpolicies.DuplicatePasteEditPolicy;
 import org.eclipse.papyrus.diagram.common.providers.ViewInfo;
@@ -56,6 +60,50 @@ public class ProfileDiagramEditPart extends DiagramEditPart {
 	 */
 	public ProfileDiagramEditPart(View view) {
 		super(view);
+		getFigure().setClippingStrategy(new IClippingStrategy() {
+
+			public Rectangle[] getClip(IFigure childFigure) {
+				// very inefficient, since it implies several tree traversals. Bit handles modifications of the tree structure
+				// It's a workaround instead of the better solution to fix BorderedNodeFigure (overload and let it return
+				// getExtendedBounds)
+				// See bug 313985 (https://bugs.eclipse.org/bugs/show_bug.cgi?id=313985) for more details
+				applyClippingStrategy(childFigure);
+				if(childFigure instanceof BorderedNodeFigure) {
+					return new Rectangle[]{ ((BorderedNodeFigure)childFigure).getExtendedBounds() };
+
+				} else {
+					return new Rectangle[]{ childFigure.getBounds() };
+				}
+			}
+		});
+	}
+
+	/**
+	 * @generated
+	 */
+	public void applyClippingStrategy(IFigure fig) {
+		boolean hasBorderedNodeChild = false;
+		for(Object child : fig.getChildren()) {
+			if(child instanceof IFigure) {
+				applyClippingStrategy((IFigure)child);
+				if(child instanceof BorderedNodeFigure) {
+					hasBorderedNodeChild = true;
+				}
+			}
+		}
+		if(hasBorderedNodeChild && (fig.getClippingStrategy() == null)) {
+			fig.setClippingStrategy(new IClippingStrategy() {
+
+				public Rectangle[] getClip(IFigure childFigure) {
+					if(childFigure instanceof BorderedNodeFigure) {
+						return new Rectangle[]{ ((BorderedNodeFigure)childFigure).getExtendedBounds() };
+					} else {
+						return new Rectangle[]{ childFigure.getBounds() };
+					}
+				}
+			});
+		}
+
 	}
 
 	/**
