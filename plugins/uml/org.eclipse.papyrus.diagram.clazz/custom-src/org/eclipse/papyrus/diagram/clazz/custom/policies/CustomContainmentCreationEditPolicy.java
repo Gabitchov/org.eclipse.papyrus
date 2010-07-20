9c2bf2b28acdc5cfb2f619e1b1f6ade9d2e622f6
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-07-20 09:21:06 +0000

    318000 -  [Class Diagram] Refactor the creation / move / drag and drop behavior of the ContainmentLink - use improved notification dialog -drop

--- plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/policies/CustomContainmentCreationEditPolicy.java
+++ plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/policies/CustomContainmentCreationEditPolicy.java
@@ -14,26 +14,14 @@
 package org.eclipse.papyrus.diagram.clazz.custom.policies;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.commands.CompoundCommand;
-import org.eclipse.gef.commands.UnexecutableCommand;
 import org.eclipse.gef.requests.ChangeBoundsRequest;
-import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.common.core.command.CompositeCommand;
-import org.eclipse.gmf.runtime.diagram.core.commands.AddCommand;
 import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GroupEditPart;
@@ -41,22 +29,16 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.LabelEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CreationEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.l10n.DiagramUIMessages;
-import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
-import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.papyrus.diagram.clazz.custom.helper.ContainmentHelper;
-import org.eclipse.papyrus.diagram.clazz.edit.parts.AssociationClassEditPart;
-import org.eclipse.papyrus.diagram.clazz.edit.parts.AssociationNodeEditPart;
+import org.eclipse.papyrus.diagram.clazz.custom.helper.ContainmentDragDropHelper;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.Class5EditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.ClassEditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.ClassEditPartCN;
-import org.eclipse.papyrus.diagram.clazz.edit.parts.Dependency2EditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.ModelEditPartCN;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.ModelEditPartTN;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.PackageEditPart;
 import org.eclipse.papyrus.diagram.clazz.edit.parts.PackageEditPartCN;
 import org.eclipse.papyrus.diagram.clazz.part.UMLVisualIDRegistry;
-import org.eclipse.uml2.uml.Element;
 
 
 /**
@@ -95,7 +77,9 @@ public class CustomContainmentCreationEditPolicy extends CreationEditPolicy {
 			if(semantic == null) {
 				cc.compose(getReparentViewCommand((IGraphicalEditPart)ep));
 			} else if(context != null && hasContainmentLink(view)) {
-				return getDropWithContainmentCommand((IGraphicalEditPart)ep);
+				View hostView = (View)getHost().getModel();
+				View movedView = (View)ep.getModel();
+				return new ContainmentDragDropHelper(((IGraphicalEditPart)ep).getEditingDomain()).getDropWithContainmentCommand(hostView, movedView);
 			} else if(context != null && shouldReparent(semantic, context)) {
 				cc.compose(getReparentCommand((IGraphicalEditPart)ep));
 			}
@@ -105,78 +89,6 @@ public class CustomContainmentCreationEditPolicy extends CreationEditPolicy {
 
 	private boolean hasContainmentLink(View movedView) {
 		return ourCanHaveContainmentLinks.contains(UMLVisualIDRegistry.getVisualID(movedView));
-	}
-
-	private Command getDropWithContainmentCommand(IGraphicalEditPart ep) {
-		View hostView = (View)getHost().getModel();
-		View movedView = (View)ep.getModel();
-		return getDropWithContainmentCommand(ep.getEditingDomain(), hostView, movedView);
-	}
-
-	private Command getDropWithContainmentCommand(TransactionalEditingDomain domain, View hostView, View movedView) {
-		EObject hostElement = hostView.getElement();
-		if(isMoveToParent(hostView, movedView)) {
-			ContainmentHelper containmentHelper = new ContainmentHelper(domain);
-			CompositeCommand cmd = new CompositeCommand("Move Element");
-			cmd.add(new AddCommand(domain, new EObjectAdapter(hostView), new EObjectAdapter(movedView)));
-			containmentHelper.deleteIncomingContainmentLinksFor(cmd, movedView);
-			return new ICommandProxy(cmd);
-		} else if(isMoveToChild(hostView, movedView)) {
-			ContainmentHelper containmentHelper = new ContainmentHelper(domain);
-			CompositeCommand cmd = new CompositeCommand("Move Element");
-			Element parent = (Element)ViewUtil.resolveSemanticElement((View)getHost().getParent().getParent().getModel());
-			Element child1 = (Element)hostElement;
-			Element child2 = (Element)ViewUtil.resolveSemanticElement(movedView);
-			cmd.add(new MoveElementCommand(domain, containmentHelper, parent, child1));
-			cmd.add(new MoveElementCommand(domain, containmentHelper, child1, child2));
-			cmd.add(new AddCommand(domain, new EObjectAdapter(hostView), new EObjectAdapter(movedView)));
-			containmentHelper.deleteOutgoingContainmentLinksFor(cmd, movedView);
-			return new ICommandProxy(cmd);
-		} else if(ContainmentHelper.hasIncomingContainmentLink(movedView)) {
-			ContainmentHelper containmentHelper = new ContainmentHelper(domain);
-			CompositeCommand cmd = new CompositeCommand("Move Element");
-			Element parent = (Element)hostElement;
-			Element child = (Element)movedView.getElement();
-			cmd.add(new MoveElementCommand(domain, containmentHelper, parent, child));
-			cmd.add(new AddCommand(domain, new EObjectAdapter(hostView), new EObjectAdapter(movedView)));
-			containmentHelper.deleteIncomingContainmentLinksFor(cmd, movedView);
-			return new ICommandProxy(cmd);
-		} else if(ContainmentHelper.hasOutgoingContainmentLink(movedView)) {
-			// move contained element into a correct place
-			return UnexecutableCommand.INSTANCE;
-		}
-		return null;
-	}
-
-	private boolean isMoveToChild(View hostView, View movedElementView) {
-		return EcoreUtil.isAncestor(movedElementView.getElement(), hostView.getElement());
-	}
-
-	private boolean isMoveToParent(View hostView, View movedElementView) {
-		return hostView.getElement().equals(movedElementView.getElement().eContainer());
-		//		return EcoreUtil.isAncestor(hostView.getElement(), movedElementView.getElement());
-	}
-	
-	private static class MoveElementCommand extends AbstractTransactionalCommand {
-
-		private final ContainmentHelper myHelper;
-
-		private final Element myTarget;
-
-		private final Element myMoved;
-
-		public MoveElementCommand(TransactionalEditingDomain domain, ContainmentHelper helper, Element target, Element moved) {
-			super(domain, "Move Element", Collections.emptyList());
-			myHelper = helper;
-			myMoved = moved;
-			myTarget = target;
-		}
-
-		@Override
-		protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
-			myHelper.move(myMoved, myTarget);
-			return CommandResult.newOKCommandResult();
-		}
 	}
 
 }
