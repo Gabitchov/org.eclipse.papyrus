60bbea114013ca46445e1aaa12afaa20eca16505
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-08-16 10:55:39 +0000

    RESOLVED - bug 317938: [Layout] Alignment doesn't work on Port https://bugs.eclipse.org/bugs/show_bug.cgi?id=317938

--- plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/editpolicies/AffixedNodeAlignmentEditPolicy.java
+++ plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/editpolicies/AffixedNodeAlignmentEditPolicy.java
@@ -19,14 +19,14 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.CompartmentEditPart;
 
 /**
  * 
- * This editpolicy provides the command for the alignment.
+ * This EditPolicy provides the command for the alignment.
  * In the case of AffixedNode, the parent is not a {@link CompartmentEditPart}, but directly the object.
  * For example, a port is owned by a ClassCompositeEditPart and not by a ClassCompositeCompartmentEditPart
  */
 public class AffixedNodeAlignmentEditPolicy extends ConstrainedItemBorderLayoutEditPolicy {
 
 	/** the key for the Port Alignment EditPolicy */
-	public static final String PORT_ALIGNMENT_ROLE = "affixed node alignment editpolicy"; //$NON-NLS-1$
+	public static final String AFFIXED_CHILD_ALIGNMENT_ROLE = "affixed node alignment editpolicy"; //$NON-NLS-1$
 
 	/**
 	 * Creates command for <tt>REQ_ALIGN_CHILDREN</tt> requests only; all others requests are ignored
