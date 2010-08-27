9c0d3f7c64daa81d502daf7b9607d7eca53b96dc
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-08-27 10:06:08 +0000

    Display changed tagged values as children of changed Element, not the root

--- extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/PapyrusDiffEngine.java
+++ extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/PapyrusDiffEngine.java
@@ -1,22 +1,19 @@
 package org.eclipse.papyrus.compare;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.emf.compare.FactoryException;
 import org.eclipse.emf.compare.diff.engine.GenericDiffEngine;
-import org.eclipse.emf.compare.diff.engine.check.AttributesCheck;
 import org.eclipse.emf.compare.diff.engine.check.ReferencesCheck;
-import org.eclipse.emf.compare.diff.metamodel.DiffFactory;
 import org.eclipse.emf.compare.diff.metamodel.DiffGroup;
-import org.eclipse.emf.compare.diff.metamodel.UpdateContainmentFeature;
 import org.eclipse.emf.compare.match.metamodel.Match2Elements;
-import org.eclipse.emf.compare.match.metamodel.Match3Elements;
+import org.eclipse.emf.compare.match.metamodel.MatchElement;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.impl.DynamicEObjectImpl;
 import org.eclipse.uml2.uml.Element;
-import org.eclipse.uml2.uml.util.UMLUtil.StereotypeApplicationHelper;
 
 
 public class PapyrusDiffEngine extends GenericDiffEngine {
@@ -37,15 +34,17 @@ public class PapyrusDiffEngine extends GenericDiffEngine {
 		// temporary hack in order to avoid displaying of StereotypeApplication as a child of resource  
 		if (false == match.getLeftElement() instanceof DynamicEObjectImpl) {
 			super.checkForDiffs(current, match);
-		}
+		} 
 		checkStereotypeApplication(current, match);
 	}
 	
-	private void checkStereotypeApplication(DiffGroup current, Match2Elements matchElement) {
-		final EObject leftElement = matchElement.getLeftElement();
-		final EObject rightElement = matchElement.getRightElement();
-		for (EObject stereotypeApp: getStereotypeApplications(leftElement)) {
-			createUpdateStereotypeApplication(current, leftElement, stereotypeApp, rightElement);
+	private void checkStereotypeApplication(DiffGroup current, Match2Elements matchElement) throws FactoryException {
+		final Iterator<MatchElement> it = matchElement.getSubMatchElements().iterator();
+		while (it.hasNext()) {
+			final Match2Elements element = (Match2Elements)it.next();
+			if (element.getLeftElement() instanceof DynamicEObjectImpl) {
+				super.checkForDiffs(current, element);
+			}
 		}
 	}
 	
@@ -58,15 +57,4 @@ public class PapyrusDiffEngine extends GenericDiffEngine {
 	}
 
 	
-	private void createUpdateStereotypeApplication(DiffGroup root, EObject leftElement, EObject leftProfileApp, EObject right) {
-		final UpdateContainmentFeature updateContainment = DiffFactory.eINSTANCE
-				.createUpdateContainmentFeature();
-		updateContainment.setLeftElement(leftProfileApp);
-		updateContainment.setRightElement(right);
-		updateContainment.setRightTarget(getMatchedEObject(leftElement));
-		updateContainment.setLeftTarget(getMatchedEObject(right.eContainer()));
-		root.getSubDiffElements().add(updateContainment);
-	}
-
-
 }
