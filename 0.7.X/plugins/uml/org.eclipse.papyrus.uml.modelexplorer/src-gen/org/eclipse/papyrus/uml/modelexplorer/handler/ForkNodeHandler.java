db9831b73037893f58c0dad8bcdd93803e934223
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-09-10 14:25:35 +0000

    NEW - bug 324966: [Element Edit Service] Centralized element edit service provide command for model element deletion, creation, modification ... https://bugs.eclipse.org/bugs/show_bug.cgi?id=324966

---
+++ 0.7.X/plugins/uml/org.eclipse.papyrus.uml.modelexplorer/src-gen/org/eclipse/papyrus/uml/modelexplorer/handler/ForkNodeHandler.java
@@ -0,0 +1,30 @@
+package org.eclipse.papyrus.uml.modelexplorer.handler;
+
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import org.eclipse.papyrus.uml.service.types.element.UMLElementTypes;
+import org.eclipse.papyrus.uml.modelexplorer.handler.CreateCommandHandler;
+
+/**
+ * <pre>
+ * Command handler for ForkNode creation
+ *
+ * </pre>
+ * @generated
+ */
+public class ForkNodeHandler extends CreateCommandHandler implements IHandler {
+
+	/**
+	 * <pre>
+	 * @see org.eclipse.papyrus.uml.service.creation.handler.CreateHandler#getElementTypeToCreate()
+	 *
+	 * @return the IElementType this handler is supposed to create
+	 *
+	 * </pre>
+	 * @generated
+	 */
+	protected IElementType getElementTypeToCreate() {
+		return UMLElementTypes.FORK_NODE;
+	}
+}
+
