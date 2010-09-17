3cedf16bdd95224d7afe75f6141888cf89e4a511
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-09-17 12:48:35 +0000

    display element contents for added/removed elements

--- extraplugins/compare/org.eclipse.papyrus.compare.report/src/org/eclipse/papyrus/compare/report/services/UmlElementService.java
+++ extraplugins/compare/org.eclipse.papyrus.compare.report/src/org/eclipse/papyrus/compare/report/services/UmlElementService.java
@@ -1,6 +1,10 @@
 package org.eclipse.papyrus.compare.report.services;
 
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 import org.eclipse.uml2.uml.edit.providers.UMLItemProviderAdapterFactory;
 
@@ -11,5 +15,38 @@ public class UmlElementService {
 	public String getText(EObject eobject) {
 		return oursAdapterLabelProvider.getText(eobject);
 	}
+	
+	public String getContents(EObject eObject) {
+		EClass eClass = eObject.eClass();
+		StringBuffer result = new StringBuffer();
+        for (int i = 0, size = eClass.getFeatureCount(); i < size; ++i)
+        {
+          EStructuralFeature eStructuralFeature = eClass.getEStructuralFeature(i);
+          if (eStructuralFeature.isChangeable() && !eStructuralFeature.isDerived())
+          {
+            if (eStructuralFeature instanceof EAttribute)
+            {
+             EAttribute attr = (EAttribute)eStructuralFeature;
+              result.append(attr.getName());
+              result.append('=');
+              result.append(eObject.eGet(attr));
+              result.append("</br>");
+            }
+            else
+            {
+              EReference eReference = (EReference)eStructuralFeature;
+              if (eReference.isContainment())
+              {
+                  result.append(eReference.getName());
+                  result.append('=');
+                  result.append(eObject.eGet(eReference));
+                  result.append("</br>");
+              }
+            }
+          }
+        }
+		return result.toString();
+	}
+	
 
 }
