68e421208dc4337682bc7e5cf1057a09b2bda517
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-09-30 07:52:05 +0000

    NEW - bug 326639: [Usability] Make Same Size Action In Toolbar https://bugs.eclipse.org/bugs/show_bug.cgi?id=326639

---
+++ 0.7.X/plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/actions/handlers/DropDownToolBarSameSizeHandler.java
@@ -0,0 +1,54 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.diagram.common.actions.handlers;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.papyrus.diagram.common.actions.SizeAction;
+import org.eclipse.papyrus.diagram.common.handlers.GraphicalCommandHandler;
+
+
+/**
+ * 
+ * The default action for the Size ToolBar is the Make Same Size Action
+ * 
+ */
+public class DropDownToolBarSameSizeHandler extends GraphicalCommandHandler {
+
+	/**
+	 * 
+	 * @see org.eclipse.papyrus.diagram.common.handlers.GraphicalCommandHandler#getCommand()
+	 * 
+	 * @return
+	 * @throws ExecutionException
+	 */
+	@Override
+	protected Command getCommand() throws ExecutionException {
+		SizeAction action = new SizeAction(SizeAction.PARAMETER_BOTH, getSelectedElements());
+		return action.getCommand();
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.core.commands.AbstractHandler#isEnabled()
+	 * 
+	 * @return always true
+	 */
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+
+}
