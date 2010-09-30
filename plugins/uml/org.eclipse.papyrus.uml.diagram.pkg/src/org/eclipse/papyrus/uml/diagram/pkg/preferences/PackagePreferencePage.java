015d3469bfa0089a5d25855160e7c5e55c97edba
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-09-30 08:17:45 +0000

    NEW - bug 326643: [Package Diagram] [Preferences] Adapt the Package Diagram to add the Compartments Preferences https://bugs.eclipse.org/bugs/show_bug.cgi?id=326643

--- plugins/uml/org.eclipse.papyrus.uml.diagram.pkg/src/org/eclipse/papyrus/uml/diagram/pkg/preferences/PackagePreferencePage.java
+++ plugins/uml/org.eclipse.papyrus.uml.diagram.pkg/src/org/eclipse/papyrus/uml/diagram/pkg/preferences/PackagePreferencePage.java
@@ -9,12 +9,52 @@
  *****************************************************************************/
 package org.eclipse.papyrus.uml.diagram.pkg.preferences;
 
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.papyrus.diagram.clazz.part.Messages;
+import org.eclipse.papyrus.preferences.utils.PreferenceConstantHelper;
 import org.eclipse.papyrus.uml.diagram.pkg.edit.part.PackageDiagramEditPart;
 
 public class PackagePreferencePage extends PackageDiagramNodePreferencePage {
 
+	/**
+	 * the list of the compartments for this node
+	 */
+	public static final String compartments[] = { Messages.PackagePackageableElementCompartmentEditPart_title };;
+
+	/**
+	 * 
+	 * Constructor.
+	 * 
+	 */
 	public PackagePreferencePage() {
 		super();
 		setPreferenceKey(PackageDiagramEditPart.DIAGRAM_ID + "_Package"); //$NON-NLS-1$
+	}
+
+	/**
+	 * 
+	 * @param store
+	 */
+	public static void initDefaults(IPreferenceStore store) {
+
+		String key = PackageDiagramEditPart.DIAGRAM_ID + "_Package";
+		// set the true value for the compartment visibility
+		for(String name : compartments) {
+			String preferenceName = PreferenceConstantHelper.getCompartmentElementConstant(key, name, PreferenceConstantHelper.COMPARTMENT_VISIBILITY);
+			store.setDefault(preferenceName, true);
+		}
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.papyrus.preferences.pages.AbstractPapyrusNodePreferencePage#initializeCompartmentsList()
+	 * 
+	 */
+	@Override
+	protected void initializeCompartmentsList() {
+		for(String name : compartments) {
+			this.compartmentsList.add(name);
+		}
 	}
 }
