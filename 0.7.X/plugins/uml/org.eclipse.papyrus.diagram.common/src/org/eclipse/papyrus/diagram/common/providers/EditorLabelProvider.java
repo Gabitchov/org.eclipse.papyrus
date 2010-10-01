3bb91a3750293060db64d78a5172f65fb68dbd66
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-10-01 09:34:34 +0000

    NEW - bug 325419: [Preferences] Add the possibility to define the visibility of the compartments in the Papyrus Preferences https://bugs.eclipse.org/bugs/show_bug.cgi?id=325419
    Minor Changes : Improve the methods getImage() and getText() in EditorLabelProvider

--- 0.7.X/plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/providers/EditorLabelProvider.java
+++ 0.7.X/plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/providers/EditorLabelProvider.java
@@ -13,127 +13,53 @@
  *****************************************************************************/
 package org.eclipse.papyrus.diagram.common.providers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.gef.EditPart;
 import org.eclipse.gmf.runtime.notation.BasicCompartment;
+import org.eclipse.gmf.runtime.notation.DecorationNode;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.papyrus.diagram.common.Activator;
 import org.eclipse.papyrus.diagram.common.Messages;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.uml2.uml.Activity;
 import org.eclipse.uml2.uml.Class;
-import org.eclipse.uml2.uml.Collaboration;
-import org.eclipse.uml2.uml.Constraint;
-import org.eclipse.uml2.uml.DataType;
-import org.eclipse.uml2.uml.Enumeration;
-import org.eclipse.uml2.uml.EnumerationLiteral;
-import org.eclipse.uml2.uml.FunctionBehavior;
-import org.eclipse.uml2.uml.InformationItem;
-import org.eclipse.uml2.uml.Interaction;
-import org.eclipse.uml2.uml.Interface;
-import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.NamedElement;
-import org.eclipse.uml2.uml.OpaqueBehavior;
-import org.eclipse.uml2.uml.Operation;
-import org.eclipse.uml2.uml.Port;
-import org.eclipse.uml2.uml.Profile;
-import org.eclipse.uml2.uml.Property;
-import org.eclipse.uml2.uml.ProtocolStateMachine;
-import org.eclipse.uml2.uml.Reception;
-import org.eclipse.uml2.uml.Signal;
-import org.eclipse.uml2.uml.StateMachine;
-import org.eclipse.uml2.uml.Stereotype;
 import org.eclipse.uml2.uml.Type;
-import org.eclipse.uml2.uml.UseCase;
 
 
 /**
  * The Class EditorLabelProvider.
- * This class provides Label and Image for some UML Element
+ * This class provides Label and Image for UML Element
+ * 
  */
 public class EditorLabelProvider implements ILabelProvider {
 
+	/**
+	 * We store the next index for the UML Element, which are not NamedElement
+	 * Key is a String representing the type of Element
+	 */
+	private Map<String, Integer> index = new HashMap<String, Integer>();
+
+
+	/** the plugin where owning the icons for the UML Element */
+	public static final String pluginID = "org.eclipse.uml2.uml.edit"; //$NON-NLS-1$
+
+	/** folder where are the UML Icon */
+	public static final String imageFolder = "/icons/full/obj16/"; //$NON-NLS-1$
+
+	/** Image returned when the image was not found */
+	public static final Image imageNotFound = Activator.getPluginIconImage(pluginID, "notFound.gif");
+
 	/** icon for metaclass */
-	public static final String ICON_METACLASS = "/icons/Metaclass.gif";//$NON-NLS-1$
-
-	/** icon for a class */
-	public static final String ICON_CLASS = "/icons/Class.gif"; //$NON-NLS-1$
-
-	/** icon for an Enumeration */
-	public static final String ICON_ENUMERATION = "/icons/Enumeration.gif"; //$NON-NLS-1$
-
-	/** icon for an EnumerationLiteral */
-	public static final String ICON_ENUMERATION_LITERAL = "/icons/EnumerationLiteral.gif"; //$NON-NLS-1$
-
-	/** icon for a DataType */
-	public static final String ICON_DATATYPE = "/icons/DataType.gif"; //$NON-NLS-1$
-
-	/** icon for a stereotype */
-	public static final String ICON_STEREOTYPE = "/icons/Stereotype.gif"; //$NON-NLS-1$
-
-	/** icon for a profile */
-	public static final String ICON_PROFILE = "/icons/Profile.gif"; //$NON-NLS-1$
-
-	/** icon for a model */
-	public static final String ICON_MODEL = "/icons/obj16/Model.gif"; //$NON-NLS-1$
-
-	/** icon for a package */
-	public static final String ICON_PACKAGE = "/icons/Package.gif"; //$NON-NLS-1$
-
-	/** icon for a string */
-	public static final String ICON_STRING = "icons/obj16/LiteralString.gif"; //$NON-NLS-1$
+	public static final String ICON_METACLASS = "/icons/Metaclass.gif";//$NON-NLS-1$ 
 
 	/** icon for a compartment */
 	public static final String ICON_COMPARTMENT = "/icons/none_comp_vis.gif"; //$NON-NLS-1$
 
-	/** icon for an operation */
-	public static final String ICON_OPERATION = "/icons/Operation.gif"; //$NON-NLS-1$
-
-	/** icon for a property */
-	public static final String ICON_PROPERTY = "/icons/Property.gif"; //$NON-NLS-1$
-
-	/** icon for an interface */
-	public static final String ICON_INTERFACE = "/icons/Interface.gif"; //$NON-NLS-1$
-
-	/** icon for an interface */
-	public static final String ICON_RECEPTION = "/icons/obj16/Reception.gif"; //$NON-NLS-1$
-
-	/** icon for a port */
-	public static final String ICON_PORT = "/icons/Port.gif"; //$NON-NLS-1$
-
-	/** icon for an activity */
-	public static final String ICON_ACTIVITY = "/icons/Activity.gif"; //$NON-NLS-1$
-
-	/** icon for a collaboration */
-	public static final String ICON_COLLABORATION = "/icons/Collaboration.gif"; //$NON-NLS-1$
-
-	/** icon for function behavior */
-	public static final String ICON_FUNCTION_BEHAVIOR = "/icons/obj16/FunctionBehavior.gif"; //$NON-NLS-1$
-
-	/** icon for an information item */
-	public static final String ICON_INFORMATION_ITEM = "/icons/obj16/InformationItem.gif"; //$NON-NLS-1$
-
-	/** icon for an interaction */
-	public static final String ICON_INTERACTION = "/icons/obj16/Interaction.gif"; //$NON-NLS-1$
-
-	/** icon for an opaque behavior */
-	public static final String ICON_OPAQUE_BEHAVIOR = "/icons/obj16/OpaqueBehavior.gif"; //$NON-NLS-1$
-
-	/** icon for a protocol state machine */
-	public static final String ICON_PROTOCOL_STATE_MACHINE = "/icons/obj16/ProtocolStateMachine.gif"; //$NON-NLS-1$
-
-	/** icon for a state machine */
-	public static final String ICON_STATE_MACHINE = "/icons/obj16/StateMachine.gif"; //$NON-NLS-1$
-
-	/** icon for a constraint */
-	public static final String ICON_CONSTRAINT = "/icons/obj16/Constraint.gif"; //$NON-NLS-1$
-
-	/** icon for a constraint */
-	public static final String ICON_SIGNAL = "/icons/obj16/Signal.gif"; //$NON-NLS-1$
-
-	/** icon for a constraint */
-	public static final String ICON_USECASE = "/icons/obj16/Usecase.gif"; //$NON-NLS-1$
 
 	/**
 	 * 
@@ -184,70 +110,46 @@ public class EditorLabelProvider implements ILabelProvider {
 	 * 
 	 * @param element
 	 * @return
+	 *         <ul>
+	 *         <li>if stereotypes are applied on the elements : return the image corresponding to the first applied stereotype</li>
+	 *         <li>if the element is a MetaClass return the image representing a metaclass</li>
+	 *         <li>if the element is a {@link DecorationNode}, returns the image corresponding to a compartment</li>
+	 *         <li> <code>null</code> if no image was found</li>
+	 *         </ul>
 	 */
 	public Image getImage(Object element) {
 		if(element instanceof EditPart) {
 			element = ((View)((EditPart)element).getModel()).getElement();
 		}
 
-
-		if(element instanceof UseCase) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_USECASE);
-		} else if(element instanceof Signal) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_SIGNAL);
-		} else if(element instanceof Enumeration) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_ENUMERATION);
-		} else if(element instanceof EnumerationLiteral) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_ENUMERATION_LITERAL);
-		} else if(element instanceof Stereotype) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_STEREOTYPE);
-		} else if(element instanceof Interface) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_INTERFACE);
-		} else if(element instanceof Interaction) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_INTERACTION);
-		} else if(element instanceof Constraint) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_CONSTRAINT);
-		} else if(element instanceof Activity) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_ACTIVITY);
-		} else if(element instanceof Collaboration) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_COLLABORATION);
-		} else if(element instanceof InformationItem) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_INFORMATION_ITEM);
-		} else if(element instanceof FunctionBehavior) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_FUNCTION_BEHAVIOR);
-		} else if(element instanceof OpaqueBehavior) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_OPAQUE_BEHAVIOR);
-		} else if(element instanceof ProtocolStateMachine) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_PROTOCOL_STATE_MACHINE);
-		} else if(element instanceof StateMachine) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_STATE_MACHINE);
-		} else if(element instanceof Class) {
+		//test for Metaclass
+		if(element instanceof Class) {
 			if(org.eclipse.papyrus.diagram.common.util.Util.isMetaclass((Type)element)) {
 				return Activator.getPluginIconImage(Activator.ID, ICON_METACLASS);
-			} else {
-				return Activator.getPluginIconImage(Activator.ID, ICON_CLASS);
 			}
-		} else if(element instanceof DataType) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_DATATYPE);
-		} else if(element instanceof Profile) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_PROFILE);
-		} else if(element instanceof Model) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_MODEL);
-		} else if(element instanceof Package) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_PACKAGE);
-		} else if(element instanceof BasicCompartment) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_COMPARTMENT);
-		} else if(element instanceof Reception) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_RECEPTION);
-		} else if(element instanceof Port) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_PORT);
-		} else if(element instanceof Property) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_PROPERTY);
-		} else if(element instanceof Operation) {
-			return Activator.getPluginIconImage(Activator.ID, ICON_OPERATION);
-		} else if(element instanceof BasicCompartment) {
-			Activator.getPluginIconImage(Activator.ID, ICON_COMPARTMENT);
 		}
+
+		//test for other UML Elements
+		if(element instanceof Element) {
+			//return the stereotype image if a stereotype is applied on the element
+			Image im = Activator.getIconElement((Element)element);
+			String imagePath = new String(imageFolder);
+			if(im == null) {
+				imagePath += element.getClass().getSimpleName() + ".gif"; //$NON-NLS-1$
+				imagePath = imagePath.replace("Impl", ""); //$NON-NLS-1$ //$NON-NLS-2$
+				im = Activator.getPluginIconImage(pluginID, imagePath);
+			}
+			if(im.equals(imageNotFound)) {
+				return null;
+			}
+			return im;
+		}
+
+		//if the element is a compartment
+		if(element instanceof BasicCompartment || element instanceof DecorationNode) {
+			return Activator.getPluginIconImage(Activator.ID, ICON_COMPARTMENT);
+		}
+
 		return null;
 	}
 
@@ -257,6 +159,11 @@ public class EditorLabelProvider implements ILabelProvider {
 	 * 
 	 * @param element
 	 * @return
+	 *         <ul>
+	 *         <li>if element is a {@link NamedElement}, we return its name</li>
+	 *         <li>else if element is a {@link Element}, we return its type + a index</li>
+	 *         <li>else return {@link Messages#EditorLabelProvider_No_name}</li>
+	 *         </ul>
 	 */
 	public String getText(Object element) {
 		if(element instanceof EditPart) {
@@ -264,7 +171,20 @@ public class EditorLabelProvider implements ILabelProvider {
 		}
 		if(element instanceof NamedElement) {
 			return ((NamedElement)element).getName();
+		} else if(element instanceof Element) {
+			//when the element is not a NamedElement, we return its Type + a index
+			String className = element.getClass().getName();
+			int i = className.lastIndexOf(".");
+			className = className.substring(i + 1);
+			className = className.replace("Impl", "");
+			Integer number = index.get(className);
+			if(number == null) {
+				number = 0;
+			}
+
+			index.put(className, number + 1);
+			return className + " " + number;
 		}
-		return Messages.ShowHideAction_No_Name;
+		return Messages.EditorLabelProvider_No_name;
 	}
 }
