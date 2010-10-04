2f0d7eaa99295cdc7ca6e8e5adb301c32925476b
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-10-04 15:13:51 +0000

    Add ExportAsHTML action to export menu

--- extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/action/ExportAsHTMLWizard.java
+++ extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/action/ExportAsHTMLWizard.java
@@ -1,33 +1,19 @@
 package org.eclipse.papyrus.compare.action;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.compare.diff.metamodel.ComparisonSnapshot;
 import org.eclipse.emf.compare.ui.internal.wizard.SaveDeltaWizard;
-import org.eclipse.emf.compare.util.ModelUtils;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.papyrus.compare.report.ui.common.GenerateAll;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
-import org.eclipse.ui.internal.wizards.newresource.ResourceMessages;
-import org.eclipse.ui.wizards.newresource.BasicNewFileResourceWizard;
 
 
 public class ExportAsHTMLWizard extends SaveDeltaWizard {
@@ -64,13 +50,10 @@ public class ExportAsHTMLWizard extends SaveDeltaWizard {
 		if (!result) {
 			return false;
 		}
-		final String page = "newFilePage1"; //$NON-NLS-1$
-		WizardNewFileCreationPage saveDiffPage = ((WizardNewFileCreationPage)getPage(page));
-
-		IPath filePath = saveDiffPage.getContainerFullPath().append(saveDiffPage.getFileName());
-		IFile fileHandle = IDEWorkbenchPlugin.getPluginWorkspace().getRoot().getFile(filePath);
-		URI uri = URI.createFileURI(fileHandle.getLocation().toOSString());
-		GenerateAll generator = new GenerateAll(uri, fileHandle.getLocation().toFile().getParentFile(), getArguments());
+		
+		URI uri = getSavedDiffModelURI();
+		
+		GenerateAll generator = new GenerateAll(uri, myNewReportFileCreationPage.getContainerFullPath().toFile(), getArguments());
 		try {
 			generator.doGenerate(new NullProgressMonitor());
 		} catch (IOException e) {
@@ -83,6 +66,17 @@ public class ExportAsHTMLWizard extends SaveDeltaWizard {
 	protected List<? extends Object> getArguments() {
 		return new ArrayList<String>();
 	}
-
+	
+	private WizardNewFileCreationPage getSaveDiffPage() {
+		final String page = "newFilePage1"; //$NON-NLS-1$
+		return (WizardNewFileCreationPage)getPage(page);
+	}
+	
+	private URI getSavedDiffModelURI() {
+		WizardNewFileCreationPage saveDiffPage = getSaveDiffPage();
+		IPath filePath = saveDiffPage.getContainerFullPath().append(saveDiffPage.getFileName());
+		IFile fileHandle = IDEWorkbenchPlugin.getPluginWorkspace().getRoot().getFile(filePath);
+		return URI.createFileURI(fileHandle.getLocation().toOSString());
+	}
 
 }
