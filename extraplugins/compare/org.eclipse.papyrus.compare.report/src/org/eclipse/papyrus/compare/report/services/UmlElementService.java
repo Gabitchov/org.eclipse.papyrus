8fb2d7fe7d2d23584e1475489b81ca98be198009
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-10-06 12:27:39 +0000

    Propose a proper file name when creating report using the wizard

--- extraplugins/compare/org.eclipse.papyrus.compare.report/src/org/eclipse/papyrus/compare/report/services/UmlElementService.java
+++ extraplugins/compare/org.eclipse.papyrus.compare.report/src/org/eclipse/papyrus/compare/report/services/UmlElementService.java
@@ -2,6 +2,8 @@ package org.eclipse.papyrus.compare.report.services;
 
 import java.util.List;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.compare.diff.metamodel.DiffModel;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
@@ -17,9 +19,21 @@ public class UmlElementService {
 	private static AdapterFactoryLabelProvider oursAdapterLabelProvider = new AdapterFactoryLabelProvider(
 			new UMLItemProviderAdapterFactory());
 
+	public String getFileName(DiffModel model) {
+		return getFirstElementName(model.getLeftRoots()) + "_diff_" + getFirstElementName(model.getRightRoots());
+	}
+
+	private String getFirstElementName(EList<EObject> children) {
+		if (children.isEmpty()) {
+			return "";
+		}
+		return getElementName(children.get(0));
+	}
+	
 	public String getText(EObject eobject) {
 		return oursAdapterLabelProvider.getText(eobject);
 	}
+	
 
 	public String getContents(EObject eObject) {
 		EClass eClass = eObject.eClass();
