28eaf439b18f02ffbea14f8e83c8906464ffe820
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-10-17 21:02:14 +0000

    NEW - bug 327999: [Element Edit Service] Provide re-orient command support in ElementTypes EditHelpers https://bugs.eclipse.org/bugs/show_bug.cgi?id=327999

---
+++ 0.7.X/plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/command/GeneralizationReorientCommand.java
@@ -0,0 +1,169 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Yann Tanguy (CEA LIST) yann.tanguy@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.uml.service.types.command;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ReorientRelationshipRequest;
+import org.eclipse.uml2.uml.Classifier;
+import org.eclipse.uml2.uml.Generalization;
+
+/**
+ * <pre>
+ * This re-orient command is adapted from re-orient command generated
+ * for {@link Generalization} in the Composite Structure Diagram.
+ * </pre>
+ * 
+ * @generated
+ */
+public class GeneralizationReorientCommand extends EditElementCommand {
+
+	/**
+	 * @generated
+	 */
+	private final int reorientDirection;
+
+	/**
+	 * @generated
+	 */
+	private final EObject oldEnd;
+
+	/**
+	 * @generated
+	 */
+	private final EObject newEnd;
+
+	/**
+	 * @generated
+	 */
+	public GeneralizationReorientCommand(ReorientRelationshipRequest request) {
+		super(request.getLabel(), request.getRelationship(), request);
+		reorientDirection = request.getDirection();
+		oldEnd = request.getOldRelationshipEnd();
+		newEnd = request.getNewRelationshipEnd();
+	}
+
+	/**
+	 * @generated
+	 */
+	public boolean canExecute() {
+		if(false == getElementToEdit() instanceof Generalization) {
+			return false;
+		}
+		if(reorientDirection == ReorientRelationshipRequest.REORIENT_SOURCE) {
+			return canReorientSource();
+		}
+		if(reorientDirection == ReorientRelationshipRequest.REORIENT_TARGET) {
+			return canReorientTarget();
+		}
+		return false;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected boolean canReorientSource() {
+		if(!(oldEnd instanceof Classifier && newEnd instanceof Classifier)) {
+			return false;
+		}
+		if(!(getLink().eContainer() instanceof Classifier)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected boolean canReorientTarget() {
+		if(!(oldEnd instanceof Classifier && newEnd instanceof Classifier)) {
+			return false;
+		}
+		if(!(getLink().eContainer() instanceof Classifier)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		if(!canExecute()) {
+			throw new ExecutionException("Invalid arguments in reorient link command"); //$NON-NLS-1$
+		}
+		if(reorientDirection == ReorientRelationshipRequest.REORIENT_SOURCE) {
+			return reorientSource();
+		}
+		if(reorientDirection == ReorientRelationshipRequest.REORIENT_TARGET) {
+			return reorientTarget();
+		}
+		throw new IllegalStateException();
+	}
+
+	/**
+	 * @generated
+	 */
+	protected CommandResult reorientSource() throws ExecutionException {
+		getLink().setSpecific(getNewSource());
+		return CommandResult.newOKCommandResult(getLink());
+	}
+
+	/**
+	 * @generated
+	 */
+	protected CommandResult reorientTarget() throws ExecutionException {
+		getLink().setGeneral(getNewTarget());
+		return CommandResult.newOKCommandResult(getLink());
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Generalization getLink() {
+		return (Generalization)getElementToEdit();
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Classifier getOldSource() {
+		return (Classifier)oldEnd;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Classifier getNewSource() {
+		return (Classifier)newEnd;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Classifier getOldTarget() {
+		return (Classifier)oldEnd;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Classifier getNewTarget() {
+		return (Classifier)newEnd;
+	}
+}
