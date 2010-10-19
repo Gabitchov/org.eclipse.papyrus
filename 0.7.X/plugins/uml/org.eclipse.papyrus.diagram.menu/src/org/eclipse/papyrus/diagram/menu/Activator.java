2302ee6eda1fb8f05232d095556736f322982b9a
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-10-19 10:12:48 +0000

    NEW - bug 327438: [Menu Diagram] Initial State for the actions https://bugs.eclipse.org/bugs/show_bug.cgi?id=327438

--- 0.7.X/plugins/uml/org.eclipse.papyrus.diagram.menu/src/org/eclipse/papyrus/diagram/menu/Activator.java
+++ 0.7.X/plugins/uml/org.eclipse.papyrus.diagram.menu/src/org/eclipse/papyrus/diagram/menu/Activator.java
@@ -6,33 +6,37 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin implements org.eclipse.ui.IStartup {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.papyrus.diagram.menu"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
-		plugin=this;
 	}
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
+		plugin = this;
 		super.start(context);
 	}
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
@@ -40,11 +44,20 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
 		return plugin;
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.ui.IStartup#earlyStartup()
+	 * 
+	 */
+	public void earlyStartup() {
+		// TODO Auto-generated method stub
 	}
 
 }
