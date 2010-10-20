8db830b0c471b9068a3f2986e3b881a5bc2a6ad0
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-10-20 22:25:36 +0000

    NEW - bug 325419: [Preferences] Add the possibility to define the visibility of the compartments in the Papyrus Preferences https://bugs.eclipse.org/bugs/show_bug.cgi?id=325419

--- 0.7.X/plugins/sysml/org.eclipse.papyrus.sysml.diagram.internalblock/src/org/eclipse/papyrus/sysml/diagram/internalblock/preferences/PropertyPreferencePage.java
+++ 0.7.X/plugins/sysml/org.eclipse.papyrus.sysml.diagram.internalblock/src/org/eclipse/papyrus/sysml/diagram/internalblock/preferences/PropertyPreferencePage.java
@@ -9,12 +9,49 @@
  *****************************************************************************/
 package org.eclipse.papyrus.sysml.diagram.internalblock.preferences;
 
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.papyrus.diagram.composite.part.Messages;
+import org.eclipse.papyrus.preferences.utils.PreferenceConstantHelper;
 import org.eclipse.papyrus.sysml.diagram.internalblock.edit.part.InternalBlockDiagramEditPart;
 
 public class PropertyPreferencePage extends InternalBlockDiagramNodePreferencePage {
 
+	/**
+	 * the list of the compartments for this node
+	 */
+	public static final String compartments[] = { Messages.PropertyPartCompartmentEditPartCN_title };;
+
 	public PropertyPreferencePage() {
 		super();
 		setPreferenceKey(InternalBlockDiagramEditPart.DIAGRAM_ID + "_Property"); //$NON-NLS-1$
+	}
+	
+	/**
+	 * 
+	 * @param store
+	 */
+	public static void initDefaults(IPreferenceStore store) {
+
+		String key = InternalBlockDiagramEditPart.DIAGRAM_ID + "_Property";
+		// set the true value for the compartment visibility
+		for(String name : compartments) {
+			String preferenceName = PreferenceConstantHelper.getCompartmentElementConstant(key, name, PreferenceConstantHelper.COMPARTMENT_VISIBILITY);
+			store.setDefault(preferenceName, true);
+		}
+		
+		store.setDefault(PreferenceConstantHelper.getElementConstant(key, PreferenceConstantHelper.WIDTH), 20);
+		store.setDefault(PreferenceConstantHelper.getElementConstant(key, PreferenceConstantHelper.HEIGHT), 20);
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.papyrus.preferences.pages.AbstractPapyrusNodePreferencePage#initializeCompartmentsList()
+	 * 
+	 */
+	@Override
+	protected void initializeCompartmentsList() {
+		for(String name : compartments) {
+			this.compartmentsList.add(name);
+		}
 	}
 }
