1ba8830a33821c3b64c8a4acc27670f4f694dfcc
aradermache <aradermache@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-11-16 10:14:30 +0000

    Added copyright message

--- plugins/core/org.eclipse.papyrus.editor/src/org/eclipse/papyrus/editor/PapyrusMatchingStrategy.java
+++ plugins/core/org.eclipse.papyrus.editor/src/org/eclipse/papyrus/editor/PapyrusMatchingStrategy.java
@@ -1,4 +1,18 @@
-package org.eclipse.papyrus.editor;
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Ansgar Radermacher  ansgar.radermacher@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+
+ package org.eclipse.papyrus.editor;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IPath;
