98afa5e7af81352158b4fea4943d8f5962402fc9
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-11-23 13:37:13 +0000

    330772 - [BDD] Allow to configure visibility of name of compartments in the preferences pages

--- plugins/core/org.eclipse.papyrus.preferences/src/org/eclipse/papyrus/preferences/ui/NodeCompartmentGroup.java
+++ plugins/core/org.eclipse.papyrus.preferences/src/org/eclipse/papyrus/preferences/ui/NodeCompartmentGroup.java
@@ -16,12 +16,15 @@ package org.eclipse.papyrus.preferences.ui;
 
 import java.util.List;
 
+import org.eclipse.gmf.runtime.common.ui.preferences.CheckBoxFieldEditor;
 import org.eclipse.jface.dialogs.DialogPage;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.papyrus.preferences.Messages;
 import org.eclipse.papyrus.preferences.utils.PreferenceConstantHelper;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
 
 /**
@@ -56,38 +59,63 @@ public class NodeCompartmentGroup extends AbstractGroup {
 	 */
 	protected void createContent(Composite parent) {
 		addCompartmentVisibilityGroup(parent);
-		addCompartmentNameVisibilityGroup(parent);
 	}
 
 	private void addCompartmentVisibilityGroup(Composite parent) {
-		if(!compartmentsName.isEmpty()) {
-			Group compartmentGroup = new Group(parent, 2);
-			compartmentGroup.setLayout(new GridLayout());
-			compartmentGroup.setText(Messages.NodeCompartmentGroup_Compartiments);
-			for(String name : compartmentsName) {
-				String compartmentVisibilityPreference = PreferenceConstantHelper.getCompartmentElementConstant(getKey(), name, PreferenceConstantHelper.COMPARTMENT_VISIBILITY);
-				BooleanFieldEditor compartmentVisibilityBooleanFieldEditor = new BooleanFieldEditor(compartmentVisibilityPreference, name, compartmentGroup);
-				compartmentVisibilityBooleanFieldEditor.setPage(this.dialogPage);
-				addFieldEditor(compartmentVisibilityBooleanFieldEditor);
-
-			}
-		}
-	}
-	
-	private void addCompartmentNameVisibilityGroup(Composite parent) {
 		if(compartmentsName.isEmpty()) {
 			return;
 		}
 		Group compartmentGroup = new Group(parent, 2);
 		compartmentGroup.setLayout(new GridLayout());
-		compartmentGroup.setText("Show names of the compartments");
+		compartmentGroup.setText(Messages.NodeCompartmentGroup_Compartiments);
 		for(String compartmentName : compartmentsName) {
-			String compartmentVisibilityPreference = PreferenceConstantHelper.getCompartmentElementConstant(getKey(), compartmentName, PreferenceConstantHelper.COMPARTMENT_NAME_VISIBILITY);
-			String label = compartmentName;
-			BooleanFieldEditor compartmentVisibilityBooleanFieldEditor = new BooleanFieldEditor(compartmentVisibilityPreference, label, compartmentGroup);
-			compartmentVisibilityBooleanFieldEditor.setPage(this.dialogPage);
-			addFieldEditor(compartmentVisibilityBooleanFieldEditor);
+			addShowCompartmentField(compartmentGroup, compartmentName);
+			addShowNameOfCompartmentField(compartmentGroup, compartmentName);
 		}
-
 	}
+
+	protected void addShowCompartmentField(Group compartmentGroup, String name) {
+		String compartmentVisibilityPreference = PreferenceConstantHelper.getCompartmentElementConstant(getKey(), name, PreferenceConstantHelper.COMPARTMENT_VISIBILITY);
+		BooleanFieldEditor compartmentVisibilityBooleanFieldEditor = new BooleanFieldEditor(compartmentVisibilityPreference, name, compartmentGroup);
+		compartmentVisibilityBooleanFieldEditor.setPage(this.dialogPage);
+		addFieldEditor(compartmentVisibilityBooleanFieldEditor);
+	}
+
+	protected void addShowNameOfCompartmentField(Group compartmentGroup, String compartmentName) {
+		String compartmentVisibilityPreference = PreferenceConstantHelper.getCompartmentElementConstant(getKey(), compartmentName, PreferenceConstantHelper.COMPARTMENT_NAME_VISIBILITY);
+		String label = "Show name";
+		CheckBoxFieldEditor compartmentVisibilityBooleanFieldEditor = new CheckBoxFieldEditor(compartmentVisibilityPreference, label, compartmentGroup);
+		indent(compartmentVisibilityBooleanFieldEditor.getCheckbox());
+		compartmentVisibilityBooleanFieldEditor.setPage(this.dialogPage);
+		addFieldEditor(compartmentVisibilityBooleanFieldEditor);
+	}
+
+	//	private void createDependency(final Button master, Preference preference, final Control[] slaves) {
+	//		indent(slaves[0]);
+	//
+	//		boolean masterState= fOverlayStore.getBoolean(preference.getKey());
+	//		for (int i= 0; i < slaves.length; i++) {
+	//			slaves[i].setEnabled(masterState);
+	//		}
+	//
+	//		SelectionListener listener= new SelectionListener() {
+	//			public void widgetSelected(SelectionEvent e) {
+	//				boolean state= master.getSelection();
+	//				for (int i= 0; i < slaves.length; i++) {
+	//					slaves[i].setEnabled(state);
+	//				}
+	//			}
+	//
+	//			public void widgetDefaultSelected(SelectionEvent e) {}
+	//		};
+	//		master.addSelectionListener(listener);
+	//		fMasterSlaveListeners.add(listener);
+	//	}
+
+	private static void indent(Control control) {
+		GridData gridData = new GridData();
+		gridData.horizontalIndent = 20;
+		control.setLayoutData(gridData);
+	}
+
 }
