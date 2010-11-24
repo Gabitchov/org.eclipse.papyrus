5e59797b46c911f391f3403144b8c1d77f838382
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-11-24 12:56:26 +0000

    [Tests] Refactor the plugin core tests to create a test fragment

---
+++ 0.7.X/tests/org.eclipse.papyrus.core.tests/src/org/eclipse/papyrus/core/tests/AllTests.java
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Remi Schnekenburger (CEA LIST) remi.schnekenburger@cea.fr - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.core.tests;
+
+import org.eclipse.papyrus.core.lifecycleevents.LifeCycleEventsProviderTest;
+import org.eclipse.papyrus.core.services.ComposedServiceTest;
+import org.eclipse.papyrus.core.services.ServicesRegistryTest;
+import org.eclipse.papyrus.resource.AbstractModelWithSharedResourceTest;
+import org.eclipse.papyrus.resource.ModelSetTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+
+@RunWith(Suite.class)
+@SuiteClasses({
+// {oep.resource}
+ModelSetTest.class, AbstractModelWithSharedResourceTest.class,
+// {oep}.core.services
+ComposedServiceTest.class, ServicesRegistryTest.class,
+// {oep}.core.lifecycleevents
+/* SaveAndDirtyServiceTest.class, */LifeCycleEventsProviderTest.class
+})
+/**
+ * Suite Class for all tests in the plugin
+ */
+public class AllTests {
+
+}
