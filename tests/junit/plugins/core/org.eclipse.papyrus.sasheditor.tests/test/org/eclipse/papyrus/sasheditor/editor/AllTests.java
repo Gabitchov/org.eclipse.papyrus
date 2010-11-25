165da9896c5f5cd0a5342e4f7dbb2289552ca90d
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-11-25 16:30:54 +0000

    [Tests] minor updates to the tests suites (adding all tests class, etc.) [Tests] Adding more test to the main test suite

---
+++ tests/junit/plugins/core/org.eclipse.papyrus.sasheditor.tests/test/org/eclipse/papyrus/sasheditor/editor/AllTests.java
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Remi Schnekenburger (CEA LIST) remi.schnekenburger@cea.fr - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.sasheditor.editor;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+/**
+ * All tests for package org.eclipse.papyrus.sasheditor.editor
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ SashMultiPageEditorTest.class })
+public class AllTests {
+
+}
