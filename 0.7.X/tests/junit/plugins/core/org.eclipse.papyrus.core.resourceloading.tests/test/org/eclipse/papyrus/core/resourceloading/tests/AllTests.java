0a564e4d690c19dce948dec43b18914cc940de4f
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-11-25 22:42:19 +0000

    [Test] Trying to invert tests in resource loading

--- 0.7.X/tests/junit/plugins/core/org.eclipse.papyrus.core.resourceloading.tests/test/org/eclipse/papyrus/core/resourceloading/tests/AllTests.java
+++ 0.7.X/tests/junit/plugins/core/org.eclipse.papyrus.core.resourceloading.tests/test/org/eclipse/papyrus/core/resourceloading/tests/AllTests.java
@@ -20,10 +20,9 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 	// testModel1
-Strategy0TestModel1.class,
 Strategy1TestModel1.class,
 Strategy2TestModel1.class,
-
+ Strategy0TestModel1.class,
 // testModel2
 Strategy0TestModel2WithModel1.class,
 Strategy0TestModel2WithPackage0.class,
