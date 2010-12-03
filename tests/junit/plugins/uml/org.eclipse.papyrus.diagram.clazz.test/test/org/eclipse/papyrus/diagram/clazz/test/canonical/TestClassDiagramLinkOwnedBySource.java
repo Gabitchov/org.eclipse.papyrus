579ca42e9ac82c2586bc97f87792c3a8270b488d
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-12-03 14:02:34 +0000

    331765: [Class diagram] it is impossible to retarget a substitution after drop it https://bugs.eclipse.org/bugs/show_bug.cgi?id=331765

---
+++ tests/junit/plugins/uml/org.eclipse.papyrus.diagram.clazz.test/test/org/eclipse/papyrus/diagram/clazz/test/canonical/TestClassDiagramLinkOwnedBySource.java
@@ -0,0 +1,73 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.diagram.clazz.test.canonical;
+
+import org.eclipse.papyrus.diagram.clazz.providers.UMLElementTypes;
+import org.junit.Test;
+
+
+/**
+ * The Class TestClassDiagramLink use to test link that are contained by the owner of the target and the source
+ */
+public class TestClassDiagramLinkOwnedBySource extends TestLinkOwnedBySource {
+
+	
+	
+	/**
+	 * Test to manage Association.
+	 */
+	@Test
+	public void testToManageGeneralization() {
+		testToManageLink(UMLElementTypes.Class_2008,UMLElementTypes.Class_2008,UMLElementTypes.Generalization_4002,UMLElementTypes.Package_2007);
+	}
+	
+	/**
+	 * Test to manage InterfaceRealization.
+	 */
+	@Test
+	public void testToManageInterfaceRealization() {
+		testToManageLink(UMLElementTypes.Class_2008,UMLElementTypes.Interface_2004,UMLElementTypes.InterfaceRealization_4003,UMLElementTypes.Package_2007);
+	}
+	
+	/**
+	 * Test to manage Substitution.
+	 */
+	@Test
+	public void testToManageSubstitution() {
+		testToManageLink(UMLElementTypes.Class_2008,UMLElementTypes.Class_2008,UMLElementTypes.Substitution_4004,UMLElementTypes.Package_2007);
+	}
+	
+	/**
+	 * Test to manage ElementImport.
+	 */
+	@Test
+	public void testToManageElementImport() {
+		testToManageLink(UMLElementTypes.Class_2008,UMLElementTypes.Class_2008,UMLElementTypes.ElementImport_4009,UMLElementTypes.Package_2007);
+	}
+	/**
+	 * Test to manage ElementImport.
+	 */
+	@Test
+	public void testToManagePackageImport() {
+		testToManageLink(UMLElementTypes.Package_2007,UMLElementTypes.Package_2007,UMLElementTypes.PackageImport_4010,UMLElementTypes.Package_2007);
+	}
+	
+	
+	//test comment link
+	//test constraintLink
+	//test template binding
+	//test containment link
+	//test instancespecificationLink
+	
+}
--- tests/junit/plugins/uml/org.eclipse.papyrus.diagram.clazz.test/test/org/eclipse/papyrus/diagram/clazz/test/canonical/TestLinkOwnedBySource.java
+++ tests/junit/plugins/uml/org.eclipse.papyrus.diagram.clazz.test/test/org/eclipse/papyrus/diagram/clazz/test/canonical/TestLinkOwnedBySource.java
@@ -1,16 +1,3 @@
-/*****************************************************************************
- * Copyright (c) 2009 CEA LIST.
- *
- *    
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *  Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
- *
- *****************************************************************************/
 package org.eclipse.papyrus.diagram.clazz.test.canonical;
 
 import java.util.ArrayList;
@@ -47,28 +34,9 @@ import org.eclipse.papyrus.diagram.common.command.wrappers.GEFtoEMFCommandWrappe
 import org.eclipse.uml2.uml.Element;
 
 
-
-// TODO: Auto-generated Javadoc
-/**
- * The Class TestTopLink to test when this is link . The link is attached to a semantic element that has to be contained by the source and the target.
- * To adapt when source owns the created link
- */
-public class TestLink extends AbstractPapyrusTestCase {
+public class TestLinkOwnedBySource extends TestLink {
 	
 	
-	
-
-	protected static final String THE_LINK_RECONNECT_EXISTS = "The Link exist";
-	protected static final String RECONNECTION_TARGET = "Reconnection of target";
-	protected static final String LINK_EXISTS_RECONNECTION_ON_TARGET = "Link exist on the target";
-	protected static final String THE_LINK_TO_RECONNECT_EXISTS = "The Link exist";
-	protected static final String RECONNECTION_SOURCE = "Reconnection of source";
-	protected static final String LINK_EXISTS_RECONNECTION_ON_SOURCE = "Link exist on the source";
-	protected GraphicalEditPart source=null;
-	protected GraphicalEditPart sourcePlayer=null;
-	protected GraphicalEditPart target=null;
-	protected GraphicalEditPart targetPlayer=null;
-
 	/**
 	 * Test view deletion.
 	 * 
@@ -77,8 +45,8 @@ public class TestLink extends AbstractPapyrusTestCase {
 	public void testViewDeletion(IElementType type) {
 		//DELETION OF THE VIEW
 		assertTrue(VIEW_DELETION +INITIALIZATION_TEST, source.getSourceConnections().size()==1);
-		assertTrue(VIEW_DELETION +INITIALIZATION_TEST, getRootSemanticModel().getOwnedElements().size()==5);
-		
+		assertTrue(VIEW_DELETION +INITIALIZATION_TEST, getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(VIEW_DELETION +INITIALIZATION_TEST, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		Request deleteViewRequest = new GroupRequest(RequestConstants.REQ_DELETE);
 		Command command = ((ConnectionEditPart)source.getSourceConnections().get(0)).getCommand(deleteViewRequest);
 		assertNotNull(VIEW_DELETION +COMMAND_NULL,command);
@@ -86,32 +54,19 @@ public class TestLink extends AbstractPapyrusTestCase {
 		assertTrue(VIEW_DELETION +TEST_IF_THE_COMMAND_CAN_BE_EXECUTED,command.canExecute()==true);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().execute(command);
 		assertTrue(VIEW_DELETION +TEST_THE_EXECUTION,source.getSourceConnections().size()==0);
-		assertTrue(VIEW_DELETION +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(VIEW_DELETION +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(VIEW_DELETION +TEST_THE_EXECUTION, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().undo();
 		assertTrue(VIEW_DELETION +TEST_THE_UNDO,source.getSourceConnections().size()==1);
-		assertTrue(VIEW_DELETION +TEST_THE_UNDO,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(VIEW_DELETION +TEST_THE_UNDO,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(VIEW_DELETION +TEST_THE_UNDO, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().redo();
 		assertTrue(VIEW_DELETION +TEST_THE_REDO,source.getSourceConnections().size()==0);
-		assertTrue(VIEW_DELETION +TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(VIEW_DELETION +TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(VIEW_DELETION +TEST_THE_REDO, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		
 	}
-	/**
-	 * Retrieves the TransactionalEditingDomain
-	 * 
-	 * @return the editing domain (can be null)
-	 */
-	protected TransactionalEditingDomain getEditingDomain() {
-		ServiceUtilsForActionHandlers serviceUtils = new ServiceUtilsForActionHandlers();
-		TransactionalEditingDomain editingDomain = null;
-		try {
-			editingDomain = serviceUtils.getTransactionalEditingDomain();
-
-		} catch (ServiceException e) {
-			Activator.log.error(e);
-		}
-
-		return editingDomain;
-	}
+	
 	/**
 	 * Test destroy.
 	 * 
@@ -122,8 +77,8 @@ public class TestLink extends AbstractPapyrusTestCase {
 
 		//DESTROY SEMANTIC+ VIEW
 		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,source.getSourceConnections().size()==1);
-		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==5);
-	
+		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		
 		Request deleteViewRequest =	new EditCommandRequestWrapper( new DestroyElementRequest(false));
 		Command command = ((ConnectionEditPart)source.getSourceConnections().get(0)).getCommand(deleteViewRequest);
@@ -131,8 +86,9 @@ public class TestLink extends AbstractPapyrusTestCase {
 		assertTrue(DESTROY_DELETION +TEST_IF_THE_COMMAND_IS_CREATED,command!=UnexecutableCommand.INSTANCE);
 		assertTrue(DESTROY_DELETION+TEST_IF_THE_COMMAND_CAN_BE_EXECUTED,command.canExecute()==true);
 		getEditingDomain().getCommandStack().execute(new GEFtoEMFCommandWrapper(command));
-		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,source.getSourceConnections().size()==0);
-		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DESTROY_DELETION +TEST_THE_EXECUTION,source.getSourceConnections().size()==0);
+		assertTrue(DESTROY_DELETION +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DESTROY_DELETION +TEST_THE_EXECUTION, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==0);
 		
 		EditingDomainUndoContext undoContext= new EditingDomainUndoContext(getEditingDomain());
 		try{
@@ -141,11 +97,13 @@ public class TestLink extends AbstractPapyrusTestCase {
 			System.err.println(e);
 		}
 		
-		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,source.getSourceConnections().size()==1);
-		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(DESTROY_DELETION +TEST_THE_UNDO,source.getSourceConnections().size()==1);
+		assertTrue(DESTROY_DELETION +TEST_THE_UNDO,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DESTROY_DELETION +TEST_THE_UNDO, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().redo();
-		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,source.getSourceConnections().size()==0);
-		assertTrue(DESTROY_DELETION +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DESTROY_DELETION +TEST_THE_REDO,source.getSourceConnections().size()==0);
+		assertTrue(DESTROY_DELETION +TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DESTROY_DELETION +TEST_THE_REDO, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==0);
 	}
 	
 	
@@ -157,14 +115,13 @@ public class TestLink extends AbstractPapyrusTestCase {
 	public void testDrop(IElementType type) {
 		//DROP
 		assertTrue(DROP +INITIALIZATION_TEST,getDiagramEditPart().getChildren().size()==4);
-		assertTrue(DROP +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==5);
-		assertTrue(CREATION +INITIALIZATION_TEST,((Diagram)getRootView()).getEdges().size()==0);
-		
-		
+		assertTrue(DROP +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DROP +INITIALIZATION_TEST, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
+		assertTrue(DROP +TEST_THE_REDO,((Diagram)getRootView()).getEdges().size()==0);
 		
 		DropObjectsRequest dropObjectsRequest= new DropObjectsRequest();
 		ArrayList<Element> list = new ArrayList<Element>();
-		list.add(getRootSemanticModel().getOwnedElements().get(4));
+		list.add(((Element)source.resolveSemanticElement()).getOwnedElements().get(0));
 		dropObjectsRequest.setObjects(list);
 		dropObjectsRequest.setLocation(new Point(20,20));
 		Command command= getDiagramEditPart().getCommand(dropObjectsRequest);
@@ -173,71 +130,23 @@ public class TestLink extends AbstractPapyrusTestCase {
 		assertTrue(DROP+TEST_IF_THE_COMMAND_CAN_BE_EXECUTED,command.canExecute()==true);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().execute(command);
 		assertTrue(DROP +TEST_THE_EXECUTION,getDiagramEditPart().getChildren().size()==4);
-		assertTrue(DROP +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(DROP +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DROP +TEST_THE_EXECUTION, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		assertTrue(DROP +TEST_THE_EXECUTION,((Diagram)getRootView()).getEdges().size()==1);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().undo();
-		assertTrue(DROP +TEST_THE_UNDO,getDiagramEditPart().getChildren().size()==4);
-		assertTrue(DROP +TEST_THE_UNDO,getRootSemanticModel().getOwnedElements().size()==5);
-		assertTrue(DROP +TEST_THE_UNDO,((Diagram)getRootView()).getEdges().size()==0);
+		assertTrue(DROP +TEST_THE_REDO,getDiagramEditPart().getChildren().size()==4);
+		assertTrue(DROP +TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DROP +TEST_THE_REDO, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
+		assertTrue(DROP +TEST_THE_REDO,((Diagram)getRootView()).getEdges().size()==0);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().redo();
 		assertTrue(DROP +TEST_THE_REDO,getDiagramEditPart().getChildren().size()==4);
-		assertTrue(DROP +TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(DROP +TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(DROP +TEST_THE_REDO, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		assertTrue(DROP +TEST_THE_REDO,((Diagram)getRootView()).getEdges().size()==1);
 		}
 	
 	
-	/**
-	 * Test change container.
-	 * 
-	 * @param type the type
-	 * @param containerType the container type
-	 */
-	public void testChangeContainer(IElementType type, IElementType containerType) {
-		//CHANGE CONTAINER
-		assertTrue(CHANGE_CONTAINER +INITIALIZATION_TEST,getDiagramEditPart().getChildren().size()==1);
-		assertTrue(CHANGE_CONTAINER +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==1);
-		
-		
-		Request requestcreation=CreateViewRequestFactory.getCreateShapeRequest(containerType, getDiagramEditPart().getDiagramPreferencesHint());
-		Command command=getDiagramEditPart().getCommand(requestcreation);
-		assertNotNull(CONTAINER_CREATION+COMMAND_NULL,command);
-		assertTrue(CONTAINER_CREATION +TEST_IF_THE_COMMAND_IS_CREATED,command!=UnexecutableCommand.INSTANCE);
-		assertTrue(CONTAINER_CREATION+TEST_IF_THE_COMMAND_CAN_BE_EXECUTED,command.canExecute()==true);
-		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().execute(command);
-		assertTrue(CONTAINER_CREATION+TEST_THE_EXECUTION,getRootView().getChildren().size()==2);
-		GraphicalEditPart containerEditPart= (GraphicalEditPart)getDiagramEditPart().getChildren().get(1);
-		ChangeBoundsRequest changeBoundsRequest = new ChangeBoundsRequest(RequestConstants.REQ_ADD);
-		changeBoundsRequest.setEditParts((EditPart)getDiagramEditPart().getChildren().get(0));
-		changeBoundsRequest.setLocation(new Point(30,30));
-		ShapeCompartmentEditPart compartment=null;
-		int index=0;
-		while (compartment==null && index <containerEditPart.getChildren().size()){
-			if((containerEditPart.getChildren().get(index)) instanceof ShapeCompartmentEditPart){
-				compartment= (ShapeCompartmentEditPart)(containerEditPart.getChildren().get(index));
-			}
-			index++;
-		}
-		assertTrue("Container not found", compartment!=null);
-		
-		
-		command=compartment.getCommand(changeBoundsRequest);
-		assertNotNull(CHANGE_CONTAINER,command);
-		assertTrue(CHANGE_CONTAINER+TEST_IF_THE_COMMAND_IS_CREATED,command!=UnexecutableCommand.INSTANCE);
-		assertTrue(CHANGE_CONTAINER+TEST_IF_THE_COMMAND_CAN_BE_EXECUTED,command.canExecute()==true);
-		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().execute(command);
-		assertTrue(CHANGE_CONTAINER+TEST_THE_EXECUTION,getRootView().getChildren().size()==1);
-		assertTrue(CHANGE_CONTAINER+TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==1);
-		assertTrue(CHANGE_CONTAINER+TEST_THE_EXECUTION,compartment.getChildren().size()==1);
-		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().undo();
-		assertTrue(CHANGE_CONTAINER+TEST_THE_UNDO,getRootView().getChildren().size()==2);
-		assertTrue(CHANGE_CONTAINER+TEST_THE_UNDO,getRootSemanticModel().getOwnedElements().size()==2);
-		assertTrue(CHANGE_CONTAINER+TEST_THE_EXECUTION,compartment.getChildren().size()==0);
-		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().redo();
-		assertTrue(CHANGE_CONTAINER+TEST_THE_REDO,getRootView().getChildren().size()==1);
-		assertTrue(CHANGE_CONTAINER+TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==1);
-		assertTrue(CHANGE_CONTAINER+TEST_THE_EXECUTION,compartment.getChildren().size()==1);
-		
-		}
+	
 	
 	/**
 	 * Test to create a link.
@@ -247,19 +156,23 @@ public class TestLink extends AbstractPapyrusTestCase {
 	public void testToCreateALink(IElementType linkType) {
 		assertTrue(CREATION +INITIALIZATION_TEST,getDiagramEditPart().getChildren().size()==4);
 		assertTrue(CREATION +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(CREATION +INITIALIZATION_TEST, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==0);
 		
 		Command command = target.getCommand(createConnectionViewRequest(linkType, source, target));   
 		assertNotNull(CREATION+COMMAND_NULL,command);
 		assertTrue(CONTAINER_CREATION+TEST_IF_THE_COMMAND_CAN_BE_EXECUTED,command.canExecute()==true);
 		diagramEditor.getEditingDomain().getCommandStack().execute(new GEFtoEMFCommandWrapper(command));
-		assertTrue(CREATION +INITIALIZATION_TEST,((Diagram)getRootView()).getEdges().size()==1);
-		assertTrue(CREATION +INITIALIZATION_TEST,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(CREATION +TEST_THE_EXECUTION,((Diagram)getRootView()).getEdges().size()==1);
+		assertTrue(CREATION +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(CREATION +TEST_THE_EXECUTION, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().undo();
 		assertTrue(CREATION+TEST_THE_UNDO,getRootView().getChildren().size()==4);
 		assertTrue(CREATION+TEST_THE_UNDO,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(CREATION +TEST_THE_UNDO, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==0);
 		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().redo();
 		assertTrue(CREATION +TEST_THE_REDO,((Diagram)getRootView()).getEdges().size()==1);
-		assertTrue(CREATION +TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(CREATION +TEST_THE_REDO,getRootSemanticModel().getOwnedElements().size()==4);
+		assertTrue(CREATION +TEST_THE_REDO, ((Element)source.resolveSemanticElement()).getOwnedElements().size()==1);
 						
 	}
 	public void installEnvironment(IElementType sourceType,IElementType targetType ){
@@ -313,25 +226,7 @@ public class TestLink extends AbstractPapyrusTestCase {
 		
 	}
 	
-	public CreateConnectionViewRequest createConnectionViewRequest(IElementType type , EditPart source, EditPart target){
-		CreateConnectionViewRequest connectionRequest = CreateViewRequestFactory.getCreateConnectionRequest(type,((IGraphicalEditPart) getDiagramEditPart())
-				.getDiagramPreferencesHint());
 
-		connectionRequest.setSourceEditPart(null);
-		connectionRequest.setTargetEditPart(source);
-		connectionRequest
-		.setType(RequestConstants.REQ_CONNECTION_START);
-		source.getCommand(connectionRequest);
-
-		// Now, setup the request in preparation to get the
-		// connection end
-		// command.
-		connectionRequest.setSourceEditPart(source);
-		connectionRequest.setTargetEditPart(target);
-		connectionRequest
-		.setType(RequestConstants.REQ_CONNECTION_END);
-		return connectionRequest;
-	}
 	
 	
 	
@@ -351,7 +246,7 @@ public class TestLink extends AbstractPapyrusTestCase {
 		getDiagramEditPart().getDiagramEditDomain().getDiagramCommandStack().execute(cmd);	
 		// EditorUtils.getTransactionalEditingDomain().getCommandStack().execute(cmd);
 		assertTrue(RECONNECTION_TARGET+TEST_THE_EXECUTION,((Diagram)getRootView()).getEdges().size()==1);
-		assertTrue(RECONNECTION_TARGET +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(RECONNECTION_TARGET +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==4);
 		assertTrue(LINK_EXISTS_RECONNECTION_ON_TARGET,binaryLink.getTarget().equals(targetPlayer));
 		
 		//undo
@@ -381,7 +276,7 @@ public class TestLink extends AbstractPapyrusTestCase {
 		assertTrue(RECONNECTION_SOURCE+TEST_IF_THE_COMMAND_CAN_BE_EXECUTED,cmd.canExecute()==true);
 		getDiagramEditPart().getDiagramEditDomain().getDiagramCommandStack().execute(cmd);	
 		assertTrue(RECONNECTION_SOURCE+TEST_THE_EXECUTION,((Diagram)getRootView()).getEdges().size()==1);
-		assertTrue(RECONNECTION_SOURCE +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==5);
+		assertTrue(RECONNECTION_SOURCE +TEST_THE_EXECUTION,getRootSemanticModel().getOwnedElements().size()==4);
 		assertTrue(LINK_EXISTS_RECONNECTION_ON_SOURCE+TEST_THE_EXECUTION,branch.getSource().equals(sourcePlayer));
 		
 		getDiagramEditPart().getDiagramEditDomain().getDiagramCommandStack().undo();
@@ -391,25 +286,4 @@ public class TestLink extends AbstractPapyrusTestCase {
 		assertTrue(LINK_EXISTS_RECONNECTION_ON_SOURCE+TEST_THE_REDO,branch.getSource().equals(sourcePlayer));
 		
 	}
-	
-	/**
-	 * Test to manage top node.
-	 * 
-	 * @param type the type
-	 * @param containerType the container type
-	 */
-	public void testToManageLink(IElementType sourceType,IElementType targetType, IElementType linkType, IElementType containerType) {
-		installEnvironment(sourceType, targetType);
-		testToCreateALink(linkType);
-		testDestroy(linkType);
-		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().undo();
-		testViewDeletion(linkType);
-		testDrop(linkType);
-		testSourceReconnectAMultiLink(linkType);
-		diagramEditor.getDiagramEditDomain().getDiagramCommandStack().undo();
-		testTargetReconnectAMultiLink(linkType);
-		
-	}
-	
-	
 }
