9382eee9a919d7fd010e23cd4b679c5c8626dcd6
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-12-07 10:15:27 +0000

    [General] Add some cast to Bundle.loadClass(), so the code can be compliant with Eclipse 3.7 [General] Remove dependency of log4j in the test main plugin

--- 0.7.X/plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/part/PapyrusPaletteViewer.java
+++ 0.7.X/plugins/uml/org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/part/PapyrusPaletteViewer.java
@@ -40,7 +40,7 @@ public class PapyrusPaletteViewer extends PaletteViewerEx {
 		if(customizerDialog == null) {
 			try {
 				@SuppressWarnings("unchecked")
-				Class<PaletteCustomizerDialog> advancedCustomizerDialogClass = Activator.getDefault().getBundle().loadClass("org.eclipse.papyrus.diagram.common.palette.customization.dialog.PapyrusPaletteCustomizerDialog");
+				Class<PaletteCustomizerDialog> advancedCustomizerDialogClass = (Class<PaletteCustomizerDialog>)Activator.getDefault().getBundle().loadClass("org.eclipse.papyrus.diagram.common.palette.customization.dialog.PapyrusPaletteCustomizerDialog");
 				Constructor<PaletteCustomizerDialog> constructor = advancedCustomizerDialogClass.getConstructor(Shell.class, PaletteCustomizer.class, PaletteRoot.class);
 				if(constructor != null) {
 					customizerDialog = constructor.newInstance(getControl().getShell(), getCustomizer(), getPaletteRoot());
