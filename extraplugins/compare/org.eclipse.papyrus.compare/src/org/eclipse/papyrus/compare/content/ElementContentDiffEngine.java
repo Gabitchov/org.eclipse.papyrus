b53d33c09a32787eef3f8df0870e52752ef296b8
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-12-10 13:57:47 +0000

    Bug 330183 - [Usability] Papyrus shall enable comparing/merging models.

--- extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/content/ElementContentDiffEngine.java
+++ extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/content/ElementContentDiffEngine.java
@@ -1,15 +1,6 @@
 package org.eclipse.papyrus.compare.content;
 
-import java.util.Collection;
-
-import org.eclipse.emf.compare.FactoryException;
-import org.eclipse.emf.compare.diff.metamodel.DiffFactory;
-import org.eclipse.emf.compare.diff.metamodel.DiffGroup;
-import org.eclipse.emf.compare.match.metamodel.Match2Elements;
-import org.eclipse.emf.compare.match.metamodel.MatchFactory;
-import org.eclipse.emf.compare.match.metamodel.MatchModel;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.papyrus.compare.PapyrusDiffEngine;
 
 
@@ -24,45 +15,4 @@ public class ElementContentDiffEngine extends PapyrusDiffEngine {
 		this.myRight = right;
 	}
 	
-	protected DiffGroup doDiffTwoWay(MatchModel match) {
-		return super.doDiffTwoWay(match);
-//		final DiffGroup diffRoot = DiffFactory.eINSTANCE.createDiffGroup();
-//
-//		// It is a possibility that no elements were matched
-//		if (match.getMatchedElements().size() > 0) {
-//			// we have to browse the model and create the corresponding operations
-//			final Match2Elements matchRoot = (Match2Elements)match.getMatchedElements().get(0);
-//			// browsing the match model
-//
-//			final DiffGroup current = diffRoot;
-//			Match2Elements match2Element = MatchFactory.eINSTANCE.createMatch2Elements();
-//			match2Element.setLeftElement(myLeft);
-//			match2Element.setRightElement(myRight);
-//			current.setRightParent(match2Element.getRightElement());
-//			try {
-//				checkForDiffs(current, match2Element);
-//			} catch (final FactoryException e) {
-//				log(e);
-//			}
-//		}
-//		// iterate over the unmatched elements end determine if they have been
-//		// added or removed.
-//		processUnmatchedElements(diffRoot, match.getUnmatchedElements());
-//		return diffRoot;
-	}
-	
-	
-
-	private EObject getMatch(EObject from) {
-		final Collection<EStructuralFeature.Setting> settings = matchCrossReferencer.get(from);
-		if (settings == null)
-			return null;
-		for (final org.eclipse.emf.ecore.EStructuralFeature.Setting setting : settings) {
-			if (setting.getEObject() instanceof Match2Elements) {
-				return setting.getEObject();
-			}
-		}
-		return null;
-	}
-
 }
--- extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/content/ElementContentMatchEngine.java
+++ extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/content/ElementContentMatchEngine.java
@@ -1,10 +1,5 @@
 package org.eclipse.papyrus.compare.content;
 
-import java.util.List;
-import java.util.Map;
-
-import org.eclipse.emf.compare.FactoryException;
-import org.eclipse.emf.compare.match.metamodel.MatchModel;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.papyrus.compare.PapyrusMatchEngine;
 
@@ -20,28 +15,5 @@ public class ElementContentMatchEngine extends PapyrusMatchEngine {
 		this.myRight = right;
 	}
 	
-	@Override
-	public MatchModel contentMatch(EObject leftObject, EObject rightObject, Map<String, Object> optionMap) {
-		// TODO Auto-generated method stub
-		return super.contentMatch(leftObject, rightObject, optionMap);
-	}
-	
-	@Override
-	protected boolean isSimilar(EObject obj1, EObject obj2) throws FactoryException {
-		if (myLeft == obj1 && myRight == obj2) {
-			return true;
-		}
-		// TODO Auto-generated method stub
-		return super.isSimilar(obj1, obj2);
-	}
-	
-	@Override
-	protected EObject findMostSimilar(EObject eObj, List<EObject> list) throws FactoryException {
-		if (eObj == myLeft) {
-			return myRight;
-		}
-		// TODO Auto-generated method stub
-		return super.findMostSimilar(eObj, list);
-	}
 
 }
