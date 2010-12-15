f02af36fb8912a7a7a5220b2efbb6c640d4a3874
aradermache <aradermache@d68fad7f-2b57-0410-97a2-f40a37629c47>
2010-12-15 22:44:35 +0000

    Initial import

---
+++ extraplugins/conversion/org.eclipse.papyrus.conversion.di2todi/src/org/eclipse/papyrus/conversion/di2todi/blackboxes/VisualIDs.java
@@ -0,0 +1,34 @@
+
+/**
+ * @author: Manel Fredj - CEA
+ * This class  in meant to centralize all the values of the visualIds that are used in the QVTO transformation
+ * At the current stage of implementation it is duplicated in VisualIDs.qvto
+ * As a future evolution, this class should be generated automatically from di plugin
+ */
+
+package org.eclipse.papyrus.conversion.di2todi.blackboxes;
+
+
+public class VisualIDs {
+	
+	//for a class
+	public static String VisualId_Class="2008";
+	
+	public static String VisualId_Class_label="5029";
+	public static String VisualId_Class_Attrbutes="7017";
+	public static String VisualId_Class_Operations="7018";
+	public static String VisualId_Class_UseCase="7019";
+	
+	public static String VisualId_Operation="3013";
+	public static String VisualId_Attribute="3012";
+	
+	//for an association  
+	public static String VisualId_Edge="4001";
+	
+	public static String VisualId_firstDecoNode="6001";
+	public static String VisualId_secondDecoNode="6002";
+	public static String VisualId_thirdDecoNode="6003";
+	public static String VisualId_fourthDecoNode="6005";
+	public static String VisualId_fifthDecoNode="6033";
+	public static String VisualId_sixthDecoNode="6034";
+}
