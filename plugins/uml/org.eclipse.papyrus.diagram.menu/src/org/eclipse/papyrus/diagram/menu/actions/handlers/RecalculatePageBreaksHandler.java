e5808159879565140088627f0f80d451ea088a9c
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-01-04 14:08:41 +0000

    NEW - bug 327351: [Usability] Create a new plugin in order to regroup the rewritten GMF action + the Papyrus Action https://bugs.eclipse.org/bugs/show_bug.cgi?id=327351
    The whole Menu View : Grid, Rulers, Page Break, Recalculate Page Break and Snap To Grid

---
+++ plugins/uml/org.eclipse.papyrus.diagram.menu/src/org/eclipse/papyrus/diagram/menu/actions/handlers/RecalculatePageBreaksHandler.java
@@ -0,0 +1,78 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.diagram.menu.actions.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramRootEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.internal.properties.WorkspaceViewerProperties;
+import org.eclipse.gmf.runtime.diagram.ui.internal.requests.RecalculatePageBreaksRequest;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramGraphicalViewer;
+
+/**
+ * 
+ * Handler for the Recalculate Page Breaks Action
+ * 
+ * 
+ */
+@SuppressWarnings("restriction")
+public class RecalculatePageBreaksHandler extends AbstractViewHandler {
+
+
+	/**
+	 * 
+	 * Constructor.
+	 * 
+	 */
+	@SuppressWarnings("restriction")
+	public RecalculatePageBreaksHandler() {
+		super(WorkspaceViewerProperties.VIEWPAGEBREAKS);
+	}
+
+
+	/**
+	 * 
+	 * @see org.eclipse.papyrus.diagram.menu.actions.handlers.AbstractViewHandler#isEnabled()
+	 * 
+	 * @return
+	 */
+	@Override
+	public boolean isEnabled() {
+		if(super.isEnabled() && (getDiagramGraphicalViewer() instanceof DiagramGraphicalViewer)) {
+			return isChecked();
+		}
+		return false;
+	}
+
+	/**
+	 * 
+	 * Execute the request via the preformRequest() call. This action does
+	 * not modify the model and does not use the request/command infrastructure.
+	 * 
+	 * @see org.eclipse.papyrus.diagram.menu.actions.handlers.AbstractViewHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 * 
+	 * @param event
+	 * @return
+	 * @throws ExecutionException
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		@SuppressWarnings("restriction")
+		RecalculatePageBreaksRequest request = new RecalculatePageBreaksRequest();
+		//not done in a CommandStack! (like the original!)
+		((DiagramRootEditPart)getDiagramEditPart().getRoot()).performRequest(request);
+		return null;
+	}
+
+}
