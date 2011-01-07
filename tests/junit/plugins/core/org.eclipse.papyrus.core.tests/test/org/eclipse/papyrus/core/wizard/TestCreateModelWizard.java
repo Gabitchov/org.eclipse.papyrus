ae60942aa6d136fac555ecee287b472e3fb6e8e9
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-01-07 15:41:37 +0000

    Bug 333558 - [Wizard] Refactor NewPapyrusProjectWizard

--- tests/junit/plugins/core/org.eclipse.papyrus.core.tests/test/org/eclipse/papyrus/core/wizard/TestCreateModelWizard.java
+++ tests/junit/plugins/core/org.eclipse.papyrus.core.tests/test/org/eclipse/papyrus/core/wizard/TestCreateModelWizard.java
@@ -1,6 +1,6 @@
 package org.eclipse.papyrus.core.wizard;
 
-import org.eclipse.papyrus.wizards.CreateModelWizard;
+import org.eclipse.papyrus.wizards.InitModelWizard;
 import org.eclipse.papyrus.wizards.pages.NewModelFilePage;
 import org.eclipse.papyrus.wizards.pages.SelectDiagramCategoryPage;
 import org.eclipse.papyrus.wizards.pages.SelectDiagramKindPage;
@@ -9,8 +9,20 @@ import org.eclipse.ui.IWorkbenchWizard;
 
 public class TestCreateModelWizard extends TestNewModelWizardBase {
 
+	@Override
 	protected IWorkbenchWizard createWizard() {
-		return new CreateModelWizard();
+		return new InitModelWizard() {
+			@Override
+			protected boolean isCreateFromExistingDomainModel() {
+				return false;
+			}
+			
+			@Override
+			public String getDiagramFileExtension() {
+				return "test.xxx";
+			}
+			
+		};
 	}
 
 	public void testOrderOfPages() {
@@ -20,6 +32,11 @@ public class TestCreateModelWizard extends TestNewModelWizardBase {
 			SelectDiagramKindPage.class,
 		};
 		testOrderOfPages(expectedPages);
+	}
+	
+	public void testDiagramFileExtenstion() {
+		NewModelFilePage page = (NewModelFilePage)getWizard().getPages()[0];
+		assertEquals("test.xxx", page.getFileExtension());
 	}
 
 }
