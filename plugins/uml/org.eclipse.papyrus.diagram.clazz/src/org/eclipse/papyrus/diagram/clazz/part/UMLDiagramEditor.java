c6444e8d33655eeb66b1a34089d937258166df48
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-02-08 10:00:22 +0000

    318417: [Papyrus Model Explorer] Facets are not removed when the Customization changes https://bugs.eclipse.org/bugs/show_bug.cgi?id=318417 clean code

--- plugins/uml/org.eclipse.papyrus.diagram.clazz/src/org/eclipse/papyrus/diagram/clazz/part/UMLDiagramEditor.java
+++ plugins/uml/org.eclipse.papyrus.diagram.clazz/src/org/eclipse/papyrus/diagram/clazz/part/UMLDiagramEditor.java
@@ -55,6 +55,7 @@ import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.ide.IGotoMarker;
 import org.eclipse.ui.navigator.resources.ProjectExplorer;
@@ -501,6 +502,8 @@ public class UMLDiagramEditor extends UmlGmfDiagramEditor implements IProviderCh
 			// If not the active editor, ignore selection changed.
 			if(this.equals(editor.getActiveEditor())) {
 				updateActions(getSelectionActions());
+			  ISelectionService selectionService=  getSite().getWorkbenchWindow().getSelectionService();
+
 				super.selectionChanged(part, selection);
 			} else {
 				super.selectionChanged(part, selection);
