2590efd0f9eab89b35582c25485176ad8643abf9
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-02-24 14:29:31 +0000

    Bug 337734 - [Wizard] Allow the user to select several diagram categories and create several models at once

--- plugins/core/org.eclipse.papyrus.wizards/src/org/eclipse/papyrus/wizards/pages/SelectSeveralDiagramCategoriesPage.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.papyrus.wizards.pages;
-
-
-public class SelectSeveralDiagramCategoriesPage extends SelectDiagramCategoryPage {
-
-	public String[] getDiagramCategories() {
-		return null;
-	}
-}
