05e7a759bd8f1ba1770e19726ee59b1374714cc9
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-03-01 08:52:24 +0000

    Bug 338414 - [UML Compare] Filter only changes properties in the Merge Property Viewer

--- extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/ui/viewer/content/UMLModelContentMergeViewer.java
+++ extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/ui/viewer/content/UMLModelContentMergeViewer.java
@@ -16,11 +16,18 @@ package org.eclipse.papyrus.compare.ui.viewer.content;
 import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.emf.compare.ui.viewer.content.ModelContentMergeViewer;
 import org.eclipse.emf.compare.ui.viewer.content.part.ModelContentMergeTabFolder;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.papyrus.compare.ui.viewer.content.part.UMLModelContentMergeTabFolder;
 import org.eclipse.swt.widgets.Composite;
 
 
 public class UMLModelContentMergeViewer extends ModelContentMergeViewer {
+
+	private Action myShowAllProperties;
 
 	public UMLModelContentMergeViewer(Composite parent, CompareConfiguration config) {
 		super(parent, config);
@@ -29,6 +36,35 @@ public class UMLModelContentMergeViewer extends ModelContentMergeViewer {
 	@Override
 	protected ModelContentMergeTabFolder createModelContentMergeTabFolder(Composite composite, int side) {
 		return new UMLModelContentMergeTabFolder(this, composite, side);
+	}
+
+	@Override
+	protected void createToolItems(ToolBarManager tbm) {
+		super.createToolItems(tbm);
+		tbm.add(new Separator("viewer")); //$NON-NLS-1$
+		myShowAllProperties = new Action("Show All Properties", IAction.AS_CHECK_BOX) {
+
+			@Override
+			public void run() {
+				showAllProperties();
+			}
+		};
+		myShowAllProperties.setToolTipText("Show All Properties of the Element");
+		//			setImageDescriptor(ImageDescriptor.createFromImage(descriptor.getEnabledImage()));
+		//			setHoverImageDescriptor(ImageDescriptor.createFromImage(descriptor.getEnabledImage()));
+		//			setDisabledImageDescriptor(ImageDescriptor.createFromImage(descriptor.getDisabledImage()));
+
+		final ActionContributionItem showAllPropertiesContribution = new ActionContributionItem(myShowAllProperties);
+		showAllPropertiesContribution.setVisible(true);
+		tbm.appendToGroup("viewer", showAllPropertiesContribution); //$NON-NLS-1$
+	}
+
+	protected void showAllProperties() {
+		//		if(currentSelection != null) {
+		//			copy(currentSelection, true);
+		//		}
+		//		currentSelection.clear();
+		//		switchCopyState(false);
 	}
 
 }
