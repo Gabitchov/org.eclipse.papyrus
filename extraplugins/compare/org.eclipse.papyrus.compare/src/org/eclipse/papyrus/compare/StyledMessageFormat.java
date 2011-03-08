ea3462cf51c04abcfe4cd2783edc464757d5e7c0
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-03-08 10:20:16 +0000

    refactoring - extract StyledMessageFormat

--- extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/StyledMessageFormat.java
+++ extraplugins/compare/org.eclipse.papyrus.compare/src/org/eclipse/papyrus/compare/StyledMessageFormat.java
@@ -16,19 +16,24 @@ package org.eclipse.papyrus.compare;
 import java.text.MessageFormat;
 
 import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.jface.viewers.StyledString.Styler;
 
 
 public class StyledMessageFormat {
+	
+	private static final Styler ELEMENT_STYLER = StyledString.DECORATIONS_STYLER;
+
+	private static final Styler MESSAGE_STYLER = null;
 
 	public static StyledString format(String pattern, String... args) {
 		StyledString styledString = new StyledString();
-		styledString.append(MessageFormat.format(pattern, (Object[])args), StyledString.DECORATIONS_STYLER);
+		styledString.append(MessageFormat.format(pattern, (Object[])args), ELEMENT_STYLER);
 		int currInd = 0;
 		int gap = 0;
 		for(int i = 0; i < args.length; i++) {
 			int nextInd = pattern.indexOf("{", currInd); //$NON-NLS-1$
 			if(nextInd != -1 && args.length > i) {
-				styledString.setStyle(nextInd + gap, args[i].length(), null);
+				styledString.setStyle(nextInd + gap, args[i].length(), MESSAGE_STYLER);
 				currInd = nextInd + 1;
 				gap += args[i].length() - "{}".length() - String.valueOf(i).length(); //$NON-NLS-1$
 			} else {
