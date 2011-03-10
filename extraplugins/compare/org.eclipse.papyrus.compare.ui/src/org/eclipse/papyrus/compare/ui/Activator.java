2c15357ecd64d382f8f19f8fb36d2226eede3397
tfesenko <tfesenko@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-03-10 17:57:08 +0000

    add java docs

--- extraplugins/compare/org.eclipse.papyrus.compare.ui/src/org/eclipse/papyrus/compare/ui/Activator.java
+++ extraplugins/compare/org.eclipse.papyrus.compare.ui/src/org/eclipse/papyrus/compare/ui/Activator.java
@@ -6,18 +6,20 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
  */
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
+	/** The Constant PLUGIN_ID. */
 	public static final String PLUGIN_ID = "org.eclipse.papyrus.compare.ui"; //$NON-NLS-1$
 
 	// The shared instance
+	/** The plugin. */
 	private static Activator plugin;
 
 	/**
-	 * The constructor
+	 * The constructor.
 	 */
 	public Activator() {
 	}
@@ -43,23 +45,39 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns the shared instance
-	 * 
+	 * Returns the shared instance.
+	 *
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
 		return plugin;
 	}
 
+	/**
+	 * Log error.
+	 *
+	 * @param e the e
+	 */
 	public static void logError(Throwable e) {
 		logError(Messages.Activator_log_error_message, e);
 	}
 
+	/**
+	 * Log error.
+	 *
+	 * @param message the message
+	 * @param e the e
+	 */
 	public static void logError(String message, Throwable e) {
 		IStatus status = new Status(IStatus.ERROR, PLUGIN_ID, message, e);
 		getDefault().getLog().log(status);
 	}
 
+	/**
+	 * Log info.
+	 *
+	 * @param message the message
+	 */
 	public static void logInfo(String message) {
 		IStatus status = new Status(IStatus.INFO, PLUGIN_ID, message);
 		getDefault().getLog().log(status);
