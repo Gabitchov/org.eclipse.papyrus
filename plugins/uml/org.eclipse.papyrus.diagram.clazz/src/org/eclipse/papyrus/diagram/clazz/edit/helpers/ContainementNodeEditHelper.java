1d3e4c01325bc0c317fa5ab037a96b0efa70e261
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-04-18 12:19:59 +0000

    NEW - bug 343111: [ClassDiagram] class diagram is not correctly regenerated https://bugs.eclipse.org/bugs/show_bug.cgi?id=343111

--- plugins/uml/org.eclipse.papyrus.diagram.clazz/src/org/eclipse/papyrus/diagram/clazz/edit/helpers/ContainementNodeEditHelper.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.papyrus.diagram.clazz.edit.helpers;
-
-/**
- * @generated
- */
-public class ContainementNodeEditHelper extends UMLBaseEditHelper {
-}
--- plugins/uml/org.eclipse.papyrus.diagram.clazz/src/org/eclipse/papyrus/diagram/clazz/edit/helpers/InstanceSpecificationLinkHelper.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.papyrus.diagram.clazz.edit.helpers;
-
-import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
-
-/**
- * @generated
- */
-public class InstanceSpecificationLinkHelper extends AbstractEditHelperAdvice {
-}
