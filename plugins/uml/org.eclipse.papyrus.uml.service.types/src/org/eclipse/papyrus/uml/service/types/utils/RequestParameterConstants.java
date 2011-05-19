8102f36957a2a9ebcdbbaa7b65cd3bad0565182c
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-05-19 18:08:11 +0000

    NEW - bug 324966: [Element Edit Service] Centralized element edit service provide command for model element deletion, creation, modification ... https://bugs.eclipse.org/bugs/show_bug.cgi?id=324966

--- plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/utils/RequestParameterConstants.java
+++ plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/utils/RequestParameterConstants.java
@@ -8,7 +8,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  Yann Tanguy (CEA LIST) yann.tanguy@cea.fr - Initial API and implementation
+ * 
+ * 		Yann Tanguy (CEA LIST) yann.tanguy@cea.fr - Initial API and implementation
  *
  *****************************************************************************/
 package org.eclipse.papyrus.uml.service.types.utils;
@@ -34,4 +35,10 @@ public interface RequestParameterConstants {
 	 * The ID for the additional parameter RECONNECT_PARENT possibly required in link reconnect request
 	 */
 	public static final String CONNECTOR_REORIENT_REQUEST_TARGET_PARENT = "RECONNECT_PARENT"; //$NON-NLS-1$
+
+	/**
+	 * Extended request data key to hold the edge view during a reconnect request.
+	 */
+	public static final String GRAPHICAL_RECONNECTED_EDGE = "graphical_edge"; //$NON-NLS-1$
+
 }
