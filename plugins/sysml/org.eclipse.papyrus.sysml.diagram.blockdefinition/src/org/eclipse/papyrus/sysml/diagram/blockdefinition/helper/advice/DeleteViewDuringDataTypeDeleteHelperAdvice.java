a62b041aab798acf60d6776b97f32e18d28713e2
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-06-06 12:21:52 +0000

    348324: [SysML Block Definition Diagram] Part and Ref. graphical refresh when aggregation changes https://bugs.eclipse.org/bugs/show_bug.cgi?id=348324
    348383: [SysML Block Definition Diagram] Consistent modification of inherited content when Generalization is deleted or re-oriented
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=348383

---
+++ plugins/sysml/org.eclipse.papyrus.sysml.diagram.blockdefinition/src/org/eclipse/papyrus/sysml/diagram/blockdefinition/helper/advice/DeleteViewDuringDataTypeDeleteHelperAdvice.java
@@ -0,0 +1,98 @@
+/*****************************************************************************
+ * Copyright (c) 2011 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		
+ *		CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.sysml.diagram.blockdefinition.helper.advice;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
+import org.eclipse.gmf.runtime.emf.core.util.EMFCoreUtil;
+import org.eclipse.gmf.runtime.emf.type.core.ISpecializationType;
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
+import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyDependentsRequest;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.papyrus.diagram.common.util.CrossReferencerUtil;
+import org.eclipse.papyrus.sysml.diagram.blockdefinition.provider.ElementTypes;
+import org.eclipse.papyrus.sysml.diagram.common.utils.SysMLGraphicalTypes;
+import org.eclipse.papyrus.sysml.service.types.element.SysMLElementTypes;
+import org.eclipse.uml2.uml.UMLPackage;
+
+/**
+ * <pre>
+ * Edit helper advice that delete Part or Reference views from diagram when its type (Block) is deleted.
+ * </pre>
+ */
+public class DeleteViewDuringDataTypeDeleteHelperAdvice extends AbstractEditHelperAdvice {
+
+	@Override
+	protected ICommand getBeforeDestroyDependentsCommand(DestroyDependentsRequest request) {
+
+		
+		EObject elementToEdit = request.getElementToDestroy();
+		Set<View> viewsToDestroy = new HashSet<View>();
+
+		// Get all Value (property) typed by this deleted DataType
+		Iterator<?> references = EMFCoreUtil.getReferencers(elementToEdit, new EReference[]{UMLPackage.eINSTANCE.getTypedElement_Type()}).iterator();
+		while(references.hasNext()) {
+			Object current = references.next();
+			
+			if (! (current instanceof EObject)) {
+				continue;
+			}
+			
+			EObject property = (EObject) current;
+			
+			// Search inconsistent views if the property is a Value
+			if (((ISpecializationType) SysMLElementTypes.VALUE_PROPERTY).getMatcher().matches(property)) {
+				viewsToDestroy.addAll(getValueViewsToDestroy(property));
+			}
+		}
+
+		if(!(viewsToDestroy.isEmpty())) {
+			DestroyDependentsRequest req = new DestroyDependentsRequest(request.getEditingDomain(), elementToEdit, false);
+			return req.getDestroyDependentsCommand(viewsToDestroy);
+		}
+		
+		return super.getBeforeDestroyDependentsCommand(request);
+	}
+
+	/**
+	 * This methods looks for inconsistent Value views to delete.
+	 * 
+	 * @param modifiedObject
+	 *        the modified {@link EObject}
+	 * @return the list of {@link View} to delete
+	 */
+	private Set<View> getValueViewsToDestroy(EObject modifiedObject) {
+		Set<View> viewsToDestroy = new HashSet<View>();
+
+		Iterator<View> viewIt = CrossReferencerUtil.getCrossReferencingViews(modifiedObject, ElementTypes.DIAGRAM_ID).iterator();
+		while(viewIt.hasNext()) {
+			View view = (View)viewIt.next();
+
+			String containerType = ViewUtil.getViewContainer(view) != null ? ViewUtil.getViewContainer(view).getType() : null;
+
+			if(SysMLGraphicalTypes.COMPARTMENT_SYSML_VALUE_AS_LIST_ID.equals(containerType)) {
+				viewsToDestroy.add(view);
+			}
+		}
+
+		return viewsToDestroy;
+	}
+
+}
---
+++ plugins/sysml/org.eclipse.papyrus.sysml.diagram.blockdefinition/src/org/eclipse/papyrus/sysml/diagram/blockdefinition/helper/advice/DeleteViewDuringPartEditHelperAdvice.java
@@ -0,0 +1,91 @@
+/*****************************************************************************
+ * Copyright (c) 2011 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		
+ *		CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.sysml.diagram.blockdefinition.helper.advice;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.command.CompositeCommand;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
+import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyDependentsRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.papyrus.diagram.common.util.CrossReferencerUtil;
+import org.eclipse.papyrus.sysml.diagram.blockdefinition.provider.ElementTypes;
+import org.eclipse.papyrus.sysml.diagram.common.utils.SysMLGraphicalTypes;
+import org.eclipse.uml2.uml.AggregationKind;
+import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.UMLPackage;
+
+/**
+ * <pre>
+ * Edit helper advice that delete Part views from diagram when its aggregation kind
+ * is modified and turns it into Reference.
+ * </pre>
+ */
+public class DeleteViewDuringPartEditHelperAdvice extends AbstractEditHelperAdvice {
+
+	@Override
+	protected ICommand getBeforeSetCommand(SetRequest request) {
+		
+		ICommand setCommand = super.getBeforeSetCommand(request);
+
+		Set<View> viewsToDestroy = new HashSet<View>();
+
+		// Get modified object and retrieve inconsistent view
+		EObject modifiedObject = request.getElementToEdit();
+		if ((modifiedObject != null) && (modifiedObject instanceof Property) 
+			&& (request.getFeature() == UMLPackage.eINSTANCE.getProperty_Aggregation()) 
+			&& (request.getValue() != AggregationKind.COMPOSITE_LITERAL)) {
+			
+			viewsToDestroy.addAll(getViewsToDestroy(modifiedObject));
+		}
+
+		if(!viewsToDestroy.isEmpty()) {
+			DestroyDependentsRequest ddr = new DestroyDependentsRequest(request.getEditingDomain(), request.getElementToEdit(), false);
+			ICommand destroyViewsCommand = ddr.getDestroyDependentsCommand(viewsToDestroy);
+			setCommand = CompositeCommand.compose(setCommand, destroyViewsCommand);
+		}
+
+		return setCommand;
+	}
+
+	/**
+	 * This methods looks for inconsistent views to delete in case a Part aggregation kind changes.
+	 * 
+	 * @param modifiedObject
+	 *        the modified {@link EObject}
+	 * @return the list of {@link View} to delete
+	 */
+	protected Set<View> getViewsToDestroy(EObject modifiedObject) {
+		Set<View> viewsToDestroy = new HashSet<View>();
+
+		Iterator<View> viewIt = CrossReferencerUtil.getCrossReferencingViews(modifiedObject, ElementTypes.DIAGRAM_ID).iterator();
+		while(viewIt.hasNext()) {
+			View view = (View)viewIt.next();
+
+			String containerType = ViewUtil.getViewContainer(view) != null ? ViewUtil.getViewContainer(view).getType() : null;
+
+			if(SysMLGraphicalTypes.COMPARTMENT_SYSML_PART_AS_LIST_ID.equals(containerType)) {
+				viewsToDestroy.add(view);
+			}
+		}
+
+		return viewsToDestroy;
+	}
+}
