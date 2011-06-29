e0496281596162f970f0f794ea078c388971a8b4
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-06-29 09:56:16 +0000

    NEW - bug 344465: [Table Editor] Papyrus should provide Facets and Queries to edit the element stereotyped with SysML https://bugs.eclipse.org/bugs/show_bug.cgi?id=344465

--- plugins/sysml/org.eclipse.papyrus.sysml.facets/src/org/eclipse/papyrus/sysml/facets/query/value/getter/GetRequirementTextQuery.java
+++ plugins/sysml/org.eclipse.papyrus.sysml.facets/src/org/eclipse/papyrus/sysml/facets/query/value/getter/GetRequirementTextQuery.java
@@ -16,24 +16,15 @@ package org.eclipse.papyrus.sysml.facets.query.value.getter;
 import org.eclipse.emf.facet.infra.query.core.exception.ModelQueryExecutionException;
 import org.eclipse.emf.facet.infra.query.core.java.IJavaModelQuery;
 import org.eclipse.emf.facet.infra.query.core.java.ParameterValueList;
-import org.eclipse.papyrus.sysml.util.SysmlResource;
+import org.eclipse.papyrus.sysml.requirements.Requirement;
+import org.eclipse.papyrus.sysml.util.ElementUtil;
 import org.eclipse.uml2.uml.Class;
-import org.eclipse.uml2.uml.Stereotype;
 
 /** Query to get the text of the requirement */
 public class GetRequirementTextQuery implements IJavaModelQuery<Class, String> {
 
 	public String evaluate(final Class context, final ParameterValueList parameterValues) throws ModelQueryExecutionException {
-		Stereotype ste = context.getAppliedStereotype(SysmlResource.REQUIREMENT_ID);
-		if(ste != null) {
-			Object value = context.getValue(ste, SysmlResource.REQUIREMENT_TEXT_ID);
-			if(value == null) {
-				return ""; //$NON-NLS-1$
-			}
-			if(value instanceof String) {
-				return (String)value;
-			}
-		}
-		return null;
+		Requirement requirement = ElementUtil.getStereotypeApplication(context, Requirement.class);
+		return (requirement != null) ?  requirement.getText() : null;
 	}
 }
