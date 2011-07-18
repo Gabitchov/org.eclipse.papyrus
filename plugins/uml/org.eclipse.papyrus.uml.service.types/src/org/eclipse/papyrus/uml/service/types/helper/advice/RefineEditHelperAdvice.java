bed6c6f150309373a2950e7f9365c810ddc280bc
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-07-18 13:54:09 +0000

    NEW - bug 352344: [Element Edit Service] Cannot get command provider for Standard::Refine https://bugs.eclipse.org/bugs/show_bug.cgi?id=352344

---
+++ plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/helper/advice/RefineEditHelperAdvice.java
@@ -0,0 +1,58 @@
+/*****************************************************************************
+ * Copyright (c) 2011 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.uml.service.types.helper.advice;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.emf.type.core.commands.ConfigureElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
+import org.eclipse.papyrus.uml.service.types.utils.NamedElementHelper;
+import org.eclipse.papyrus.uml.standard.Refine;
+import org.eclipse.papyrus.uml.standard.StandardPackage;
+import org.eclipse.papyrus.uml.standard.util.StandardResource;
+import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.Stereotype;
+
+/** Standard Profile {@link Refine} edit helper advice */
+public class RefineEditHelperAdvice extends AbstractStereotypedElementEditHelperAdvice {
+
+	/** Default constructor */
+	public RefineEditHelperAdvice() {
+		requiredProfileIDs.add(StandardResource.STANDARD_ID);
+	}
+
+	/** Complete creation process by applying the expected stereotype */
+	@Override
+	protected ICommand getBeforeConfigureCommand(final ConfigureRequest request) {
+
+		return new ConfigureElementCommand(request) {
+
+			protected CommandResult doExecuteWithResult(IProgressMonitor progressMonitor, IAdaptable info) throws ExecutionException {
+				NamedElement element = (NamedElement)request.getElementToConfigure();
+				if(element != null) {
+					Stereotype stereotypeToApply = element.getApplicableStereotype(StandardResource.REFINE_ID);
+					if(stereotypeToApply != null) {
+						element.applyStereotype(stereotypeToApply);
+					}
+
+					// Set default name
+					// Initialize the element name based on the created IElementType
+					String initializedName = NamedElementHelper.EINSTANCE.getNewUMLElementName(element.getOwner(), StandardPackage.eINSTANCE.getRefine());
+					element.setName(initializedName);
+				}
+				return CommandResult.newOKCommandResult(element);
+			}
+		};
+	}
+}
