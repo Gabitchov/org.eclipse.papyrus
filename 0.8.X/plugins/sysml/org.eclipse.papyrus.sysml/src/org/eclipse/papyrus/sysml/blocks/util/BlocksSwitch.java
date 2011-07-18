de28a28643b1eb2d358fcfd05bf506bde5a51226
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-07-18 11:40:46 +0000

    NEW - bug 352334: [SysML Static Profile] A part of the API has not been generated (EPackageSwitch classes) https://bugs.eclipse.org/bugs/show_bug.cgi?id=352334

--- 0.8.X/plugins/sysml/org.eclipse.papyrus.sysml/src/org/eclipse/papyrus/sysml/blocks/util/BlocksSwitch.java
+++ 0.8.X/plugins/sysml/org.eclipse.papyrus.sysml/src/org/eclipse/papyrus/sysml/blocks/util/BlocksSwitch.java
@@ -13,10 +13,9 @@
  *****************************************************************************/
 package org.eclipse.papyrus.sysml.blocks.util;
 
-import java.util.List;
-
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.util.Switch;
 import org.eclipse.papyrus.sysml.blocks.BindingConnector;
 import org.eclipse.papyrus.sysml.blocks.Block;
 import org.eclipse.papyrus.sysml.blocks.BlocksPackage;
@@ -39,7 +38,7 @@ import org.eclipse.papyrus.sysml.blocks.ValueType;
  * @see org.eclipse.papyrus.sysml.blocks.BlocksPackage
  * @generated
  */
-public class BlocksSwitch<T> {
+public class BlocksSwitch<T> extends Switch<T> {
 
 	/**
 	 * The cached model package
@@ -59,6 +58,20 @@ public class BlocksSwitch<T> {
 		if(modelPackage == null) {
 			modelPackage = BlocksPackage.eINSTANCE;
 		}
+	}
+
+	/**
+	 * Checks whether this is a switch for the given package.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @parameter ePackage the package in question.
+	 * @return whether this is a switch for the given package.
+	 * @generated
+	 */
+	@Override
+	protected boolean isSwitchFor(EPackage ePackage) {
+		return ePackage == modelPackage;
 	}
 
 	/**
@@ -227,6 +240,7 @@ public class BlocksSwitch<T> {
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
 	 * @generated
 	 */
+	@Override
 	public T defaultCase(EObject object) {
 		return null;
 	}
@@ -238,33 +252,7 @@ public class BlocksSwitch<T> {
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	protected T doSwitch(EClass theEClass, EObject theEObject) {
-		if(theEClass.eContainer() == modelPackage) {
-			return doSwitch(theEClass.getClassifierID(), theEObject);
-		} else {
-			List<EClass> eSuperTypes = theEClass.getESuperTypes();
-			return eSuperTypes.isEmpty() ? defaultCase(theEObject) : doSwitch(eSuperTypes.get(0), theEObject);
-		}
-	}
-
-	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
-	public T doSwitch(EObject theEObject) {
-		return doSwitch(theEObject.eClass(), theEObject);
-	}
-
-	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
+	@Override
 	protected T doSwitch(int classifierID, EObject theEObject) {
 		switch(classifierID) {
 		case BlocksPackage.BLOCK:
