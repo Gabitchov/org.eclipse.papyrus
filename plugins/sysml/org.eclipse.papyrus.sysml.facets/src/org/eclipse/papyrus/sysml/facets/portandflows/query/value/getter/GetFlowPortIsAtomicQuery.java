47f884efe844ea3a8715bd2af83336c195c50ed0
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-07-22 12:10:14 +0000

    Merged Branch 0.8.X changes 5098::5109 for the plugin oep.sysml.facets
    NEW - bug 352852: [Table Editor] The SysML profile should be represented by Facets for the table
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=352852

---
+++ plugins/sysml/org.eclipse.papyrus.sysml.facets/src/org/eclipse/papyrus/sysml/facets/portandflows/query/value/getter/GetFlowPortIsAtomicQuery.java
@@ -0,0 +1,31 @@
+package org.eclipse.papyrus.sysml.facets.portandflows.query.value.getter;
+
+import org.eclipse.emf.facet.infra.query.core.exception.ModelQueryExecutionException;
+import org.eclipse.emf.facet.infra.query.core.java.IJavaModelQuery;
+import org.eclipse.emf.facet.infra.query.core.java.ParameterValueList;
+import org.eclipse.papyrus.sysml.portandflows.FlowPort;
+import org.eclipse.papyrus.sysml.util.ElementUtil;
+import org.eclipse.uml2.uml.Port;
+
+/** Query to get the derived attribute "isAtomic" of the FlowPort */
+public class GetFlowPortIsAtomicQuery implements IJavaModelQuery<Port, Boolean> {
+
+	/**
+	 * 
+	 * @see org.eclipse.emf.facet.infra.query.core.java.IJavaModelQuery#evaluate(org.eclipse.emf.ecore.EObject,
+	 *      org.eclipse.emf.facet.infra.query.core.java.ParameterValueList)
+	 * 
+	 * @param context
+	 * @param parameterValues
+	 * @return
+	 * @throws ModelQueryExecutionException
+	 */
+	public Boolean evaluate(final Port context, final ParameterValueList parameterValues)
+			throws ModelQueryExecutionException {
+		FlowPort flowPort = ElementUtil.getStereotypeApplication(context, FlowPort.class);
+		if(flowPort != null) {
+			return flowPort.isIsAtomic();
+		}
+		return null;
+	}
+}
