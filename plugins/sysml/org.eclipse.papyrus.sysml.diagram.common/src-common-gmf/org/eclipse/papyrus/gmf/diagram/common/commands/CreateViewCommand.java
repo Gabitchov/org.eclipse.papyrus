d71ccba4fbe98ed0a9587316188c9fefb3cf3c60
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-07-27 08:30:13 +0000

    RESOLVED - bug 353129: [SysML Block Definition Diagram] Connector handles throw NPE https://bugs.eclipse.org/bugs/show_bug.cgi?id=353129

--- plugins/sysml/org.eclipse.papyrus.sysml.diagram.common/src-common-gmf/org/eclipse/papyrus/gmf/diagram/common/commands/CreateViewCommand.java
+++ plugins/sysml/org.eclipse.papyrus.sysml.diagram.common/src-common-gmf/org/eclipse/papyrus/gmf/diagram/common/commands/CreateViewCommand.java
@@ -22,9 +22,10 @@ import org.eclipse.gmf.runtime.emf.type.core.IElementType;
 import org.eclipse.gmf.runtime.notation.View;
 
 /**
- * A replacement for CreateCommand that avoids that takes into account the incorrect 
- * generation of ViewProvider by GMFTooling and modifies SemanticAdapter in call to 
- * {@link ViewService#provides(Class, org.eclipse.core.runtime.IAdaptable, View, String, int, boolean, org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint)}.
+ * A replacement for CreateCommand that avoids that takes into account the incorrect
+ * generation of ViewProvider by GMFTooling and modifies SemanticAdapter in call to
+ * {@link ViewService#provides(Class, org.eclipse.core.runtime.IAdaptable, View, String, int, boolean, org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint)}
+ * .
  */
 public class CreateViewCommand extends CreateCommand {
 
@@ -38,24 +39,22 @@ public class CreateViewCommand extends CreateCommand {
 	 */
 	@Override
 	public boolean canExecute() {
-		
+
+		// Warning the element adapter can possibly be null (see: https://bugs.eclipse.org/bugs/show_bug.cgi?id=353129)
+		if(viewDescriptor.getElementAdapter() == null) {
+			return false;
+		}
+
 		// Try to adapt the descriptor ElementAdapter in EObject
-		EObject element = (EObject) viewDescriptor.getElementAdapter().getAdapter(EObject.class);
-		IElementType elementType = (IElementType) viewDescriptor.getElementAdapter().getAdapter(IElementType.class);
-		
+		EObject element = (EObject)viewDescriptor.getElementAdapter().getAdapter(EObject.class);
+		IElementType elementType = (IElementType)viewDescriptor.getElementAdapter().getAdapter(IElementType.class);
+
 		SemanticElementAdapter semanticAdapter = new SemanticElementAdapter(element, elementType);
-		
+
 		// Use the semanticAdapter instead of view descriptor element adapter to avoid the use of provides(ViewForKind) method
 		// from ViewProvider which is incorrectly implemented in GMF Tooling generated editors (other editors may have undesired side-effect on each-other).
-		
-		return ViewService.getInstance().provides(
-			viewDescriptor.getViewKind(),
-			semanticAdapter,
-			containerView,
-			viewDescriptor.getSemanticHint(),
-			viewDescriptor.getIndex(),
-			viewDescriptor.isPersisted(),
-			viewDescriptor.getPreferencesHint());
+
+		return ViewService.getInstance().provides(viewDescriptor.getViewKind(), semanticAdapter, containerView, viewDescriptor.getSemanticHint(), viewDescriptor.getIndex(), viewDescriptor.isPersisted(), viewDescriptor.getPreferencesHint());
 	}
 
 
