b346b929d2581d0c71b9ebbba8473d0943eae9aa
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-07-29 08:56:43 +0000

    Merged Branch 0.8.X changes 4860::5149 for oep.diagram.profile
    NEW - bug 330250: [Profile Diagram] Undo after adding metaclass (model keeps consistent)
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=330250

--- plugins/uml/org.eclipse.papyrus.diagram.profile/custom-src/org/eclipse/papyrus/diagram/profile/custom/commands/CustomSemanticCreateCommand.java
+++ plugins/uml/org.eclipse.papyrus.diagram.profile/custom-src/org/eclipse/papyrus/diagram/profile/custom/commands/CustomSemanticCreateCommand.java
@@ -23,19 +23,16 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.commands.CompoundCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.common.core.command.CompositeCommand;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
-import org.eclipse.gmf.runtime.diagram.core.edithelpers.CreateElementRequestAdapter;
-import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
-import org.eclipse.papyrus.diagram.common.Activator;
 import org.eclipse.papyrus.diagram.profile.custom.messages.Messages;
 import org.eclipse.papyrus.diagram.profile.custom.requests.CustomCreateElementRequestAdapter;
 import org.eclipse.papyrus.diagram.profile.custom.ui.dialog.ChooseSetMetaclassDialog;
 import org.eclipse.papyrus.profile.ImageManager;
-import org.eclipse.papyrus.ui.toolbox.LookForElement;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -53,7 +50,7 @@ import org.eclipse.uml2.uml.UMLPackage;
  * @author VL222926
  * 
  */
-public class CustomSemanticCreateCommand extends org.eclipse.gmf.runtime.diagram.ui.commands.SemanticCreateCommand {
+public class CustomSemanticCreateCommand extends AbstractTransactionalCommand {
 
 	/**
 	 * the requestAdapter
@@ -70,28 +67,35 @@ public class CustomSemanticCreateCommand extends org.eclipse.gmf.runtime.diagram
 	 */
 	private Profile profile;
 
-
 	/**
-	 * the request is here to allow the creation of the requestAdapterList
 	 * 
-	 * @param requestAdapterNEW
+	 * Constructor.
 	 * 
-	 * @param requestAdapterList2
+	 * @param domain
+	 * @param requestAdapter
+	 * @param realSemanticCommand
+	 * @param profile
 	 */
-	public CustomSemanticCreateCommand(CustomCreateElementRequestAdapter requestAdapter, Command realSemanticCommand, Profile profile) {
-		// /!\ warning 
-		// the first argument is here to be conform with the metaclass's constructor. It isn't used!
-		super(new CreateElementRequestAdapter(requestAdapter.getRequest(0)), realSemanticCommand);
+	public CustomSemanticCreateCommand(TransactionalEditingDomain domain, CustomCreateElementRequestAdapter requestAdapter, Profile profile) {
+		super(domain, "Create Custom Semantic Command for Metaclass", null); //$NON-NLS-1$
 		this.profile = profile;
 		this.customRequestAdapter = requestAdapter;
 	}
 
-
-
+	/**
+	 * 
+	 * @see org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand#doExecuteWithResult(org.eclipse.core.runtime.IProgressMonitor,
+	 *      org.eclipse.core.runtime.IAdaptable)
+	 * 
+	 * @param progressMonitor
+	 * @param info
+	 * @return
+	 * @throws ExecutionException
+	 */
 	@Override
 	protected CommandResult doExecuteWithResult(IProgressMonitor progressMonitor, IAdaptable info) throws ExecutionException {
 		CommandResult result = CommandResult.newCancelledCommandResult();
-		ChooseSetMetaclassDialog cmcd = new ChooseSetMetaclassDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), profile);
+		ChooseSetMetaclassDialog cmcd = new ChooseSetMetaclassDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), this.profile);
 
 		// opening of a message box indicating profile application
 		final Shell message = new Shell(new Shell(), SWT.APPLICATION_MODAL | SWT.SHELL_TRIM);
@@ -112,7 +116,11 @@ public class CustomSemanticCreateCommand extends org.eclipse.gmf.runtime.diagram
 		if(cmcd.getReturnCode() == IStatus.OK) {
 			this.addedMetaclasses = cmcd.getSelectedElements();
 			if(!this.addedMetaclasses.isEmpty()) {
-				result = applyModification();
+				ICommand createElementImportCommand = getImportElementCommand();
+				if(createElementImportCommand != null) {
+					createElementImportCommand.execute(progressMonitor, info);
+				}
+				return CommandResult.newOKCommandResult();
 			}
 			//else we return a canceled command result (avoid an Undo for nothing for the user!)
 		}
@@ -121,33 +129,31 @@ public class CustomSemanticCreateCommand extends org.eclipse.gmf.runtime.diagram
 	}
 
 	/**
-	 * Apply the modifications to the Profile : import required metaclasses
 	 * 
 	 * @return
-	 *         the command result
+	 *         the command to create the new ElementImport for each new import metaclass
 	 */
-	private CommandResult applyModification() {
-		CommandResult result = null;
-		CompoundCommand addingCommand = new CompoundCommand();
+	private ICommand getImportElementCommand() {
+		CompositeCommand addindCommand = new CompositeCommand("Add ImportElement Command"); //$NON-NLS-1$
 		//----Obtain the actual imported metaclasses
-		EList<?> metaclassSet = profile.getReferencedMetaclasses();
+		EList<?> metaclassSet = this.profile.getReferencedMetaclasses();
 
 		//we add the new metaclasses
-		for(int i = 0; i < addedMetaclasses.size(); i++) {
+		for(int i = 0; i < this.addedMetaclasses.size(); i++) {
 			//we need to import the metaclass
-			if(!metaclassSet.contains(addedMetaclasses.get(i))) {
+			if(!metaclassSet.contains(this.addedMetaclasses.get(i))) {
 				//we create an ElementImport for the new metaclass
 				ElementImport ei = UMLFactory.eINSTANCE.createElementImport();
 				//we create the class		
 
 
-				ei.setImportedElement((PackageableElement)addedMetaclasses.get(i));
-				ei.setAlias(((NamedElement)addedMetaclasses.get(i)).getName());
+				ei.setImportedElement((PackageableElement)this.addedMetaclasses.get(i));
+				ei.setAlias(((NamedElement)this.addedMetaclasses.get(i)).getName());
 				//					addedMetaclasses.add(addedMetaclasses.get(i));
 				ElementImport added = ei;
 
 				//we need make the import only if it doesn't exist yet!
-				EList<ElementImport> list = profile.getElementImports();
+				EList<ElementImport> list = this.profile.getElementImports();
 
 				//We search if this metaclass is imported yet
 				//ListIterator<ElementImport> iterator = list.listIterator();
@@ -163,31 +169,26 @@ public class CustomSemanticCreateCommand extends org.eclipse.gmf.runtime.diagram
 				}
 
 				//we add the metaclass in metaclassReference
-				SetRequest setRequest = new SetRequest(profile, UMLPackage.eINSTANCE.getProfile_MetaclassReference(), added);
-				org.eclipse.papyrus.service.edit.service.IElementEditService provider = org.eclipse.papyrus.service.edit.service.ElementEditServiceUtils.getCommandProvider(profile);
+				SetRequest setRequest = new SetRequest(this.profile, UMLPackage.eINSTANCE.getProfile_MetaclassReference(), added);
+				org.eclipse.papyrus.service.edit.service.IElementEditService provider = org.eclipse.papyrus.service.edit.service.ElementEditServiceUtils.getCommandProvider(this.profile);
 
 				if(provider != null) {
 					ICommand editCommand = null;
 					editCommand = provider.getEditCommand(setRequest);
-					if(editCommand != null && editCommand.canExecute()) {
-						addingCommand.add(new ICommandProxy(editCommand));
+					if(editCommand != null) {
+						addindCommand.add(editCommand);
 					}
 				}
 			} else {
 				//the metaclass is already imported, nothing to do here!
 			}
-			this.customRequestAdapter.setNewElement((EObject)addedMetaclasses.get(i));
+			this.customRequestAdapter.setNewElement((EObject)this.addedMetaclasses.get(i));
 		}
-		if(addingCommand != null && addingCommand.canExecute()) {
-			try {
-				LookForElement.getCommandStack().execute(addingCommand);
-				//				addingCommand.execute();
-				result = CommandResult.newOKCommandResult(this.customRequestAdapter);
-			} catch (Exception e) {
-				Activator.log.error(e);
-			}
+		if(!addindCommand.isEmpty()) {
+			return addindCommand;
 		}
-		return result;
+		return null;
 	}
+
 
 }
