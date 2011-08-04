372c0c5f06fe753a2e53615b9a83e7b1d166489e
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-08-04 09:36:45 +0000

    NEW - bug 348693: [SysML Internal Block Diagram] Creation of Comment and Constraint in Block fails https://bugs.eclipse.org/bugs/show_bug.cgi?id=348693

---
+++ plugins/sysml/org.eclipse.papyrus.sysml.diagram.internalblock/src-gen/org/eclipse/papyrus/sysml/diagram/internalblock/provider/InheritedCompositeDiagramEditPartProvider.java
@@ -0,0 +1,80 @@
+/*****************************************************************************
+ * Copyright (c) 2011 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		
+ *		CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.sysml.diagram.internalblock.provider;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.service.IOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.editpart.CreateGraphicEditPartOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.editpart.IEditPartOperation;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.papyrus.diagram.composite.providers.UMLEditPartProvider;
+
+public class InheritedCompositeDiagramEditPartProvider extends UMLEditPartProvider {
+
+	@Override
+	public synchronized boolean provides(IOperation operation) {
+		if(operation instanceof CreateGraphicEditPartOperation) {
+			View view = ((IEditPartOperation)operation).getView();
+
+			// Ensure current diagram is a InternalBlock Diagram
+			if(!ElementTypes.DIAGRAM_ID.equals(view.getDiagram().getType())) {
+				return false;
+			}
+
+			// Test supported inherited types
+			EObject eobject = view.getElement();
+
+			/** Nodes (and ChildLabelNodes) *********** */
+			if(eobject instanceof org.eclipse.uml2.uml.Class) {
+				return true;
+			}
+			if(eobject instanceof org.eclipse.uml2.uml.Port) {
+				return true;
+			}
+			if(eobject instanceof org.eclipse.uml2.uml.Property) {
+				return true;
+			}
+			if(eobject instanceof org.eclipse.uml2.uml.Comment) {
+				return true;
+			}
+			if(eobject instanceof org.eclipse.uml2.uml.Constraint) {
+				return true;
+			}
+
+			/** Edges *********** */
+			if(eobject instanceof org.eclipse.uml2.uml.Connector) {
+				return true;
+			}
+
+			// Additional test needed here to decide whether to support Feature type links.
+			// As feature type link are not related to a MetaClass from the domain model
+			// they are not already handled by previous tests.
+			// Also concerns NotationType.
+			String hint = view.getType();
+
+			/** Edges (Feature) : COMMENT_ANNOTATED_ELEMENT *********** */
+			if(ElementTypes.COMMENT_ANNOTATED_ELEMENT.getSemanticHint().equals(hint)) {
+				return true;
+			}
+			/** Edges (Feature) : CONSTRAINT_CONSTRAINED_ELEMENT *********** */
+			if(ElementTypes.CONSTRAINT_CONSTRAINED_ELEMENT.getSemanticHint().equals(hint)) {
+				return true;
+			}
+
+
+
+		}
+		return false;
+	}
+}
