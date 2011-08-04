e96f35264d600f3b1588db74932f3f4707950428
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-08-04 09:21:10 +0000

    NEW - bug 353859: [Package Diagram] The dialog does not open on double-click for some elements https://bugs.eclipse.org/bugs/show_bug.cgi?id=353859

---
+++ plugins/uml/org.eclipse.papyrus.uml.diagram.pkg/src/org/eclipse/papyrus/uml/diagram/pkg/provider/CustomEditPolicyProvider.java
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ * Copyright (c) 2011 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		
+ *		CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.uml.diagram.pkg.provider;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.papyrus.diagram.common.editpolicies.NavigationEditPolicy;
+
+public class CustomEditPolicyProvider extends PackageDiagramEditPolicyProvider {
+
+	public void createEditPolicies(EditPart editPart) {
+		super.createEditPolicies(editPart);
+		editPart.installEditPolicy(NavigationEditPolicy.NAVIGATION_POLICY, new NavigationEditPolicy());
+	}
+
+}
