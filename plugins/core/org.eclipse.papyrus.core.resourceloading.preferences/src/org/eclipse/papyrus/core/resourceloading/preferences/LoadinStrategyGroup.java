045bd96e73e05d7d8851723fe58bce486c5587de
vhemery <vhemery@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-08-25 09:12:01 +0000

    NEW - bug 354220:  [collaborative work] ability to load/unload a submodel from model explorer https://bugs.eclipse.org/bugs/show_bug.cgi?id=354220
    Update loading strategies in 0.8.X branch

--- plugins/core/org.eclipse.papyrus.core.resourceloading.preferences/src/org/eclipse/papyrus/core/resourceloading/preferences/LoadinStrategyGroup.java
+++ plugins/core/org.eclipse.papyrus.core.resourceloading.preferences/src/org/eclipse/papyrus/core/resourceloading/preferences/LoadinStrategyGroup.java
@@ -16,7 +16,7 @@ package org.eclipse.papyrus.core.resourceloading.preferences;
 import java.util.Map;
 
 import org.eclipse.jface.dialogs.DialogPage;
-import org.eclipse.jface.preference.ComboFieldEditor;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
 import org.eclipse.papyrus.core.resourceloading.impl.ProxyManager;
 import org.eclipse.papyrus.preferences.ui.AbstractGroup;
 import org.eclipse.swt.SWT;
@@ -26,47 +26,47 @@ import org.eclipse.swt.widgets.Group;
 
 public class LoadinStrategyGroup extends AbstractGroup {
 
-	private static final String LABEL_LOADING_STRATEGY = "Loading strategy: ";
+    private static final String LABEL_LOADING_STRATEGY = "Loading strategy :";
 
-	private static final String SELECT_LOADING_STRATEGY = "Select the loading strategy when model is opened";
+    private static final String SELECT_LOADING_STRATEGY = "Select the loading strategy when model is opened";
 
-	private ComboFieldEditor fComboSize;
+    private RadioGroupFieldEditor fStrategyRadio;
 
-	public LoadinStrategyGroup(Composite parent, String title, DialogPage dialogPage) {
-		super(parent, title, dialogPage);
-		createContent(parent);
-	}
+    public LoadinStrategyGroup(Composite parent, String title, DialogPage dialogPage) {
+        super(parent, title, dialogPage);
+        createContent(parent);
+    }
 
-	public void createContent(Composite parent) {
-		Group group = new Group(parent, SWT.SCROLL_PAGE);
-		group.setLayout(new GridLayout());
-		group.setText(SELECT_LOADING_STRATEGY);
-		fComboSize = createComboSize(group);
-		addFieldEditor(fComboSize);
-	}
+    public void createContent(Composite parent) {
+        Group group = new Group(parent, SWT.SCROLL_PAGE);
+        group.setLayout(new GridLayout());
+        group.setText(SELECT_LOADING_STRATEGY);
+        fStrategyRadio = createStrategyRadio(group);
+        addFieldEditor(fStrategyRadio);
+    }
 
-	private static ComboFieldEditor createComboSize(Group group) {
-		String[][] values = getValuesFromExtensionPoint();
-		ComboFieldEditor combo = new ComboFieldEditor(ICorePreferenceConstants.PREF_CORE_DEFINE_LOADING_STRATEGY, LABEL_LOADING_STRATEGY, values, group);
-		return combo;
-	}
+    private static RadioGroupFieldEditor createStrategyRadio(Group group) {
+        String[][] values = getValuesFromExtensionPoint();
+        RadioGroupFieldEditor radio = new RadioGroupFieldEditor(ICorePreferenceConstants.PREF_CORE_DEFINE_LOADING_STRATEGY, LABEL_LOADING_STRATEGY, 1, values, group);
+        return radio;
+    }
 
-	/**
-	 * Gets the strategies value from extension point.
-	 * 
-	 * @return the strategies value
-	 */
-	private static String[][] getValuesFromExtensionPoint() {
-		Map<Integer, String> allStrategies = ProxyManager.getAllStrategies();
-		String[][] result = new String[allStrategies.size()][2];
-		int i = 0;
-		for(Integer val : allStrategies.keySet()) {
-			result[i][0] = allStrategies.get(val);
-			result[i][1] = String.valueOf(val);
-			i++;
-		}
-		return result;
-	}
+    /**
+     * Gets the strategies value from extension point.
+     * 
+     * @return the strategies value
+     */
+    private static String[][] getValuesFromExtensionPoint() {
+        Map<Integer, String> allStrategies = ProxyManager.getAllStrategies();
+        String[][] result = new String[allStrategies.size()][2];
+        int i = 0;
+        for(Integer val : allStrategies.keySet()) {
+            result[i][0] = allStrategies.get(val);
+            result[i][1] = String.valueOf(val);
+            i++;
+        }
+        return result;
+    }
 
 
 
