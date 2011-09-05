c0f871066dc9acc6a131eeba04acd21accf32867
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-09-05 09:28:28 +0000

    356180: [Property View] When modifying a property for a multi-selection, more than one command is executed https://bugs.eclipse.org/bugs/show_bug.cgi?id=356180

---
+++ plugins/uml/org.eclipse.papyrus.properties.uml/src/org/eclipse/papyrus/properties/uml/databinding/CommandBasedObservableValue.java
@@ -0,0 +1,8 @@
+package org.eclipse.papyrus.properties.uml.databinding;
+
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+
+
+public interface CommandBasedObservableValue extends CommandBasedObservable, IObservableValue {
+
+}
