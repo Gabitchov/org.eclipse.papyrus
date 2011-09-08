f1151e9a18f1b9caaa067c443097a0c2f0c9a72b
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-09-08 09:50:38 +0000

    NEW - bug 357049: [Table Editor][SysM] Direction for Facet FlowPort is not correctly displayed https://bugs.eclipse.org/bugs/show_bug.cgi?id=357049

--- plugins/sysml/org.eclipse.papyrus.sysml.facets/src/org/eclipse/papyrus/sysml/facets/portandflows/query/value/setter/SetFlowPortDirectionQuery.java
+++ plugins/sysml/org.eclipse.papyrus.sysml.facets/src/org/eclipse/papyrus/sysml/facets/portandflows/query/value/setter/SetFlowPortDirectionQuery.java
@@ -1,4 +1,16 @@
+/*****************************************************************************
+ * Copyright (c) 2011 CEA LIST.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+ *****************************************************************************/
 package org.eclipse.papyrus.sysml.facets.portandflows.query.value.setter;
+
 import org.eclipse.emf.common.util.Enumerator;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
@@ -10,8 +22,8 @@ import org.eclipse.papyrus.core.services.ServiceException;
 import org.eclipse.papyrus.core.utils.GMFtoEMFCommandWrapper;
 import org.eclipse.papyrus.core.utils.ServiceUtilsForActionHandlers;
 import org.eclipse.papyrus.gmf.diagram.common.commands.IdentityCommandWithNotification;
+import org.eclipse.papyrus.sysml.facets.extendedsysml.extendedsysml.portandflows.NAFlowPortDirection;
 import org.eclipse.papyrus.sysml.facets.messages.Messages;
-import org.eclipse.papyrus.sysml.portandflows.FlowDirection;
 import org.eclipse.papyrus.sysml.portandflows.FlowPort;
 import org.eclipse.papyrus.sysml.portandflows.PortandflowsPackage;
 import org.eclipse.papyrus.sysml.util.ElementUtil;
@@ -19,15 +31,16 @@ import org.eclipse.papyrus.ui.toolbox.notification.Type;
 import org.eclipse.uml2.uml.Port;
 
 /** Query to qet the attribute "Direction" of the FlowPort */
-public class SetFlowPortDirectionQuery implements IJavaModelQueryWithEditingDomain<Port, FlowDirection> {
+public class SetFlowPortDirectionQuery implements IJavaModelQueryWithEditingDomain<Port, Enumerator> {
 
-	public FlowDirection evaluate(final Port context, final ParameterValueList parameterValues)
-			throws ModelQueryExecutionException {
+	public Enumerator evaluate(final Port context, final ParameterValueList parameterValues) throws ModelQueryExecutionException {
 		//nothing to do
 		return null;
 	}
 
-	public FlowDirection evaluate(Port context, ParameterValueList parameterValues, EditingDomain editingDomain) throws ModelQueryExecutionException {
+
+	@Override
+	public Enumerator evaluate(Port context, ParameterValueList parameterValues, EditingDomain editingDomain) throws ModelQueryExecutionException {
 		FlowPort flowPort = ElementUtil.getStereotypeApplication(context, FlowPort.class);
 
 		if(flowPort != null) {
@@ -37,24 +50,25 @@ public class SetFlowPortDirectionQuery implements IJavaModelQueryWithEditingDoma
 
 				// Retrieve new value from parameter and update if the property value has changed.
 				Enumerator newValue = (parameterValues.get(0).getValue() instanceof Enumerator) ? (Enumerator)parameterValues.get(0).getValue() : null;
-				if(newValue != flowPort.getDirection()) {
-					try {
-						TransactionalEditingDomain domain = ServiceUtilsForActionHandlers.getInstance().getTransactionalEditingDomain();
-						SetCommand command = new SetCommand(domain, flowPort, PortandflowsPackage.eINSTANCE.getFlowPort_Direction(), newValue);
-						if(command.canExecute()) {
-							domain.getCommandStack().execute(command);
+				if(!(newValue instanceof NAFlowPortDirection)) {//we do nothing in this case
+					if(newValue != flowPort.getDirection()) {
+						try {
+							TransactionalEditingDomain domain = ServiceUtilsForActionHandlers.getInstance().getTransactionalEditingDomain();
+							SetCommand command = new SetCommand(domain, flowPort, PortandflowsPackage.eINSTANCE.getFlowPort_Direction(), newValue);
+							if(command.canExecute()) {
+								domain.getCommandStack().execute(command);
+							}
+
+						} catch (ServiceException e) {
+							throw new ModelQueryExecutionException(e);
 						}
 
-					} catch (ServiceException e) {
-						throw new ModelQueryExecutionException(e);
 					}
-
 				}
 			}
 		} else {
 			editingDomain.getCommandStack().execute(new GMFtoEMFCommandWrapper(new IdentityCommandWithNotification(Messages.SetRequirementTextQuery_AssignmentCantBeDone, "The edited element is not a SysML FlowPort.", Type.ERROR)));
 		}
-
 		return null;
 	}
 }
