7a8f79b6f63d2ae2bb604941a4e283e325afd7c0
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-09-22 11:27:52 +0000

    NEW - bug 358538: [Model Explorer] [Import] the Import actions in the Model Explorer should be regrouped in the same sub-menu https://bugs.eclipse.org/bugs/show_bug.cgi?id=358538
    Remove the import action in the plugin.xml

--- plugins/uml/org.eclipse.papyrus.uml.import/src/org/eclipse/papyrus/importt/Activator.java
+++ plugins/uml/org.eclipse.papyrus.uml.import/src/org/eclipse/papyrus/importt/Activator.java
@@ -20,7 +20,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.import"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.uml.import"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
