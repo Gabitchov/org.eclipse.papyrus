898e1194f2df895d23d506ab9a40d640d01d60e1
tfaure <tfaure@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-09-29 16:34:07 +0000

    Integration of work for prevent call operation action when a featuresetter is set

--- plugins/uml/org.eclipse.papyrus.diagram.activity/src/org/eclipse/papyrus/diagram/activity/edit/commands/CallOperationActionCreateCommand.java
+++ plugins/uml/org.eclipse.papyrus.diagram.activity/src/org/eclipse/papyrus/diagram/activity/edit/commands/CallOperationActionCreateCommand.java
@@ -13,6 +13,8 @@
  *****************************************************************************/
 package org.eclipse.papyrus.diagram.activity.edit.commands;
 
+import java.util.Map;
+
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -24,16 +26,19 @@ import org.eclipse.gmf.runtime.emf.type.core.IElementType;
 import org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.papyrus.diagram.activity.edit.commands.util.CreateCommandUtil;
 import org.eclipse.papyrus.diagram.activity.edit.dialogs.CreateCallOperationActionDialog;
 import org.eclipse.papyrus.diagram.activity.providers.ElementInitializers;
+import org.eclipse.papyrus.diagram.common.service.palette.IFeatureSetterAspectAction.IFeatureSetterAspectActionUtil;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.uml2.uml.Activity;
 import org.eclipse.uml2.uml.CallOperationAction;
 import org.eclipse.uml2.uml.Operation;
 import org.eclipse.uml2.uml.UMLFactory;
+import org.eclipse.uml2.uml.UMLPackage;
 
 /**
  * @generated
@@ -53,7 +58,8 @@ public class CallOperationActionCreateCommand extends EditElementCommand {
 	/**
 	 * @generated
 	 */
-	public CallOperationActionCreateCommand(CreateElementRequest req, EObject eObject) {
+	public CallOperationActionCreateCommand(CreateElementRequest req,
+			EObject eObject) {
 		super(req.getLabel(), null, req);
 		this.eObject = eObject;
 		this.eClass = eObject != null ? eObject.eClass() : null;
@@ -62,7 +68,8 @@ public class CallOperationActionCreateCommand extends EditElementCommand {
 	/**
 	 * @generated
 	 */
-	public static CallOperationActionCreateCommand create(CreateElementRequest req, EObject eObject) {
+	public static CallOperationActionCreateCommand create(
+			CreateElementRequest req, EObject eObject) {
 		return new CallOperationActionCreateCommand(req, eObject);
 	}
 
@@ -80,12 +87,12 @@ public class CallOperationActionCreateCommand extends EditElementCommand {
 	 */
 	protected EObject getElementToEdit() {
 
-
-		EObject container = ((CreateElementRequest)getRequest()).getContainer();
-		if(container instanceof View) {
-			container = ((View)container).getElement();
+		EObject container = ((CreateElementRequest) getRequest())
+				.getContainer();
+		if (container instanceof View) {
+			container = ((View) container).getElement();
 		}
-		if(container != null) {
+		if (container != null) {
 			return container;
 		}
 		return eObject;
@@ -95,65 +102,85 @@ public class CallOperationActionCreateCommand extends EditElementCommand {
 	 * @generated NOT check that there is a correct model container.
 	 */
 	public boolean canExecute() {
-		//check that there is a correct model container
-		return CreateCommandUtil.canCreateNode(getRequest(), getElementToEdit());
+		// check that there is a correct model container
+		return CreateCommandUtil
+				.canCreateNode(getRequest(), getElementToEdit());
 	}
 
 	/**
 	 * @generated NOT use the initialization popup, set appropriate parents
 	 */
-	protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+	protected CommandResult doExecuteWithResult(IProgressMonitor monitor,
+			IAdaptable info) throws ExecutionException {
 
 		// get the activity containing the new element
 		Activity parentActivity = null;
 		EObject parent = getElementToEdit();
-		while(parent != null && parentActivity == null) {
-			if(parent instanceof Activity) {
-				parentActivity = (Activity)parent;
+		while (parent != null && parentActivity == null) {
+			if (parent instanceof Activity) {
+				parentActivity = (Activity) parent;
 			}
 			parent = parent.eContainer();
 		}
 
-		CallOperationAction newElement = UMLFactory.eINSTANCE.createCallOperationAction();
-
-		CreateCallOperationActionDialog dialog = new CreateCallOperationActionDialog(Display.getDefault().getActiveShell(), parentActivity);
-		if(IDialogConstants.OK_ID == dialog.open()) {
-			// initialize the invoked element (no need to use a command, since action is being created)
-			EObject operation = dialog.getSelectedInvoked();
-			if(operation instanceof Operation) {
-				newElement.setOperation((Operation)operation);
+		CallOperationAction newElement = UMLFactory.eINSTANCE
+				.createCallOperationAction();
+		if (isOperationAlreadyManaged()) {
+			CreateCallOperationActionDialog dialog = new CreateCallOperationActionDialog(
+					Display.getDefault().getActiveShell(), parentActivity);
+			if (IDialogConstants.OK_ID == dialog.open()) {
+				// initialize the invoked element (no need to use a command,
+				// since action is being created)
+				EObject operation = dialog.getSelectedInvoked();
+				if (operation instanceof Operation) {
+					newElement.setOperation((Operation) operation);
+				}
+				// initialize synchronous
+				newElement.setIsSynchronous(dialog.getIsSynchronous());
+			} else {
+				return CommandResult.newCancelledCommandResult();
 			}
-			// initialize synchronous
-			newElement.setIsSynchronous(dialog.getIsSynchronous());
-		} else {
-			return CommandResult.newCancelledCommandResult();
 		}
-
 		// set appropriate parents
-		if(!CreateCommandUtil.setNodeParents(newElement, getRequest(), getElementToEdit())) {
+		if (!CreateCommandUtil.setNodeParents(newElement, getRequest(),
+				getElementToEdit())) {
 			return CommandResult.newCancelledCommandResult();
 		}
-		//		Activity owner = (Activity)getElementToEdit();
-		//		owner.getNodes().add(newElement);
+		// Activity owner = (Activity)getElementToEdit();
+		// owner.getNodes().add(newElement);
 
-		ElementInitializers.getInstance().init_CallOperationAction_3010(newElement);
+		ElementInitializers.getInstance().init_CallOperationAction_3010(
+				newElement);
 
 		doConfigure(newElement, monitor, info);
 
-		((CreateElementRequest)getRequest()).setNewElement(newElement);
+		((CreateElementRequest) getRequest()).setNewElement(newElement);
 		return CommandResult.newOKCommandResult(newElement);
+	}
+
+	private boolean isOperationAlreadyManaged() {
+		IEditCommandRequest aRequest = getRequest();
+		Map map = aRequest.getParameters();
+		return !IFeatureSetterAspectActionUtil.areFeaturesManaged(getRequest(),
+				UMLPackage.Literals.CALL_OPERATION_ACTION__OPERATION);
 	}
 
 	/**
 	 * @generated
 	 */
-	protected void doConfigure(CallOperationAction newElement, IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
-		IElementType elementType = ((CreateElementRequest)getRequest()).getElementType();
-		ConfigureRequest configureRequest = new ConfigureRequest(getEditingDomain(), newElement, elementType);
-		configureRequest.setClientContext(((CreateElementRequest)getRequest()).getClientContext());
+	protected void doConfigure(CallOperationAction newElement,
+			IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+		IElementType elementType = ((CreateElementRequest) getRequest())
+				.getElementType();
+		ConfigureRequest configureRequest = new ConfigureRequest(
+				getEditingDomain(), newElement, elementType);
+		configureRequest.setClientContext(((CreateElementRequest) getRequest())
+				.getClientContext());
 		configureRequest.addParameters(getRequest().getParameters());
-		ICommand configureCommand = elementType.getEditCommand(configureRequest);
-		if(configureCommand != null && configureCommand.canExecute()) {
+		ICommand configureCommand = elementType
+				.getEditCommand(configureRequest);
+		if (configureCommand != null && configureCommand.canExecute()) {
 			configureCommand.execute(monitor, info);
 		}
 	}
