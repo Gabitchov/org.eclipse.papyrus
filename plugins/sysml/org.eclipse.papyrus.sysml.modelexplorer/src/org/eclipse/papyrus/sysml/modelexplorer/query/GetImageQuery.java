5a92d3a73dcd4209d500c7e5f6774fee21dbf730
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2011-10-11 09:09:06 +0000

    ASSIGNED - bug 360451: Smart atomic flow-port icons are sometimes replaced by red squares https://bugs.eclipse.org/bugs/show_bug.cgi?id=360451

--- plugins/sysml/org.eclipse.papyrus.sysml.modelexplorer/src/org/eclipse/papyrus/sysml/modelexplorer/query/GetImageQuery.java
+++ plugins/sysml/org.eclipse.papyrus.sysml.modelexplorer/src/org/eclipse/papyrus/sysml/modelexplorer/query/GetImageQuery.java
@@ -1,5 +1,6 @@
 package org.eclipse.papyrus.sysml.modelexplorer.query;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.facet.infra.query.core.exception.ModelQueryExecutionException;
 import org.eclipse.emf.facet.infra.query.core.java.IJavaModelQuery;
 import org.eclipse.emf.facet.infra.query.core.java.ParameterValueList;
@@ -14,20 +15,26 @@ public class GetImageQuery implements IJavaModelQuery<Element, String> {
 
 	public String evaluate(final Element context, final ParameterValueList parameterValues) throws ModelQueryExecutionException {
 
-		String icon_relative_path = "";
+		String icon_path = "";
 		if(!context.getAppliedStereotypes().isEmpty()) {
 			Stereotype first_stereotype = context.getAppliedStereotypes().get(0);
 
 			if(!first_stereotype.getIcons().isEmpty()) {
 				org.eclipse.uml2.uml.Image icon = ElementUtil.getStereotypeImage(context, first_stereotype, "icon");
-				icon_relative_path = icon.getLocation();
+				icon_path = icon.getLocation();
 			}
 		}
 
 		String image_path = "";
-		if(!"".equals(icon_relative_path)) {
-			image_path = sysml_plugin_path + icon_relative_path;
+		URI iconURI = URI.createURI(icon_path);
+		if(!"".equals(icon_path)) {
+			if(iconURI.isRelative()) {
+				image_path = sysml_plugin_path + icon_path;
+			} else {
+				image_path = iconURI.toPlatformString(true);
+			}
 		}
+
 		return image_path;
 	}
 }
