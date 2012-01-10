c2b3d3227dbf71178784b8eda2b1e9e13b87ff86
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-01-10 13:42:29 +0000

    NEW - Bug 365919: [Architecture - SVN - Build] Rewrite the Build https://bugs.eclipse.org/bugs/show_bug.cgi?id=365919
    build publishing

--- releng/org.eclipse.mdt.papyrus.releng.buckminster/serverConfig/promoteFunctions.sh
+++ releng/org.eclipse.mdt.papyrus.releng.buckminster/serverConfig/promoteFunctions.sh
@@ -93,7 +93,7 @@ function promote() {
 	echo "[$DATE] enabling download statistics"
 	$ADD_DOWNLOAD_STATS "$_updateSite"
 	echo "[$DATE] setting access rights"
-	chmod -R 755 "$buildsDir"
+	chmod -R 755 "$buildsDir"/${_zipName/%\.zip/}
 	chmod -R 755 "$_updateSite"
 	
 	# copy milestone and release builds to the archive, and tag the build
