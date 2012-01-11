e26c89fa45f41c62b3c89ee1b4e515bcd3200ca9
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-01-11 16:07:28 +0000

    NEW - Bug 365919: [Architecture - SVN - Build] Rewrite the Build https://bugs.eclipse.org/bugs/show_bug.cgi?id=365919
    extras build

---
+++ releng/org.eclipse.mdt.papyrus.releng.buckminster/papyrus-trunk-extra-nightly/build-before.sh
@@ -0,0 +1,20 @@
+SVN=/usr/local/bin/svn
+promoteDirName=/opt/public/modeling/mdt/papyrus/papyrus-trunk-extra-nightly/promoteDirName
+RELENG_PROJECT="sourceTree/releng/org.eclipse.mdt.papyrus.releng.buckminster"
+
+echo "building in " $WORKSPACE
+
+echo "cleaning previous results"
+rm -f ?20*.zip
+
+echo "displaying java version"
+java -version
+#ls -al /shared/common/jdk-1.5.0-22.x86_64/jre/bin
+
+COMPACT_BUILD_ID="${BUILD_ID//[-_]}"
+COMPACT_BUILD_ID="${COMPACT_BUILD_ID:0:12}"
+FULL_BUILD_ID=${BUILD_TYPE}${COMPACT_BUILD_ID}
+echo $FULL_BUILD_ID > $promoteDirName
+
+revision=$($SVN info svn://dev.eclipse.org/svnroot/modeling/org.eclipse.mdt.papyrus | grep '^Revision:' | awk '{print $2}')
+echo "$revision" > "$WORKSPACE/revision.txt"
