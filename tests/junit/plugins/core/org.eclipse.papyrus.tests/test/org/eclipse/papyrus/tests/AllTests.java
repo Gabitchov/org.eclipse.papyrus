5e8a7a77b8e70958ef5299c36a48dacc1bd40801
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-01-26 09:55:54 +0000

    365919: [Architecture - SVN - Build] Rewrite the Build https://bugs.eclipse.org/bugs/show_bug.cgi?id=365919
    Marte and layout are extra features

--- tests/junit/plugins/core/org.eclipse.papyrus.tests/test/org/eclipse/papyrus/tests/AllTests.java
+++ tests/junit/plugins/core/org.eclipse.papyrus.tests/test/org/eclipse/papyrus/tests/AllTests.java
@@ -37,7 +37,7 @@ public class AllTests {
 		/* **************** plugins *********************** */
 		/* core */
 		// {oep}.core
-		suiteClasses.add(new FragmentTestSuiteClass(org.eclipse.papyrus.infra.core.Activator.PLUGIN_ID, "org.eclipse.papyrus.core.tests.AllTests"));
+		suiteClasses.add(new FragmentTestSuiteClass(org.eclipse.papyrus.infra.core.Activator.PLUGIN_ID, "org.eclipse.papyrus.infra.core.tests.AllTests"));
 		// {oep}.sasheditor.tests
 		//		new FragmentTestSuiteClass(org.eclipse.papyrus.infra.core.sasheditor.Activator.PLUGIN_ID, "org.eclipse.papyrus.sasheditor.tests.AllTests"),
 		// {oep}.core.resourceloading.tests
