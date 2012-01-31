ab417725ecfc3660a7373412319969624bd32508
cdumoulin <cdumoulin@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-01-31 10:37:38 +0000

    RESOLVED - bug 370129: [Core] ServicesRegistry should be accessible from any EObject attached to a Papyrus ResourceSet https://bugs.eclipse.org/bugs/show_bug.cgi?id=370129
    Update documentation

--- plugins/infra/emf/org.eclipse.papyrus.infra.emf/src/org/eclipse/papyrus/infra/emf/utils/ServiceUtilsForResource.java
+++ plugins/infra/emf/org.eclipse.papyrus.infra.emf/src/org/eclipse/papyrus/infra/emf/utils/ServiceUtilsForResource.java
@@ -14,7 +14,9 @@
 
 package org.eclipse.papyrus.infra.emf.utils;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.papyrus.infra.core.services.ServiceException;
 import org.eclipse.papyrus.infra.core.services.ServiceNotFoundException;
@@ -22,6 +24,22 @@ import org.eclipse.papyrus.infra.core.services.ServicesRegistry;
 import org.eclipse.papyrus.infra.core.utils.AbstractServiceUtils;
 
 /**
+ * Get the {@link ServicesRegistry} from a {@link EObject} or a {@link Resource}.
+ * This class allow to retrieve the {@link ServicesRegistry} associated to the {@link ResourceSet}
+ * owning the {@link Resource} owning the EObject.
+ * 
+ * <br>
+ * To work properly, the EObject should be associated to a {@link Resource}, itself
+ * registered in a {@link ResourceSet}. 
+ * Also, the ServicesRegistry should be associated to the ResourceSet with the help of the 
+ * {@link ServiceRegistryAdapterFactory}. 
+ * <br>
+ * Normally, this is automatically done thanks to the {@link ServiceUtilsForResourceInitializerService} service.
+ * <br>
+ * If you access this class from a service, you can ensure that the previous service is started by letting your service
+ * depends on the <b>org.eclipse.papyrus.infra.emf.utils.ServiceUtilsForResourceInitializerService</b>.
+ *   
+ * 
  * @author cedric dumoulin
  * 
  */
@@ -39,11 +57,12 @@ public class ServiceUtilsForResource extends AbstractServiceUtils<Resource> {
 	}
 
 	/**
-	 * @see org.eclipse.papyrus.infra.core.utils.AbstractServiceUtils#getServiceRegistry(java.lang.Object)
+	 * Get the {@link ServicesRegistry} from a {@link Resource}.
 	 * 
-	 * @param from
+	 * @param from The {@link Resource} from which we want the associated {@link ServicesRegistry}.
 	 * @return
-	 * @throws ServiceException
+	 * @throws ServiceException If there is no {@link ServicesRegistry} associated to the {@link ResourceSet}
+	 *   owning the {@link Resource}.
 	 */
 	@Override
 	public ServicesRegistry getServiceRegistry(Resource from) throws ServiceException {
