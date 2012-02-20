75b63303f62324463ceb9f9ee9d4461f7cf6df8a
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-02-20 15:48:30 +0000

    372031: [Documentation][Developper] The creation of nested Editor should be documented https://bugs.eclipse.org/bugs/show_bug.cgi?id=372031

---
+++ incoming/org.eclipse.papyrus.example.uml.comment.editor.v1/src/org/eclipse/papyrus/example/uml/comment/editor/v1/modelresource/TextEditorModelSharedResource.java
@@ -0,0 +1,109 @@
+/*****************************************************************************
+ * Copyright (c) 2012 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) Vincent.Lorenzo@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.example.uml.comment.editor.v1.modelresource;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.papyrus.example.test.instance.papyrustextinstance.PapyrusTextInstance;
+import org.eclipse.papyrus.infra.core.resource.AbstractModelWithSharedResource;
+import org.eclipse.papyrus.infra.core.resource.IModel;
+
+
+public class TextEditorModelSharedResource extends AbstractModelWithSharedResource<PapyrusTextInstance> implements IModel {
+
+	/*
+	 * To save in a new file :
+	 * the class should extends AbstractBaseModel and NOTATION_FILE_EXTENSION = "nattable" for example
+	 */
+
+
+	/**
+	 * File extension used for notation.
+	 */
+	//	public static final String NOTATION_FILE_EXTENSION = "nattable"; //$NON-NLS-1$
+
+	public static final String NOTATION_FILE_EXTENSION = "notation"; //$NON-NLS-1$
+
+	/**
+	 * Model ID.
+	 */
+	public static final String MODEL_ID = "org.eclipse.papyrus.example.text.TextEditorModelSharedResource"; //$NON-NLS-1$
+
+
+	/**
+	 *
+	 * Constructor.
+	 *
+	 */
+	public TextEditorModelSharedResource() {
+
+	}
+
+
+	/**
+	 * Get the file extension used for this model.
+	 *
+	 * @see org.eclipse.papyrus.infra.core.resource.AbstractBaseModel#getModelFileExtension()
+	 *
+	 * @return
+	 */
+	@Override
+	protected String getModelFileExtension() {
+		return NOTATION_FILE_EXTENSION;
+	}
+
+	/**
+	 * Get the identifier used to register this model.
+	 *
+	 * @see org.eclipse.papyrus.infra.core.resource.AbstractBaseModel#getIdentifier()
+	 *
+	 * @return
+	 */
+	@Override
+	public String getIdentifier() {
+		return MODEL_ID;
+	}
+
+
+	/**
+	 * Add a new initialized {@link PapyrusEMFCompareInstance} to the model.
+	 *
+	 * @param compareInstance
+	 *        The compareInstance to add.
+	 */
+	public void addPapyrusTextInstance(PapyrusTextInstance compareInstance) {
+		getResource().getContents().add(compareInstance);
+	}
+
+	/**
+	 * Add a new initialized {@link PapyrusEMFCompareInstance} to the model.
+	 *
+	 * @param compareInstance
+	 *        The compareInstance to add.
+	 */
+	public void removePapyrusTextInstance(PapyrusTextInstance compareInstance) {
+		getResource().getContents().remove(compareInstance);
+	}
+
+	/**
+	 *
+	 * @see org.eclipse.papyrus.infra.core.resource.AbstractModelWithSharedResource#isModelRoot(org.eclipse.emf.ecore.EObject)
+	 *
+	 * @param object
+	 * @return
+	 */
+	@Override
+	protected boolean isModelRoot(EObject object) {
+		return false;
+	}
+
+}
