e3a99923cf5f2ae9879c614ed0a0868215765a71
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-02-23 12:50:44 +0000

    372321: [Diagram - All] Papyrus should provide a refresh action for its diagrams https://bugs.eclipse.org/bugs/show_bug.cgi?id=372321

---
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/helper/NotationHelper.java
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ * Copyright (c) 2012 CEA LIST.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Camille Letavernier (CEA LIST) camille.letavernier@cea.fr - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.infra.gmfdiag.common.helper;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.gmf.runtime.notation.View;
+
+/**
+ * A Helper class related to the GMF Notation metamodel.
+ * 
+ * @author Camille Letavernier
+ */
+public class NotationHelper {
+
+	/**
+	 * Retrieves the GMF View associated to the source object
+	 * 
+	 * @param source
+	 *        the source
+	 * @return the resolved view, or null if it cannot be found
+	 */
+	public static View findView(Object source) {
+		if(source instanceof View) {
+			return (View)source;
+		}
+		if(source instanceof IAdaptable) {
+			IAdaptable adaptable = (IAdaptable)source;
+			Object adapter = adaptable.getAdapter(View.class);
+			if(adapter != null) {
+				return (View)adapter;
+			}
+		}
+		return null;
+	}
+}
