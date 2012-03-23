2f957b2d2628f7051944267e3efa985b8f36671e
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-03-23 16:43:48 +0000

    330176: [All Diagrams] Papyrus shall enable to display stereotype properties in a dedicated compartment. https://bugs.eclipse.org/bugs/show_bug.cgi?id=330176

---
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/providers/RestrictedAbstractEditPartProvider.java
@@ -0,0 +1,49 @@
+package org.eclipse.papyrus.infra.gmfdiag.common.providers;
+
+/*****************************************************************************
+ * Copyright (c) 2012 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+import org.eclipse.gmf.runtime.common.core.service.IOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.editpart.AbstractEditPartProvider;
+import org.eclipse.gmf.runtime.diagram.ui.services.editpart.CreateGraphicEditPartOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.editpart.IEditPartOperation;
+
+/**
+ * This abstract edit part provider restricts its contribution to view that are owned by
+ * a given type of diagram.
+ */
+public abstract class RestrictedAbstractEditPartProvider extends AbstractEditPartProvider {
+
+	/** The provides only provides for this diagram type */
+	protected String diagramType;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean provides(IOperation operation) {
+		if(operation instanceof CreateGraphicEditPartOperation) {
+			String currentDiagramType = ((IEditPartOperation)operation).getView().getDiagram().getType();
+
+			if((diagramType == null) || (!diagramType.equals(currentDiagramType))) {
+				return false;
+			}
+		}
+
+		return super.provides(operation);
+	}
+
+
+
+
+}
