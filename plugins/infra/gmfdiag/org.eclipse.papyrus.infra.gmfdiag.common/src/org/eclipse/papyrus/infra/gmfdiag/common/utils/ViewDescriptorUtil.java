e8eadc1b87d0e2e8d52ab124ebde0fa2b5f76ebc
ytanguy <ytanguy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-03-28 13:38:39 +0000

    375535: Use constant rather than simple boolean as ViewDescriptor persistence parameter https://bugs.eclipse.org/bugs/show_bug.cgi?id=375535

---
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/utils/ViewDescriptorUtil.java
@@ -0,0 +1,23 @@
+/*****************************************************************************
+ * Copyright (c) 2012 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		
+ *		CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.infra.gmfdiag.common.utils;
+
+public class ViewDescriptorUtil {
+
+	/** The Constant PERSISTED for use in ViewDescriptor constructors. */
+	public static final boolean PERSISTED = true;
+
+	/** The Constant PERSISTED_NOT for use in ViewDescriptor constructors. */
+	public static final boolean PERSISTED_NOT = false;
+}
