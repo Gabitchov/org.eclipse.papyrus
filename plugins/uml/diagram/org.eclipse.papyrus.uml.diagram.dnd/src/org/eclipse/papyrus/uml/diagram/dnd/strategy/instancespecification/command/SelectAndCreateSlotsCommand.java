73a8dfff5132ffcdfc4205a8359523669ec525a9
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-06-12 14:39:35 +0000

    370819: [Global] A Flexible Drag & Drop framework https://bugs.eclipse.org/bugs/show_bug.cgi?id=370819

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.dnd/src/org/eclipse/papyrus/uml/diagram/dnd/strategy/instancespecification/command/SelectAndCreateSlotsCommand.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.dnd/src/org/eclipse/papyrus/uml/diagram/dnd/strategy/instancespecification/command/SelectAndCreateSlotsCommand.java
@@ -21,6 +21,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gef.EditPart;
+import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gmf.runtime.common.core.command.AbstractCommand;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
@@ -30,12 +31,15 @@ import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewAndElementRequest;
 import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewAndElementRequest.ViewAndElementDescriptor;
+import org.eclipse.gmf.runtime.emf.type.core.commands.CreateElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.commands.SetValueCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.jface.window.Window;
 import org.eclipse.papyrus.infra.emf.utils.EMFHelper;
+import org.eclipse.papyrus.uml.diagram.clazz.edit.parts.InstanceSpecificationSlotCompartmentEditPart;
+import org.eclipse.papyrus.uml.diagram.clazz.edit.parts.InstanceSpecificationSlotCompartmentEditPartCN;
 import org.eclipse.papyrus.uml.diagram.clazz.edit.parts.SlotEditPart;
 import org.eclipse.papyrus.uml.diagram.clazz.part.UMLVisualIDRegistry;
 import org.eclipse.papyrus.uml.diagram.clazz.providers.UMLElementTypes;
@@ -73,6 +77,7 @@ public class SelectAndCreateSlotsCommand extends AbstractCommand {
 
 	@Override
 	protected CommandResult doExecuteWithResult(IProgressMonitor progressMonitor, IAdaptable info) throws ExecutionException {
+		//Open the dialog to select the slots
 		SlotSelectionDialog dialog = new SlotSelectionDialog(Display.getCurrent().getActiveShell(), specification, classifiers);
 		if(dialog.open() != Window.OK) {
 			return CommandResult.newCancelledCommandResult();
@@ -80,65 +85,109 @@ public class SelectAndCreateSlotsCommand extends AbstractCommand {
 
 		Object[] result = dialog.getResult();
 
+		//For each selected property, create the corresponding slot
 		for(Object propertyObject : result) {
 			Property property = (Property)propertyObject;
 			//Creates the slot
 			TransactionalEditingDomain domain = (TransactionalEditingDomain)EMFHelper.resolveEditingDomain(targetEditPart);
 			CreateElementRequest createElementRequest = new CreateElementRequest(domain, specification, UMLElementTypes.Slot_3030);
 
-			CreateElementRequestAdapter adapter = new CreateElementRequestAdapter(createElementRequest);
-			ViewAndElementDescriptor descriptor = new ViewAndElementDescriptor(adapter, Node.class, UMLVisualIDRegistry.getType(SlotEditPart.VISUAL_ID), ((IGraphicalEditPart)targetEditPart).getDiagramPreferencesHint());
+			ICommand slotCreationCommand;
 
-			CreateViewAndElementRequest createRequest = new CreateViewAndElementRequest(descriptor);
-			Command gefCommand = targetEditPart.getCommand(createRequest);
+			//If the slot compartment is visible, create the slot graphically
+			if(isSlotCompartmentAvailable(targetEditPart)) {
+				CreateElementRequestAdapter adapter = new CreateElementRequestAdapter(createElementRequest);
+				ViewAndElementDescriptor descriptor = new ViewAndElementDescriptor(adapter, Node.class, UMLVisualIDRegistry.getType(SlotEditPart.VISUAL_ID), ((IGraphicalEditPart)targetEditPart).getDiagramPreferencesHint());
+				Request createRequest = new CreateViewAndElementRequest(descriptor);
+				Command gefCommand = targetEditPart.getCommand(createRequest);
 
-			if(gefCommand instanceof ICommandProxy) {
-				ICommand iCommand = ((ICommandProxy)gefCommand).getICommand();
-				iCommand = iCommand.reduce();
-				iCommand.execute(new NullProgressMonitor(), null);
+				if(gefCommand instanceof ICommandProxy) {
+					slotCreationCommand = ((ICommandProxy)gefCommand).getICommand();
+					slotCreationCommand = slotCreationCommand.reduce();
+				} else {
+					return CommandResult.newErrorCommandResult("Impossible to create slots");
+				}
+			} else { //The compartment is not visible ; only create the semantic slot
+				slotCreationCommand = new CreateElementCommand(createElementRequest);
+			}
 
-				CommandResult commandResult = iCommand.getCommandResult();
+			slotCreationCommand.execute(new NullProgressMonitor(), null);
 
-				//Initialize properties: feature & name & default value
-				if(commandResult != null) {
-					if(!iCommand.getCommandResult().getStatus().isOK()) {
-						return iCommand.getCommandResult();
-					}
+			CommandResult commandResult = slotCreationCommand.getCommandResult();
+			if(commandResult != null) {
+				if(!commandResult.getStatus().isOK()) {
+					return commandResult;
+				}
+			}
 
-					Object objectResult = commandResult.getReturnValue();
-					if(objectResult instanceof List) {
-						List<?> listResult = (List<?>)objectResult;
-						for(Object elementResult : listResult) {
-							if(elementResult instanceof CreateElementRequestAdapter) {
-								CreateElementRequest request = (CreateElementRequest)((CreateElementRequestAdapter)elementResult).getAdapter(CreateElementRequest.class);
-								if(request != null) {
-									EObject newElement = request.getNewElement();
-									if(newElement instanceof Slot) {
-										Slot slot = (Slot)newElement;
+			//Retrieve the created slot, and update its properties (
+			Slot newSlot = getNewSlot(commandResult);
+			if(newSlot != null) {
+				updateSlotProperties(newSlot, property);
+			} else {
+				return CommandResult.newErrorCommandResult("Could not create the slot for property " + property.getName());
+			}
 
-										SetRequest setFeatureRequest = new SetRequest(slot, UMLPackage.eINSTANCE.getSlot_DefiningFeature(), property);
-										SetValueCommand setFeatureCommand = new SetValueCommand(setFeatureRequest);
-										setFeatureCommand.execute(new NullProgressMonitor(), null);
+			//Initialize properties: feature & name & default value
+		}
 
-										if(property.getDefaultValue() != null) {
-											ValueSpecification defaultValue = property.getDefaultValue();
-											ValueSpecification currentValue = EcoreUtil.copy(defaultValue);
-											currentValue.setName(property.getName());
+		return CommandResult.newOKCommandResult();
+	}
 
-											SetRequest setValueRequest = new SetRequest(slot, UMLPackage.eINSTANCE.getSlot_Value(), currentValue);
-											SetValueCommand setValueCommand = new SetValueCommand(setValueRequest);
-											setValueCommand.execute(new NullProgressMonitor(), null);
-										}
-									}
-								}
-							}
+	// Retrieves a slot from a CommandResult
+	protected Slot getNewSlot(CommandResult commandResult) {
+		Object objectResult = commandResult.getReturnValue();
+		if(objectResult instanceof List) {
+			//Result of the semantic + graphical creation command
+			List<?> listResult = (List<?>)objectResult;
+			for(Object elementResult : listResult) {
+				if(elementResult instanceof CreateElementRequestAdapter) {
+					CreateElementRequest request = (CreateElementRequest)((CreateElementRequestAdapter)elementResult).getAdapter(CreateElementRequest.class);
+					if(request != null) {
+						EObject newElement = request.getNewElement();
+						if(newElement instanceof Slot) {
+							Slot slot = (Slot)newElement;
+							return slot;
 						}
 					}
 				}
 			}
+		} else if(commandResult.getReturnValue() instanceof Slot) {
+			//Result of the semantic creation command
+			return (Slot)commandResult.getReturnValue();
 		}
 
-		return CommandResult.newOKCommandResult();
+		return null;
+	}
+
+	//Sets the slot's property (definingFeature) and initialize its value (property#defaultValue)
+	protected void updateSlotProperties(Slot slot, Property property) throws ExecutionException {
+		SetRequest setFeatureRequest = new SetRequest(slot, UMLPackage.eINSTANCE.getSlot_DefiningFeature(), property);
+		SetValueCommand setFeatureCommand = new SetValueCommand(setFeatureRequest);
+		setFeatureCommand.execute(new NullProgressMonitor(), null);
+
+		if(property.getDefaultValue() != null) {
+			ValueSpecification defaultValue = property.getDefaultValue();
+			ValueSpecification currentValue = EcoreUtil.copy(defaultValue);
+			currentValue.setName(property.getName());
+
+			SetRequest setValueRequest = new SetRequest(slot, UMLPackage.eINSTANCE.getSlot_Value(), currentValue);
+			SetValueCommand setValueCommand = new SetValueCommand(setValueRequest);
+			setValueCommand.execute(new NullProgressMonitor(), null);
+		}
+	}
+
+	//Tests whether the slot compartment edit part is available
+	protected boolean isSlotCompartmentAvailable(EditPart targetEditPart) {
+		if(targetEditPart instanceof InstanceSpecificationSlotCompartmentEditPart || targetEditPart instanceof InstanceSpecificationSlotCompartmentEditPartCN) {
+			return true;
+		}
+		for(Object editPartObject : targetEditPart.getChildren()) {
+			if(isSlotCompartmentAvailable((EditPart)editPartObject)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
