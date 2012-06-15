0aaf93c7fb0650edb6b7e9064b497b9c004edcdf
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-06-15 11:34:17 +0000

    381889: Create a script to generate easily psf file to download Papyrus project https://bugs.eclipse.org/bugs/show_bug.cgi?id=381889

--- readme/scripts/update_psf_files_script.sh
+++ readme/scripts/update_psf_files_script.sh
@@ -9,9 +9,18 @@
 #This script downlaod the last version of some scripts located in the readme project
 #on the papyrus svn and replace the existing version on build.eclipse.org
 path_on_svn="http://dev.eclipse.org/svnroot/modeling/org.eclipse.mdt.papyrus/trunk/readme/scripts/"
-cd /opt/public/modeling/mdt/papyrus/
-
-wget -r -nd -np $path_on_svn/create_psf_file.sh
-chmod +x create_psf_file.sh
-wget -r -nd -np $path_on_svn/update_psf_files_script.sh
-chmod +x update_psf_files_script.sh
+tmpFolder=$HOME/readme
+dest="/opt/public/modeling/mdt/papyrus/"
+echo "This script download the existing script ont the SVN in trunk/readme/scripts"
+echo "do a chmod+x"
+echo 'and move them in $dest'
+mkdir $tmpFolder && cd $tmpFolder
+#we download all the scripts
+wget -N -r -nd -np $path_on_svn
+for current in `find . -name ".sh`
+  do
+  chmod +x $current
+  cp -f $current $dest
+done
+cd $HOME
+rm $tmpFolder
