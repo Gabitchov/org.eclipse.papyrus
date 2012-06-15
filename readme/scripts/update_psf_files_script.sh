9b846f6d05f0f4affd58332dd3ce27966ca6e1f1
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-06-15 11:38:53 +0000

    381889: Create a script to generate easily psf file to download Papyrus project https://bugs.eclipse.org/bugs/show_bug.cgi?id=381889

--- readme/scripts/update_psf_files_script.sh
+++ readme/scripts/update_psf_files_script.sh
@@ -19,7 +19,7 @@ mkdir $tmpFolder && cd $tmpFolder
 wget -N -r -nd -np $path_on_svn
 for current in `find . -name "*.sh"`
   do
-  chmod+x $current
+  chmod +x $current
   cp -f $current $dest
 done
 cd $HOME
