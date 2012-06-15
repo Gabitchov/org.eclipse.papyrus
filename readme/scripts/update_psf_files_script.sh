6cadd86f77fd6e837171f00c53be23c8a24edda4
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-06-15 11:21:42 +0000

    381889: Create a script to generate easily psf file to download Papyrus project https://bugs.eclipse.org/bugs/show_bug.cgi?id=381889

--- readme/scripts/update_psf_files_script.sh
+++ readme/scripts/update_psf_files_script.sh
@@ -6,10 +6,12 @@
 #http://www.eclipse.org/legal/epl-v10.html
 # Vincent Lorenzo (CEA-LIST) vincent.lorenzo@cea.fr - Initial API and Implementation
 
-#This script downlaod the last version of the script create_psf_file.sh 
+#This script downlaod the last version of some scripts located in the readme project
 #on the papyrus svn and replace the existing version on build.eclipse.org
-path_on_svn="http://dev.eclipse.org/svnroot/modeling/org.eclipse.mdt.papyrus/trunk/readme/scripts"
+path_on_svn="http://dev.eclipse.org/svnroot/modeling/org.eclipse.mdt.papyrus/trunk/readme/scripts/"
 cd /opt/public/modeling/mdt/papyrus/
-wget -r -nd -np $path_on_svn
+
+wget -r -nd -np $path_on_svn/create_psf_file.sh
 chmod +x create_psf_file.sh
+wget -r -nd -np $path_on_svn/update_psf_files_script.sh
 chmod +x update_psf_files_script.sh
