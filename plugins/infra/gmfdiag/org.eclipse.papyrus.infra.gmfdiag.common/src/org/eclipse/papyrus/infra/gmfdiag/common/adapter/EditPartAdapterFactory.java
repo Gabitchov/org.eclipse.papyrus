8dd6b4d209d4f146385548c14af384b0bb1535ae
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-07-25 13:51:37 +0000

    385726: [UML Compare] Add an action in the Diagram Menu to allow the comparison between to 2 UML Element https://bugs.eclipse.org/bugs/show_bug.cgi?id=385726
    Create the AdapterFactory in gmfdiag.common
    in order to be able do adapt EditPart to EObject in a plugin.xml

---
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/adapter/EditPartAdapterFactory.java
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ * Copyright (c) 2012 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) Vincent.Lorenzo@cea.fr - Initial API and implementation
+ *  
+ *****************************************************************************/
+package org.eclipse.papyrus.infra.gmfdiag.common.adapter;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.EditPart;
+
+/**
+ * 
+ * This factory allows to get an EObject from an EditPart
+ * It is useful in the plugin.xml
+ * 
+ * handler
+ * activeWhen
+ * with
+ * selection
+ * iterate
+ * adapt to EObject
+ * 
+ * -> The handler is active when the selection can be adapted to EObject
+ * 
+ * It is useful for action active on the EditPart in the Diagrams
+ * 
+ */
+public class EditPartAdapterFactory implements IAdapterFactory {
+
+	/**
+	 * 
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object, java.lang.Class)
+	 * 
+	 * @param adaptableObject
+	 * @param adapterType
+	 * @return
+	 */
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		if(adaptableObject instanceof EditPart && adaptableObject instanceof IAdaptable) {
+			return ((IAdaptable)adaptableObject).getAdapter(adapterType);
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapterList()
+	 * 
+	 * @return
+	 */
+	public Class<?>[] getAdapterList() {
+		return new Class[]{ EObject.class };
+	}
+
+}
