eeaddd14694a32a6e10d443ad857311347ed60be
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-07-25 14:08:46 +0000

    385946: [UML Compare] The handler to create NestedUMLCompareEditor should be accessible https://bugs.eclipse.org/bugs/show_bug.cgi?id=385946

--- extraplugins/uml/compare/org.eclipse.papyrus.uml.compare/src/org/eclipse/papyrus/uml/compare/internal/handler/UMLCompareEditorHandler.java
+++ extraplugins/uml/compare/org.eclipse.papyrus.uml.compare/src/org/eclipse/papyrus/uml/compare/internal/handler/UMLCompareEditorHandler.java
@@ -11,7 +11,7 @@
  *  Vincent Lorenzo (CEA LIST) Vincent.Lorenzo@cea.fr - Initial API and implementation
  *
  *****************************************************************************/
-package org.eclipse.papyrus.uml.compare.handler;
+package org.eclipse.papyrus.uml.compare.internal.handler;
 
 import java.util.ArrayList;
 import java.util.List;
