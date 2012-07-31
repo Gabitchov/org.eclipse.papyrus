f66c0e441ef2d2a1b457c54f003a86a79d0aa03f
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-07-31 15:23:37 +0000

    330349: [All diagrams]Applying a profile with a Property of an undefined Type corrupts the model and makes save action crash https://bugs.eclipse.org/bugs/show_bug.cgi?id=330349

--- plugins/uml/org.eclipse.papyrus.uml.profile/src/org/eclipse/papyrus/uml/profile/constraints/ClientSelector.java
+++ plugins/uml/org.eclipse.papyrus.uml.profile/src/org/eclipse/papyrus/uml/profile/constraints/ClientSelector.java
@@ -1,8 +1,25 @@
+/*****************************************************************************
+ * Copyright (c) 2012 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Patrick Tessier (CEA LIST) Patrick.Tessier@cea.fr - modification
+ *
+ *****************************************************************************/
 package org.eclipse.papyrus.uml.profile.constraints;
 
 import org.eclipse.emf.validation.model.IClientSelector;
 import org.eclipse.uml2.uml.Element;
 
+/**
+ * this is the client context to execute constraint of profile in the context of EMF validation
+ *
+ */
 
 public class ClientSelector implements IClientSelector {
 
