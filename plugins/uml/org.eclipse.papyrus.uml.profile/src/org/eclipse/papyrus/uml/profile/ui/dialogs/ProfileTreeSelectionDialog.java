377b26af0450ad1bbbf67f5bb6ad600e76d6116c
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-07-31 17:29:18 +0000

    330349: [All diagrams]Applying a profile with a Property of an undefined Type corrupts the model and makes save action crash https://bugs.eclipse.org/bugs/show_bug.cgi?id=330349

--- plugins/uml/org.eclipse.papyrus.uml.profile/src/org/eclipse/papyrus/uml/profile/ui/dialogs/ProfileTreeSelectionDialog.java
+++ plugins/uml/org.eclipse.papyrus.uml.profile/src/org/eclipse/papyrus/uml/profile/ui/dialogs/ProfileTreeSelectionDialog.java
@@ -17,7 +17,22 @@ package org.eclipse.papyrus.uml.profile.ui.dialogs;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.DiagnosticChain;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.transaction.util.TransactionUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TreeItem;
@@ -43,6 +58,9 @@ public class ProfileTreeSelectionDialog extends ElementImportTreeSelectionDialog
 	 */
 	public ProfileTreeSelectionDialog(Shell parent, Package model) {
 		super(parent, model);
+		if( !isAValidProfile(model)){
+			this.model=null;
+		}
 		subSelection = true;
 		subProfilesList = new ArrayList<String>();
 	}
@@ -57,9 +75,42 @@ public class ProfileTreeSelectionDialog extends ElementImportTreeSelectionDialog
 	 */
 	public ProfileTreeSelectionDialog(Shell parent, List<Package> model) {
 		this(parent, model, new ArrayList<String>());
+
+
 		subSelection = true;
 	}
 
+	/**
+	 * constructor
+	 * 
+	 * @param model
+	 *        of the profile
+	 * @param parent
+	 *        the shell
+	 * @param testValidity true to test if profile to apply are valids       
+	 */
+	public ProfileTreeSelectionDialog(Shell parent, List<Package> model, boolean testValidity) {
+		this(parent, model, new ArrayList<String>());
+		if(testValidity){
+			List<Package> modelToRemoveList= new ArrayList<Package>();
+			for(Iterator<Package> iterator = model.iterator(); iterator.hasNext();) {
+				Package currentPackage = (Package)iterator.next();
+				if( !isAValidProfile(currentPackage)){
+					modelToRemoveList.add(currentPackage);
+				}
+
+			}
+			for(Iterator<Package> iterator = modelToRemoveList.iterator(); iterator.hasNext();) {
+				Package currentPackage = (Package)iterator.next();
+				this.models.remove(currentPackage);
+			}
+			if( this.models.size()==0){
+				MessageDialog.openError(new Shell(), "Profiles not Valid", "Selected profiles cannot be applied because their definition are not valid");
+				getShell().dispose();
+			}
+		}
+		subSelection = true;
+	}
 	/**
 	 * constructor
 	 * 
@@ -91,6 +142,47 @@ public class ProfileTreeSelectionDialog extends ElementImportTreeSelectionDialog
 			}
 		}
 		return profileList;
+	}
+
+	private boolean isAValidProfile(Package profile){
+
+		EditingDomain domain=TransactionUtil.getEditingDomain(profile);;
+		AdapterFactory adapterFactory = domain instanceof AdapterFactoryEditingDomain ? ((AdapterFactoryEditingDomain)domain).getAdapterFactory() : null;
+		Diagnostician diagnostician = createDiagnostician(adapterFactory, new NullProgressMonitor());
+		BasicDiagnostic diagnostic = diagnostician.createDefaultDiagnostic(profile);
+		Map<Object, Object> context = diagnostician.createDefaultContext();
+		boolean isValid = diagnostician.validate(profile, diagnostic, context);
+		int severity=diagnostic.getSeverity();
+		if(severity==Diagnostic.ERROR) {return false;}
+		return true;
+	}
+
+	/**
+	 * create a diagnostician to evaluate a profil
+	 * @param adapterFactory
+	 * @param progressMonitor
+	 * @return
+	 */
+	protected Diagnostician createDiagnostician(final AdapterFactory adapterFactory, final IProgressMonitor progressMonitor) {
+		return new Diagnostician() {
+
+			@Override
+			public String getObjectLabel(EObject eObject) {
+				if(adapterFactory != null && !eObject.eIsProxy()) {
+					IItemLabelProvider itemLabelProvider = (IItemLabelProvider)adapterFactory.adapt(eObject, IItemLabelProvider.class);
+					if(itemLabelProvider != null) {
+						return itemLabelProvider.getText(eObject);
+					}
+				}
+				return super.getObjectLabel(eObject);
+			}
+
+			@Override
+			public boolean validate(EClass eClass, EObject eObject, DiagnosticChain diagnostics, Map<Object, Object> context) {
+				progressMonitor.worked(1);
+				return super.validate(eClass, eObject, diagnostics, context);
+			}
+		};
 	}
 
 	/*
