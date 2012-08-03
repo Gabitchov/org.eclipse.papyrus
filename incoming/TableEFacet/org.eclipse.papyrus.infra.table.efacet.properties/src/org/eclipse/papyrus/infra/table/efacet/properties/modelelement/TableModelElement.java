3e7f10e7c50cae1b760b2b41034063706c05d877
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-08-03 15:08:49 +0000

    Work in progress - Table EFacet properties view

---
+++ incoming/TableEFacet/org.eclipse.papyrus.infra.table.efacet.properties/src/org/eclipse/papyrus/infra/table/efacet/properties/modelelement/TableModelElement.java
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ * Copyright (c) 2012 CEA LIST.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Camille Letavernier (CEA LIST) camille.letavernier@cea.fr - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.infra.table.efacet.properties.modelelement;
+
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.papyrus.infra.table.efacet.metamodel.papyrustable.PapyrusTable;
+import org.eclipse.papyrus.views.properties.modelelement.EMFModelElement;
+
+
+public class TableModelElement extends EMFModelElement {
+
+	private PapyrusTable sourceTable;
+
+	public TableModelElement(PapyrusTable source, EditingDomain domain) {
+		super(source, domain);
+		this.sourceTable = source;
+	}
+
+
+}
