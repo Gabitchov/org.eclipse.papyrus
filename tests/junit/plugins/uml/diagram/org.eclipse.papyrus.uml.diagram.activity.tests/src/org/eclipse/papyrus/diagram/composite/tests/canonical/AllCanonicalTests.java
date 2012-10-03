65efa5413475606fe274484f7fb7d7c4e6c6ae49
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-10-03 09:54:03 +0000

    390996: [Activity Diagram] undo redo of nodes does not run https://bugs.eclipse.org/bugs/show_bug.cgi?id=390996

---
+++ tests/junit/plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.activity.tests/src/org/eclipse/papyrus/diagram/composite/tests/canonical/AllCanonicalTests.java
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Remi Schnekenburger (CEA LIST) remi.schnekenburger@cea.fr - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.diagram.composite.tests.canonical;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+/**
+ * All test in canonical package
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+
+// child nodes
+TestActivityDiagramChildNode.class,
+//labelNodes
+//TestActivityDiagramChildLabel.class,
+//test links
+//TestActivityDiagramLink.class,
+//test links owned by source
+//TestClassDiagramLinkOwnedBySource.class,
+//containmentLink
+//TestClassDiagramContainment.class,
+
+})
+public class AllCanonicalTests {
+
+}
---
+++ tests/junit/plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.activity.tests/src/org/eclipse/papyrus/diagram/composite/tests/canonical/TestActivityChildNode.java
@@ -0,0 +1,41 @@
+package org.eclipse.papyrus.diagram.composite.tests.canonical;
+
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.UnexecutableCommand;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequest;
+import org.eclipse.papyrus.diagram.tests.canonical.TestChildNode;
+import org.eclipse.papyrus.editor.PapyrusMultiDiagramEditor;
+import org.eclipse.papyrus.infra.core.editor.IMultiDiagramEditor;
+import org.eclipse.papyrus.infra.core.extension.commands.ICreationCommand;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+
+
+public  abstract class TestActivityChildNode extends TestChildNode {
+
+
+	@Override
+	protected void setUp() throws Exception {
+		projectCreation();
+
+		while( !(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor()instanceof IMultiDiagramEditor)){}
+		IEditorPart editorPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		papyrusEditor=((PapyrusMultiDiagramEditor)editorPart);
+		
+		assertTrue(CREATION +INITIALIZATION_TEST,getDiagramEditPart().getChildren().size()==1);
+		GraphicalEditPart containerEditPart= (GraphicalEditPart)getDiagramEditPart().getChildren().get(0);
+		rootCompartment = null;
+		int index=0;
+		while (rootCompartment==null && index <containerEditPart.getChildren().size()){
+			if((containerEditPart.getChildren().get(index)) instanceof ShapeCompartmentEditPart){
+				rootCompartment= (ShapeCompartmentEditPart)(containerEditPart.getChildren().get(index));
+			}
+			index++;
+		}
+
+	}
+
+}
