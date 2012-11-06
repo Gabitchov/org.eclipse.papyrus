f9bd9ed6319e8419cfbb827623517cb99e37cba8
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-11-06 08:55:43 +0000

    389601: [Table] Papyrus should provides an easy way to edit the properties of the stereotypes using a Dialog Show/Hide columns https://bugs.eclipse.org/bugs/show_bug.cgi?id=389601
    Manage the inherited properties of stereotypes

--- extraplugins/table/org.eclipse.papyrus.uml.profileefacet/src/org/eclipse/papyrus/uml/profileefacet/queries/getter/GetGenericValue.java
+++ extraplugins/table/org.eclipse.papyrus.uml.profileefacet/src/org/eclipse/papyrus/uml/profileefacet/queries/getter/GetGenericValue.java
@@ -13,6 +13,8 @@
  *****************************************************************************/
 package org.eclipse.papyrus.uml.profileefacet.queries.getter;
 
+import java.util.Collection;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.facet.efacet.core.IFacetManager;
 import org.eclipse.emf.facet.efacet.core.exception.DerivedTypedElementException;
@@ -21,22 +23,33 @@ import org.eclipse.emf.facet.query.java.core.IParameterValueList2;
 import org.eclipse.papyrus.uml.profileefacet.metamodel.profileefacet.StereotypeFacet;
 import org.eclipse.papyrus.uml.profileefacet.metamodel.profileefacet.StereotypePropertyElement;
 import org.eclipse.papyrus.uml.profileefacet.utils.Constants;
+import org.eclipse.papyrus.uml.tools.utils.StereotypeUtil;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Stereotype;
 
 
 public abstract class GetGenericValue<R> implements IJavaQuery2<Element, R> {
 
-	public R evaluate(Element source, IParameterValueList2 parameterValues, IFacetManager facetManager) throws DerivedTypedElementException {
+	public R evaluate(final Element source, final IParameterValueList2 parameterValues, final IFacetManager facetManager) throws DerivedTypedElementException {
 		final StereotypePropertyElement element = (StereotypePropertyElement)parameterValues.getParameterValueByName(Constants.EDITED_FEATURE).getValue();
 		if(element != null) {
-			EObject container = element.eContainer();
+			final EObject container = element.eContainer();
 			assert container instanceof StereotypeFacet;
-			StereotypeFacet facet = (StereotypeFacet)container;
+			final StereotypeFacet facet = (StereotypeFacet)container;
 			final String stereotypeQN = facet.getStereotypeQualifiedName();
 			final Stereotype appliedStereotype = source.getAppliedStereotype(stereotypeQN);
 			if(appliedStereotype != null) {
 				return (R)source.getValue(appliedStereotype, element.getPropertyName());
+			} else if(appliedStereotype == null) {//the property can comes from inheritance...
+				for(Stereotype appliedSte : source.getAppliedStereotypes()) {
+					final Collection<Stereotype> superClasses = StereotypeUtil.getAllSuperStereotypes(appliedSte);
+					for(final Stereotype current : superClasses) {
+						if(current.getQualifiedName().equals(stereotypeQN)) {
+							return (R)source.getValue(appliedSte, element.getPropertyName());
+						}
+					}
+				}
+
 			}
 		}
 		return null;
