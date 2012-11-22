87a62c36f06c0383868e06941e591d7d9bdc8250
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-11-22 13:04:48 +0000

    269492: [All Diagrams] Display properties of a applied stereotypes as a " comment" https://bugs.eclipse.org/bugs/show_bug.cgi?id=269492

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.communication/custom-src/org/eclipse/papyrus/uml/diagram/communication/custom/providers/CustomEditPolicyProvider.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.communication/custom-src/org/eclipse/papyrus/uml/diagram/communication/custom/providers/CustomEditPolicyProvider.java
@@ -55,14 +55,12 @@ public class CustomEditPolicyProvider implements IEditPolicyProvider {
 
 			editPart.installEditPolicy(NavigationEditPolicy.NAVIGATION_POLICY, new NavigationEditPolicy());
 			if( editPart instanceof IPrimaryEditPart){
-
-
 				editPart.installEditPolicy(AppliedStereotypeCommentCreationEditPolicy.APPLIED_STEREOTYPE_COMMENT, new AppliedStereotypeCommentCreationEditPolicy());
-			}
 
-			if(!( editPart instanceof ConnectionEditPart)){
-				editPart.installEditPolicy(EditPolicyRoles.POPUPBAR_ROLE, new HyperLinkPopupBarEditPolicy());
+				if(!( editPart instanceof ConnectionEditPart)){
+					editPart.installEditPolicy(EditPolicyRoles.POPUPBAR_ROLE, new HyperLinkPopupBarEditPolicy());
 
+				}
 			}
 			if(editPart instanceof NamedElementEditPart ){
 				editPart.installEditPolicy(AppliedStereotypeLabelDisplayEditPolicy.STEREOTYPE_LABEL_POLICY, new AppliedStereotypeCompartmentEditPolicy());
@@ -79,7 +77,7 @@ public class CustomEditPolicyProvider implements IEditPolicyProvider {
 		if(!(epOperation.getEditPart() instanceof GraphicalEditPart)&&!(epOperation.getEditPart() instanceof ConnectionEditPart)) {
 			return false;
 		}
-		
+
 		EditPart gep = (EditPart)epOperation.getEditPart();
 		String diagramType =((View) gep.getModel()).getDiagram().getType();
 		if(org.eclipse.papyrus.uml.diagram.communication.edit.parts.ModelEditPart.MODEL_ID.equals(diagramType)) {
