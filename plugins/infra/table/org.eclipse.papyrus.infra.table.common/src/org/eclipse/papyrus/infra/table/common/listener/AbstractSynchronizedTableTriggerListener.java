bd9dec7b19a42200657cb88b35f10814566fb28e
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-11-23 16:21:20 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=369382 https://bugs.eclipse.org/bugs/show_bug.cgi?id=369208

--- plugins/infra/table/org.eclipse.papyrus.infra.table.common/src/org/eclipse/papyrus/infra/table/common/listener/AbstractSynchronizedTableTriggerListener.java
+++ plugins/infra/table/org.eclipse.papyrus.infra.table.common/src/org/eclipse/papyrus/infra/table/common/listener/AbstractSynchronizedTableTriggerListener.java
@@ -200,18 +200,19 @@ public abstract class AbstractSynchronizedTableTriggerListener extends TriggerLi
 						TableInstance table = provider.getNatTableWidget().getTableInstance();
 						List<Row> newOrder = new ArrayList<Row>();
 						List<Row> rows = table.getRows();
-						Assert.isTrue(rows.size() == allElements.size());
-						for(EObject current : allElements) {
-							for(Row currentRow : rows) {
-								if(currentRow.getElement() == current) {
-									newOrder.add(currentRow);
-									break;
+
+						//Assert.isTrue(rows.size() == allElements.size());//not the case when we are deleting elements of the table
+							for(EObject current : allElements) {
+								for(Row currentRow : rows) {
+									if(currentRow.getElement() == current) {
+										newOrder.add(currentRow);
+										break;
+									}
 								}
 							}
-						}
-						ICommandFactory commandFactory = ICommandFactoriesRegistry.INSTANCE.getCommandFactoryFor(domain);
-						Command command = commandFactory.createSetCommand(domain, table, TableinstancePackage.eINSTANCE.getTableInstance_Rows(), newOrder);
-						command.execute();
+							ICommandFactory commandFactory = ICommandFactoriesRegistry.INSTANCE.getCommandFactoryFor(domain);
+							Command command = commandFactory.createSetCommand(domain, table, TableinstancePackage.eINSTANCE.getTableInstance_Rows(), newOrder);
+							command.execute();
 						return null;
 					}
 				}));
