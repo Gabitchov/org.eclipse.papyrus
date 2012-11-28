011bb489bf350005ea1f78a41063a584c0633222
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-11-28 10:08:45 +0000

    Save modifications for oep.infra.emf.export

--- sandbox/org.eclipse.papyrus.infra.emf.export/src/org/eclipse/papyrus/infra/emf/export/handler/EMFSaveAsHandler.java
+++ sandbox/org.eclipse.papyrus.infra.emf.export/src/org/eclipse/papyrus/infra/emf/export/handler/EMFSaveAsHandler.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -22,6 +24,7 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
@@ -48,30 +51,34 @@ public class EMFSaveAsHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ResourceSet set = new ResourceSetImpl();
-		final Resource selectedResource = getSelectedResource(set);
+		final List<Resource> selectedResources = getSelectedResource(set);
 
 		// the dialog used to choose a file to browse
 		final CheckedTreeSelectionDialog dialog = new CheckedTreeSelectionDialog(Display.getCurrent().getActiveShell(), getLabelProvider(), getContentProvider());
 		dialog.setTitle("Browse Model");
 		dialog.setMessage("Select the elements to export");
-		dialog.setInput(selectedResource);
+		dialog.setInput(selectedResources);
 
 		if(dialog.open() == Window.OK) {
 			final Object[] selectedEObject = dialog.getResult();
 			final Collection<EObject> toExport = new ArrayList<EObject>();
 			for(final Object current : selectedEObject) {
 				assert current instanceof EObject;
+				EcoreUtil.resolveAll((EObject)current);
 				toExport.add((EObject)current);
 			}
 
 			SaveAsDialog saveAsDialog = new SaveAsDialog(Display.getCurrent().getActiveShell());
-//			saveAsDialog.setTitle("Select the destination for the exported elements"); //without effect
+			//			saveAsDialog.setTitle("Select the destination for the exported elements"); //without effect
 			if(saveAsDialog.open() == Window.OK) {
 				Resource newResource = createNewResource(set, saveAsDialog.getResult());
 				newResource.getContents().addAll(toExport);
 				try {
 					newResource.save(Collections.emptyMap());
-					selectedResource.save(Collections.emptyMap());
+					for(final Resource current : selectedResources) {
+						current.save(Collections.emptyMap());
+					}
+
 				} catch (IOException e) {
 					Activator.log.error(e);
 				}
@@ -89,11 +96,12 @@ public class EMFSaveAsHandler extends AbstractHandler {
 	 */
 	@Override
 	public boolean isEnabled() {
-		return getSelectedResource(new ResourceSetImpl()) != null;
+		return getSelectedResource(new ResourceSetImpl()).size() != 0;
 	}
 
 	/**
 	 * FIXME : use the label provider service
+	 * 
 	 * @return
 	 *         the label provider to use
 	 */
@@ -107,6 +115,7 @@ public class EMFSaveAsHandler extends AbstractHandler {
 
 	/**
 	 * FIXME : use the content provider service
+	 * 
 	 * @return
 	 *         the content provider to use
 	 */
@@ -119,6 +128,9 @@ public class EMFSaveAsHandler extends AbstractHandler {
 				public Object[] getElements(Object inputElement) {
 					if(inputElement instanceof Resource) {
 						return ((Resource)inputElement).getContents().toArray();
+
+					} else if(inputElement instanceof Collection<?>) {
+						return ((Collection)inputElement).toArray();
 					} else {
 						return super.getElements(inputElement);
 					}
@@ -135,22 +147,48 @@ public class EMFSaveAsHandler extends AbstractHandler {
 	 * @return
 	 *         the selected resource
 	 */
-	protected Resource getSelectedResource(final ResourceSet set) {
-		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
-		final Object selectedElement = ((IStructuredSelection)selection).getFirstElement();
-		String pathName = "";
-		Resource res = null;
-		if(selectedElement instanceof IAdaptable) {
-			final File file = (File)((IAdaptable)selectedElement).getAdapter(IFile.class);
-			if(file!=null){
-				if(selectedElement instanceof File) {
-					pathName = ((File)selectedElement).getFullPath().toString();
+	protected List<Resource> getSelectedResource(final ResourceSet set) {
+		IStructuredSelection selection = (IStructuredSelection)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+		final List<Resource> selectedResources = new ArrayList<Resource>();
+		final List<?> selectedElement = selection.toList();
+		for(Object object : selectedElement) {
+
+			if(object instanceof IAdaptable) {
+				final File file = (File)((IAdaptable)object).getAdapter(IFile.class);
+				if(file != null) {
+
+					final String pathName = ((File)file).getFullPath().toString();
+
+
+					final URI uri = URI.createPlatformResourceURI(pathName, true);
+					final Resource res = set.getResource(uri, true);
+					EcoreUtil.resolveAll(res);
+					if(pathName.contains("notation")) {
+
+						selectedResources.add(0, res);
+					} else {
+						selectedResources.add(res);
+					}
 				}
-				final URI uri = URI.createFileURI(pathName);
-				 res = set.getResource(uri, true);
 			}
+
 		}
-		return res;
+		//		final Object selectedElement = ((IStructuredSelection)selection).getFirstElement();
+		//		String pathName = "";
+		//		Resource res = null;
+		//		if(selectedElement instanceof IAdaptable) {
+		//			final File file = (File)((IAdaptable)selectedElement).getAdapter(IFile.class);
+		//			if(file!=null){
+		//			
+		//					pathName = ((File)file).getFullPath().toString();
+		//
+		//				final URI uri = URI.createPlatformResourceURI(pathName, true);
+		//				 res = set.getResource(uri, true);
+		//			}
+		//		}
+		//		return res;
+		//		System.out.println(object);
+		return selectedResources;
 	}
 
 	/**
@@ -163,18 +201,19 @@ public class EMFSaveAsHandler extends AbstractHandler {
 	 *         the created resource with the wanted path
 	 */
 	protected Resource createNewResource(final ResourceSet set, final IPath iPath) {
-//		EcoreResourceUtil.
-//		URIConverter converter = set.getURIConverter();
-//		converter.
-//		IPath wsPath = ResourcesPlugin.getWorkspace().getDescription()
-//		java.io.File file = iPath.toFile();
-//		file.
-//		URI.createPlatformResourceURI(pathName)
-		
+		//		EcoreResourceUtil.
+		//		URIConverter converter = set.getURIConverter();
+		//		converter.
+		//		IPath wsPath = ResourcesPlugin.getWorkspace().getDescription()
+		//		java.io.File file = iPath.toFile();
+		//		file.
+		//		URI.createPlatformResourceURI(pathName)
+
 		//FIXME : the references to the new files are stored with OS path (F:/....)
-		IPath fullPath = ResourcesPlugin.getWorkspace().getRoot().getFile(iPath).getLocation(); 
-//		fullPath.get
-		final URI uri = URI.createFileURI(fullPath.toFile().getAbsolutePath());
+		IPath fullPath = ResourcesPlugin.getWorkspace().getRoot().getFile(iPath).getLocation();
+		//		fullPath.get
+		final URI uri = URI.createPlatformResourceURI(iPath.toString(), true);//FileURI(fullPath.toFile().getAbsolutePath());
+		final URI uri2 = URI.createPlatformResourceURI(iPath.toFile().getPath(), true);
 		String device = iPath.getDevice();
 		final Resource newResource = set.createResource(uri);
 		return newResource;
