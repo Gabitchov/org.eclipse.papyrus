ca4f25fedb0e97742066eed1c2e8b7c82c205943
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-11-29 14:46:46 +0000

    394747: New features developed on EFacet Table should be backported on the old version of the EMF Facet table https://bugs.eclipse.org/bugs/show_bug.cgi?id=394747

--- plugins/uml/profilefacet/org.eclipse.papyrus.uml.profilefacet/src/org/eclipse/papyrus/uml/profilefacet/queries/setter/SetGenericSingleValue.java
+++ plugins/uml/profilefacet/org.eclipse.papyrus.uml.profilefacet/src/org/eclipse/papyrus/uml/profilefacet/queries/setter/SetGenericSingleValue.java
@@ -13,72 +13,103 @@
  *****************************************************************************/
 package org.eclipse.papyrus.uml.profilefacet.queries.setter;
 
+import java.util.Collection;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.facet.infra.facet.core.FacetContext;
 import org.eclipse.emf.facet.infra.query.core.exception.ModelQueryExecutionException;
-import org.eclipse.emf.facet.infra.query.core.java.IJavaModelQuery;
 import org.eclipse.emf.facet.infra.query.core.java.IJavaModelQueryWithEditingDomain;
 import org.eclipse.emf.facet.infra.query.core.java.ParameterValueList;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eclipse.papyrus.commands.wrappers.GMFtoEMFCommandWrapper;
+import org.eclipse.papyrus.uml.profilefacet.metamodel.profilefacet.StereotypeFacet;
+import org.eclipse.papyrus.uml.profilefacet.metamodel.profilefacet.StereotypePropertyElement;
+import org.eclipse.papyrus.uml.profilefacet.utils.ArgumentUtils;
+import org.eclipse.papyrus.uml.tools.utils.NamedElementUtil;
+import org.eclipse.papyrus.uml.tools.utils.StereotypeUtil;
 import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Stereotype;
 
-
-public class SetGenericSingleValue<R> implements IJavaModelQuery<Element, R> {
+public class SetGenericSingleValue<R> implements IJavaModelQueryWithEditingDomain<Element, R> {
 
 
 	//FIXME : maybe we should implements IJavaModelQueryWithEditingDomain
 	public R evaluate(Element source, ParameterValueList parameterValues) throws ModelQueryExecutionException {
-		//FIXME
-		//		final StereotypePropertyElement element = (StereotypePropertyElement)parameterValues.getParameterValueByName(Constants.EDITED_FEATURE).getValue();
-		//		if(element != null) {
-		//			final EObject container = element.eContainer();
-		//			assert container instanceof StereotypeFacet;
-		//			final StereotypeFacet facet = (StereotypeFacet)container;
-		//			final String stereotypeQN = facet.getStereotypeQualifiedName();
-		//			final Stereotype appliedStereotype = source.getAppliedStereotype(stereotypeQN);
-		//			final String propertyQN = element.getPropertyQualifiedName();
-		//			final String propertyName = NamedElementUtil.getNameFromQualifiedName(propertyQN);
-		//			if(appliedStereotype != null) {
-		//				return (R)source.getValue(appliedStereotype, propertyName);
-		//			} else if(appliedStereotype == null) {//the property can comes from inheritance...
-		//				for(Stereotype appliedSte : source.getAppliedStereotypes()) {
-		//					final Collection<Stereotype> superClasses = StereotypeUtil.getAllSuperStereotypes(appliedSte);
-		//					for(final Stereotype current : superClasses) {
-		//						if(current.getQualifiedName().equals(stereotypeQN)) {
-		//							return (R)source.getValue(appliedSte, propertyName);
-		//						}
-		//					}
-		//				}
-		//
-		//			}
-		//		}
+		final R newValue = (R)parameterValues.getValueByParameterName(FacetContext.NEW_VALUE);
+		final StereotypePropertyElement element = (StereotypePropertyElement)parameterValues.getParameterValueByName(ArgumentUtils.EDITED_FEATURE).getValue();
+		if(element != null) {
+			final EObject container = element.eContainer();
+			assert container instanceof StereotypeFacet;
+			final StereotypeFacet facet = (StereotypeFacet)container;
+			final String stereotypeQN = facet.getStereotypeQualifiedName();
+			final Stereotype appliedStereotype = source.getAppliedStereotype(stereotypeQN);
+			final String propertyQN = element.getPropertyQualifiedName();
+			final String propertyName = NamedElementUtil.getNameFromQualifiedName(propertyQN);
+			if(appliedStereotype != null) {
+				source.setValue(appliedStereotype, propertyName, newValue);
+			} else if(appliedStereotype == null) {//the property can comes from inheritance...
+				for(Stereotype appliedSte : source.getAppliedStereotypes()) {
+					final Collection<Stereotype> superClasses = StereotypeUtil.getAllSuperStereotypes(appliedSte);
+					for(final Stereotype current : superClasses) {
+						if(current.getQualifiedName().equals(stereotypeQN)) {
+							source.setValue(appliedSte, propertyName, newValue);
+						}
+					}
+				}
+
+			}
+		}
 
 		return null;
 	}
 
-	//	public R evaluate(final Element source, final IParameterValueList2 parameterValues, final IFacetManager facetManager) throws DerivedTypedElementException {
-	//		final StereotypePropertyElement element = (StereotypePropertyElement)parameterValues.getParameterValueByName(Constants.EDITED_FEATURE).getValue();
-	//		if(element != null) {
-	//			final EObject container = element.eContainer();
-	//			assert container instanceof StereotypeFacet;
-	//			final StereotypeFacet facet = (StereotypeFacet)container;
-	//			final String stereotypeQN = facet.getStereotypeQualifiedName();
-	//			final Stereotype appliedStereotype = source.getAppliedStereotype(stereotypeQN);
-	//			final String propertyQN = element.getPropertyQualifiedName();
-	//			final String propertyName = NamedElementUtil.getNameFromQualifiedName(propertyQN);
-	//			if(appliedStereotype != null) {
-	//				return (R)source.getValue(appliedStereotype, propertyName);
-	//			} else if(appliedStereotype == null) {//the property can comes from inheritance...
-	//				for(Stereotype appliedSte : source.getAppliedStereotypes()) {
-	//					final Collection<Stereotype> superClasses = StereotypeUtil.getAllSuperStereotypes(appliedSte);
-	//					for(final Stereotype current : superClasses) {
-	//						if(current.getQualifiedName().equals(stereotypeQN)) {
-	//							return (R)source.getValue(appliedSte, propertyName);
-	//						}
-	//					}
-	//				}
-	//
-	//			}
-	//		}
-	//	
-	//		return null;
-	//	}
+	public R evaluate(final Element source, final ParameterValueList parameterValues, final EditingDomain editingDomain) throws ModelQueryExecutionException {
+		assert editingDomain != null;
+		final R newValue = (R)parameterValues.getValueByParameterName(FacetContext.NEW_VALUE);
+		final StereotypePropertyElement element = (StereotypePropertyElement)parameterValues.getParameterValueByName(ArgumentUtils.EDITED_FEATURE).getValue();
+		Stereotype foundStereotype = null;
+		if(element != null) {
+			final EObject container = element.eContainer();
+			assert container instanceof StereotypeFacet;
+			final StereotypeFacet facet = (StereotypeFacet)container;
+			final String stereotypeQN = facet.getStereotypeQualifiedName();
+			final Stereotype appliedStereotype = source.getAppliedStereotype(stereotypeQN);
+			final String propertyQN = element.getPropertyQualifiedName();
+			final String propertyName = NamedElementUtil.getNameFromQualifiedName(propertyQN);
+			if(appliedStereotype != null) {
+				foundStereotype = appliedStereotype;//source.setValue(appliedStereotype, propertyName, newValue);
+			} else if(appliedStereotype == null) {//the property can comes from inheritance...
+				loop: for(Stereotype appliedSte : source.getAppliedStereotypes()) {
+					final Collection<Stereotype> superClasses = StereotypeUtil.getAllSuperStereotypes(appliedSte);
+					for(final Stereotype current : superClasses) {
+						if(current.getQualifiedName().equals(stereotypeQN)) {
+							foundStereotype = appliedSte;
+							break loop;
+						}
+					}
+				}
+			}
+			if(foundStereotype != null) {
+				final Stereotype stereotype = foundStereotype;
+				final ICommand setValueCommand = new AbstractTransactionalCommand((TransactionalEditingDomain)editingDomain, "Set Stereotype Value Command", null) { //$NON-NLS-1$
 
+					@Override
+					protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+						source.setValue(stereotype, propertyName, newValue);
+						return CommandResult.newOKCommandResult();
+					}
+				};
+
+				editingDomain.getCommandStack().execute(new GMFtoEMFCommandWrapper(setValueCommand));
+			}
+		}
+		return null;
+	}
 }
