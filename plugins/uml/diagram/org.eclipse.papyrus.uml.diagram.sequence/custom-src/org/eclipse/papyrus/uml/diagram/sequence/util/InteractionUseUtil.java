92409d33e33754c81afdca75541ca79d60fe8c54
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-01 13:06:05 +0000

    398712: [Core] Multiple issues with the PageManager https://bugs.eclipse.org/bugs/show_bug.cgi?id=398712

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/util/InteractionUseUtil.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/util/InteractionUseUtil.java
@@ -4,31 +4,34 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.papyrus.infra.core.sasheditor.contentprovider.IPageMngr;
-import org.eclipse.papyrus.infra.core.utils.ServiceUtilsForActionHandlers;
+import org.eclipse.papyrus.infra.core.sasheditor.contentprovider.IPageManager;
+import org.eclipse.papyrus.infra.emf.utils.ServiceUtilsForEObject;
 import org.eclipse.uml2.uml.Interaction;
 
 
 public class InteractionUseUtil {
- 
+
 	public static Diagram findDiagram(View view, Interaction refInteraction) {
 		Diagram diagram = view.getDiagram();
 		EList<EObject> list = diagram.eResource().getContents();
-		for(EObject o : list)
-			if(o instanceof Diagram){
+		for(EObject o : list) {
+			if(o instanceof Diagram) {
 				Diagram ref = (Diagram)o;
-				if(refInteraction.equals(ref.getElement()))
+				if(refInteraction.equals(ref.getElement())) {
 					return ref;
+				}
 			}
-		
+		}
+
 		return null;
 	}
- 
-	public static void openDiagram(Diagram diag){
-		IPageMngr pageMngr = null;
+
+	public static void openDiagram(Diagram diag) {
+		IPageManager pageMngr;
 		try {
-			pageMngr = ServiceUtilsForActionHandlers.getInstance().getIPageMngr();
+			pageMngr = ServiceUtilsForEObject.getInstance().getIPageManager(diag);
 		} catch (Exception e) {
+			return;
 		}
 		if(pageMngr != null) {
 			if(pageMngr.allPages().contains(diag)) {
@@ -36,10 +39,11 @@ public class InteractionUseUtil {
 				 * Close the diagram if it was already open
 				 */
 				if(pageMngr.isOpen(diag)) {
-					pageMngr.closePage(diag);
+					pageMngr.selectPage(diag);
+				} else {
+					pageMngr.openPage(diag);
 				}
-				pageMngr.openPage(diag);
 			}
 		}
- 	}
+	}
 }
