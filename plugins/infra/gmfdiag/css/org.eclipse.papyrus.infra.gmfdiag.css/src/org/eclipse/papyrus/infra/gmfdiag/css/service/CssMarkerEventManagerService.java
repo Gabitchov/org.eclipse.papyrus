0bf7659c5e08e2af592b80d3e5961ceca9777419
acuccuru <acuccuru@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-01 09:54:44 +0000

    400254: [css] The CSS Engine should be sensitive to markers https://bugs.eclipse.org/bugs/show_bug.cgi?id=400254
    This commit addresses the issue related to the CssMarkerEventManagerService

--- plugins/infra/gmfdiag/css/org.eclipse.papyrus.infra.gmfdiag.css/src/org/eclipse/papyrus/infra/gmfdiag/css/service/CssMarkerEventManagerService.java
+++ plugins/infra/gmfdiag/css/org.eclipse.papyrus.infra.gmfdiag.css/src/org/eclipse/papyrus/infra/gmfdiag/css/service/CssMarkerEventManagerService.java
@@ -8,21 +8,18 @@ import java.util.MissingResourceException;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EValidator;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.papyrus.infra.core.services.IService;
 import org.eclipse.papyrus.infra.core.services.ServiceException;
 import org.eclipse.papyrus.infra.core.services.ServicesRegistry;
+import org.eclipse.papyrus.infra.emf.utils.ResourceUtils;
 import org.eclipse.papyrus.infra.gmfdiag.css.editpolicies.MarkerEventListenerEditPolicy;
 import org.eclipse.papyrus.infra.services.markerlistener.IMarkerEventListener;
 
@@ -38,15 +35,18 @@ public class CssMarkerEventManagerService implements IMarkerEventListener {
 	}
 
 	public void startService() throws ServiceException {
-		// TODO Auto-generated method stub
-
+		// Nothing specific to be done
 	}
 
 	public void disposeService() throws ServiceException {
-		// TODO Auto-generated method stub
-
+		// Nothing specific to be done
 	}
 
+	/**
+	 * Registers a MarkerEventListenerEditPolicy with this CssMarkerEventManagerService
+	 * 
+	 * @param editPolicy The edit policy to be registered with this CssMarkerEventManagerService
+	 */
 	public void registerEditPolicy(MarkerEventListenerEditPolicy editPolicy) {
 		Assert.isTrue(editPolicy.getHost().getModel() instanceof View) ;
 		if (editPolicy.getHost().getModel() instanceof View) {
@@ -66,7 +66,7 @@ public class CssMarkerEventManagerService implements IMarkerEventListener {
 				eObjectURIToEditPolicies.put(semanticElementURI, correspondingGraphicalEditPolicies) ;
 				
 				// retrieves all markers associated with the resource of the semanticElement, and pass these markers for notification
-				IResource resource = this.getIResource(semanticElement.eResource()) ;
+				IResource resource = ResourceUtils.getFile(semanticElement.eResource()) ;
 				if (resource != null) {
 					try {
 						IMarker[] allPapyrusMarkers = resource.findMarkers("org.eclipse.papyrus.modelmarker", true, IResource.DEPTH_INFINITE) ;
@@ -86,6 +86,11 @@ public class CssMarkerEventManagerService implements IMarkerEventListener {
 		}
 	}
 
+	/**
+	 * Unregisters the given MarkerEventListenerEditPolicy from this CssMarkerEventManagerService
+	 * 
+	 * @param editPolicy The edit policy to be unregistered from this CssMarkerEventManagerService
+	 */
 	public void unregisterEditPolicy(MarkerEventListenerEditPolicy editPolicy) {
 		Assert.isTrue(editPolicy.getHost().getModel() instanceof View) ;
 		if (editPolicy.getHost().getModel() instanceof View) {
@@ -106,6 +111,9 @@ public class CssMarkerEventManagerService implements IMarkerEventListener {
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.papyrus.infra.services.markerlistener.IMarkerEventListener#notifyMarkerChange(org.eclipse.emf.ecore.EObject, org.eclipse.core.resources.IMarker, int)
+	 */
 	public void notifyMarkerChange(EObject semanticElement, IMarker marker, int markerState) {
 		if (semanticElement != null) {
 			String semanticElementURI = EcoreUtil.getURI(semanticElement).toString() ;
@@ -116,18 +124,6 @@ public class CssMarkerEventManagerService implements IMarkerEventListener {
 				}
 			}
 		}
-
-	}
-
-	protected IResource getIResource(Resource resource) {
-		String uriPath = resource.getURI().toPlatformString(true);
-		IResource iresource = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(uriPath));
-		if(iresource != null) {
-			if(iresource.exists()) {
-				return iresource;
-			}
-		}
-		return null;
 	}
 	
 	/**
@@ -164,6 +160,9 @@ public class CssMarkerEventManagerService implements IMarkerEventListener {
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.papyrus.infra.services.markerlistener.IMarkerEventListener#isNotifiedOnInitialMarkerCheck()
+	 */
 	public boolean isNotifiedOnInitialMarkerCheck() {
 		return false;
 	}
