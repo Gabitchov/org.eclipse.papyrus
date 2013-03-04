b2a0869434a00bad2b41b6d485fd58dc38b347cc
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-04 10:52:09 +0000

    402196: [Property View] Appearance tab slows down every selection https://bugs.eclipse.org/bugs/show_bug.cgi?id=402196

--- plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.widgets/src/org/eclipse/papyrus/infra/gmfdiag/widgets/editors/XWTAwareFontEditor.java
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.widgets/src/org/eclipse/papyrus/infra/gmfdiag/widgets/editors/XWTAwareFontEditor.java
@@ -11,16 +11,58 @@
  *****************************************************************************/
 package org.eclipse.papyrus.infra.gmfdiag.widgets.editors;
 
+import java.text.Collator;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.papyrus.infra.widgets.editors.ReferenceCombo;
+import org.eclipse.papyrus.infra.widgets.providers.IStaticContentProvider;
+import org.eclipse.papyrus.infra.widgets.providers.StaticContentProvider;
 import org.eclipse.papyrus.views.properties.widgets.AbstractPropertyEditor;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 
 
 public class XWTAwareFontEditor extends AbstractPropertyEditor {
 
-	private FontEditor editor;
+	private ReferenceCombo editor;
+
+	private static IStaticContentProvider fontContentProvider;
 
 	public XWTAwareFontEditor(Composite parent, int style) {
-		editor = new FontEditor(parent, style);
+		editor = new ReferenceCombo(parent, style);
 		setEditor(editor);
 	}
+
+	@Override
+	protected void doBinding() {
+		IStaticContentProvider contentProvider = getFontContentProvider();
+
+		editor.setContentProvider(contentProvider);
+		editor.setUnsettable(!input.isMandatory(propertyPath));
+
+		super.doBinding();
+	}
+
+	private synchronized static IStaticContentProvider getFontContentProvider() {
+		if(fontContentProvider == null) {
+			FontData[] fontDatas = Display.getDefault().getFontList(null, true);
+
+			Set<String> fontNames = new HashSet<String>();
+			for(FontData data : fontDatas) {
+				if(data.getName() != null) {
+					fontNames.add(data.getName());
+				}
+			}
+
+			String[] uniqueFontNames = fontNames.toArray(new String[fontNames.size()]);
+			Arrays.sort(uniqueFontNames, Collator.getInstance());
+
+			fontContentProvider = new StaticContentProvider(uniqueFontNames);
+		}
+		return fontContentProvider;
+	}
+
 }
