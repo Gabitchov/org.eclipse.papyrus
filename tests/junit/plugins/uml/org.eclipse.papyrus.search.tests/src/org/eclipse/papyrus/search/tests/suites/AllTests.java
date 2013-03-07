38dc27cb18f515fb68775129b88fe5386219e3d7
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-07 16:19:38 +0000

    399856: [Search] Papyrus shall provide model-aware search capability https://bugs.eclipse.org/bugs/show_bug.cgi?id=399856
    Minor fixes

--- tests/junit/plugins/uml/org.eclipse.papyrus.search.tests/src/org/eclipse/papyrus/search/tests/suites/AllTests.java
+++ tests/junit/plugins/uml/org.eclipse.papyrus.search.tests/src/org/eclipse/papyrus/search/tests/suites/AllTests.java
@@ -2,11 +2,13 @@ package org.eclipse.papyrus.search.tests.suites;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 /**
  * Main Test suite.
  */
 @RunWith(Suite.class)
+@SuiteClasses({})
 public class AllTests {
 	//TODO: Model-Search tests
 }
