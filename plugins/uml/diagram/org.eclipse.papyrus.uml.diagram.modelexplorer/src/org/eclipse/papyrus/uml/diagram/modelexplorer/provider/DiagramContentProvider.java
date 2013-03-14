75603b579db5fbe9c39b1d18362c085c400844d4
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-14 13:26:15 +0000

    336827: [Model Explorer] Model explorer shall display only diagrams https://bugs.eclipse.org/bugs/show_bug.cgi?id=336827
    Change the DiagramContentProvider to use EMF Facet customizations

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.modelexplorer/src/org/eclipse/papyrus/uml/diagram/modelexplorer/provider/DiagramContentProvider.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.modelexplorer/src/org/eclipse/papyrus/uml/diagram/modelexplorer/provider/DiagramContentProvider.java
@@ -15,9 +15,7 @@ package org.eclipse.papyrus.uml.diagram.modelexplorer.provider;
 
 import java.util.ArrayList;
 
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.papyrus.infra.core.resource.ModelUtils;
 import org.eclipse.papyrus.infra.core.sasheditor.contentprovider.IPageManager;
 import org.eclipse.papyrus.infra.core.services.ServicesRegistry;
@@ -29,39 +27,8 @@ import org.eclipse.papyrus.uml.diagram.modelexplorer.Activator;
  * this content provider is used to display diagrams and their contents
  * 
  */
-//TODO : This ContentProvider should extend SemanticUMLContentProvider
 @Deprecated
 public class DiagramContentProvider extends MoDiscoContentProvider {
-
-	@Override
-	public Object[] getChildren(final Object parentElement) {
-		ArrayList<Object> result = new ArrayList<Object>();
-
-
-		if(parentElement instanceof IAdaptable) {
-			EObject eObject = (EObject)((IAdaptable)parentElement).getAdapter(EObject.class);
-			if(eObject instanceof Diagram) {
-				return result.toArray();
-			}
-		}
-
-		Object[] arrayObject = super.getChildren(parentElement);
-		if(arrayObject != null) {
-			for(int i = 0; i < arrayObject.length; i++) {
-				result.add(arrayObject[i]);
-			}
-		}
-		return result.toArray();
-	}
-
-	@Override
-	public Object[] getElements(final Object inputElement) {
-		Object[] rootElements = getRootElements(inputElement);
-		if(rootElements == null) {
-			return null;
-		}
-		return rootElements;
-	}
 
 	@Override
 	public EObject[] getRootElements(Object inputElement) {
