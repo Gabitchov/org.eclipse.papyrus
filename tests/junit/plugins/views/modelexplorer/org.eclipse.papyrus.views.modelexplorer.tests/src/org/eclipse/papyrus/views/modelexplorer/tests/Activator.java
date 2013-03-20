056cd65e2b94eea6c7d1495b15ae88b9a5be68f7
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-20 08:41:51 +0000

    [Trivial] Fix PLUGIN_ID for new or test plug-ins

--- tests/junit/plugins/views/modelexplorer/org.eclipse.papyrus.views.modelexplorer.tests/src/org/eclipse/papyrus/views/modelexplorer/tests/Activator.java
+++ tests/junit/plugins/views/modelexplorer/org.eclipse.papyrus.views.modelexplorer.tests/src/org/eclipse/papyrus/views/modelexplorer/tests/Activator.java
@@ -22,7 +22,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.infra.modelexplorer.tests"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.views.modelexplorer.tests"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
