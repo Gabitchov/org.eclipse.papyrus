39fdafc3ea7e219f208f7f5fabfd5b8d4f7fe967
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-20 10:32:57 +0000

    402662: [Editor - Services] Papyrus shall provide a Service to open page(s) associated to an Element https://bugs.eclipse.org/bugs/show_bug.cgi?id=402662
    Implement the missing methods in OpenElementService

--- incoming/org.eclipse.papyrus.infra.services.openelement/src/org/eclipse/papyrus/infra/services/openelement/service/impl/OpenElementServiceImpl.java
+++ incoming/org.eclipse.papyrus.infra.services.openelement/src/org/eclipse/papyrus/infra/services/openelement/service/impl/OpenElementServiceImpl.java
@@ -14,6 +14,7 @@ package org.eclipse.papyrus.infra.services.openelement.service.impl;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -78,45 +79,58 @@ public class OpenElementServiceImpl implements OpenElementService {
 		registry = null;
 	}
 
-	public IMultiDiagramEditor openElement(final EObject element) throws PartInitException, ServiceException {
-		IMultiDiagramEditor editor = getCurrentEditor();
+	/**
+	 * {@inheritDoc}
+	 */
+	public IMultiDiagramEditor openElement(final EObject viewElement) throws PartInitException, ServiceException {
+		URI pageURI = getPageForViewElement(viewElement);
+		if(pageURI == null) {
+			return null;
+		}
 
-		URI[] pages = getPages(element);
+		return openURI(pageURI);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public IMultiDiagramEditor openSemanticElement(final EObject semanticElement) throws PartInitException, ServiceException {
+		URI[] pages = getPagesForSemanticElement(semanticElement);
+		return openSemanticElement(semanticElement, pages);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public IMultiDiagramEditor openSemanticElement(EObject semanticElement, Object[] pages) throws PartInitException, ServiceException {
+		URI[] pageURIs = new URI[pages.length];
+		int i = 0;
+
+		for(Object pageElement : pages) {
+			URI uri = null;
+			if(pageElement instanceof EObject) {
+				uri = EcoreUtil.getURI((EObject)pageElement);
+			}
+			pageURIs[i++] = uri;
+		}
+
+		return openSemanticElement(semanticElement, pageURIs);
+	}
+
+	private IMultiDiagramEditor openSemanticElement(final EObject semanticElement, URI[] pageURIs) throws ServiceException, PartInitException {
+		IMultiDiagramEditor editor = getCurrentEditor();
 
 		if(editor == null) {
 			//Cannot find the IMultiDiagramEditor: try to open a new one
-
-			IFile diFile = getDiFile(element);
-
-			final IEditorInput input = new PapyrusPageInput(diFile, pages, false);
-
-			RunnableWithResult<IMultiDiagramEditor> runnable;
-			Display.getDefault().syncExec(runnable = new RunnableWithResult.Impl() {
-
-				public void run() {
-					try {
-						final IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-						IMultiDiagramEditor openedEditor = (IMultiDiagramEditor)IDE.openEditor(page, input, PAPYRUS_EDITOR_ID);
-						setResult(openedEditor);
-						setStatus(Status.OK_STATUS);
-					} catch (PartInitException ex) {
-						setStatus(new Status(IStatus.ERROR, Activator.PLUGIN_ID, ex.getMessage(), ex));
-					}
-				}
-			});
-
-			if(runnable.getStatus().getSeverity() == IStatus.ERROR) {
-				throw (PartInitException)runnable.getStatus().getException();
-			}
-
-			editor = runnable.getResult();
+			IFile diFile = getDiFile(semanticElement);
+			editor = openURIsInNewEditor(diFile, pageURIs);
 		} else {
 			final IPageManager pageMngr = registry.getService(IPageManager.class);
 			ModelSet modelSet = registry.getService(ModelSet.class);
-			final List<EObject> pagesToOpen = new LinkedList<EObject>();
-			final List<EObject> pagesToSelect = new LinkedList<EObject>();
+			final LinkedHashSet<EObject> pagesToOpen = new LinkedHashSet<EObject>();
+			final LinkedHashSet<EObject> pagesToSelect = new LinkedHashSet<EObject>();
 
-			for(URI pageURI : pages) {
+			for(URI pageURI : pageURIs) {
 				final EObject page = modelSet.getEObject(pageURI, true);
 
 				if(pageMngr.allPages().contains(page)) {
@@ -142,6 +156,8 @@ public class OpenElementServiceImpl implements OpenElementService {
 				editingDomain.getCommandStack().execute(command);
 			}
 
+			//More than one editor can be visible (SashEditor). 
+			//We need to select each page
 			for(EObject page : pagesToSelect) {
 				pageMngr.selectPage(page);
 			}
@@ -152,7 +168,7 @@ public class OpenElementServiceImpl implements OpenElementService {
 			Display.getDefault().syncExec(new Runnable() {
 
 				public void run() {
-					revealElement.revealSemanticElement(Collections.singletonList(element));
+					revealElement.revealSemanticElement(Collections.singletonList(semanticElement));
 				}
 			});
 
@@ -161,16 +177,16 @@ public class OpenElementServiceImpl implements OpenElementService {
 		return editor;
 	}
 
-	private URI[] getPages(EObject element) {
+	private URI[] getPagesForSemanticElement(EObject element) {
 		try {
-			IPageManager pageMngr = registry.getService(IPageManager.class);
+			IPageManager pageManager = registry.getService(IPageManager.class);
 
 			Collection<EStructuralFeature.Setting> usages = EMFHelper.getUsages(element);
 			List<URI> result = new LinkedList<URI>();
 			for(EStructuralFeature.Setting usage : usages) {
 				EObject referencer = usage.getEObject();
 				do {
-					if(pageMngr.allPages().contains(referencer)) {
+					if(pageManager.allPages().contains(referencer)) {
 						result.add(EcoreUtil.getURI(referencer));
 						break;
 					}
@@ -179,7 +195,7 @@ public class OpenElementServiceImpl implements OpenElementService {
 
 			return result.toArray(new URI[result.size()]);
 		} catch (ServiceException ex) {
-			//There is not IPageMngr: return all usages' URIs
+			//There is no IPageManager: return all usages' URIs
 			Set<URI> result = new HashSet<URI>();
 			Collection<EStructuralFeature.Setting> usages = EMFHelper.getUsages(element);
 			for(EStructuralFeature.Setting usage : usages) {
@@ -195,10 +211,40 @@ public class OpenElementServiceImpl implements OpenElementService {
 		}
 	}
 
-	private IFile getDiFile(EObject element) {
-		Resource resource = element.eResource();
-		URI fileURI = resource.getURI();
-		fileURI = fileURI.trimFileExtension();
+	private URI getPageForViewElement(EObject viewElement) {
+		if(viewElement == null) {
+			return null;
+		}
+
+		try {
+			IPageManager pageManager = registry.getService(IPageManager.class);
+
+			EObject currentViewElement = viewElement;
+			while(currentViewElement != null) {
+				if(pageManager.allPages().contains(currentViewElement)) {
+					break;
+				}
+
+				currentViewElement = currentViewElement.eContainer();
+			}
+
+			if(currentViewElement != null) {
+				return EcoreUtil.getURI(currentViewElement);
+			}
+		} catch (ServiceException ex) {
+			//There is no IPageManager (The editor is not opened yet)	
+			//The page is "probably" the root element of the view
+			EObject rootElement = EcoreUtil.getRootContainer(viewElement);
+			if(rootElement != null) {
+				return EcoreUtil.getURI(rootElement);
+			}
+		}
+
+		return null;
+	}
+
+	private IFile getDiFile(URI uri) {
+		URI fileURI = uri.trimFileExtension();
 		fileURI = fileURI.appendFileExtension(SashModel.MODEL_FILE_EXTENSION);
 
 		if(fileURI.isPlatformResource()) {
@@ -223,6 +269,12 @@ public class OpenElementServiceImpl implements OpenElementService {
 		return null;
 	}
 
+	private IFile getDiFile(EObject element) {
+		Resource resource = element.eResource();
+		URI fileURI = resource.getURI();
+		return getDiFile(fileURI);
+	}
+
 	private IMultiDiagramEditor getCurrentEditor() {
 		try {
 			IMultiDiagramEditor currentEditor = registry.getService(IMultiDiagramEditor.class);
@@ -232,14 +284,61 @@ public class OpenElementServiceImpl implements OpenElementService {
 		}
 	}
 
-	public IMultiDiagramEditor openSemanticElement(EObject semanticElement) throws PartInitException, ServiceException {
-		// TODO Auto-generated method stub
-		return null;
+	private IMultiDiagramEditor openURI(URI pageURI) throws ServiceException, PartInitException {
+		IMultiDiagramEditor editor = getCurrentEditor();
+		if(editor == null) {
+			//Cannot find the IMultiDiagramEditor: try to open a new one
+			IFile diFile = getDiFile(pageURI);
+			editor = openURIsInNewEditor(diFile, new URI[]{ pageURI });
+		} else {
+			final IPageManager pageManager = registry.getService(IPageManager.class);
+			ModelSet modelSet = registry.getService(ModelSet.class);
+
+			final EObject page = modelSet.getEObject(pageURI, true);
+
+			if(pageManager.allPages().contains(page)) {
+				if(pageManager.isOpen(page)) {
+					pageManager.selectPage(page);
+				} else {
+					TransactionalEditingDomain editingDomain = registry.getService(TransactionalEditingDomain.class);
+					RecordingCommand command = new RecordingCommand(editingDomain, "Open page") {
+
+						@Override
+						protected void doExecute() {
+							pageManager.openPage(page);
+						}
+					};
+					editingDomain.getCommandStack().execute(command);
+				}
+			}
+		}
+
+		return editor;
 	}
 
-	public IMultiDiagramEditor openSemanticElement(EObject semanticElement, Object[] pages) throws PartInitException, ServiceException {
-		// TODO Auto-generated method stub
-		return null;
+	private IMultiDiagramEditor openURIsInNewEditor(IFile diFile, URI[] pageURIs) throws PartInitException {
+		final IEditorInput input = new PapyrusPageInput(diFile, pageURIs, false);
+
+		RunnableWithResult<IMultiDiagramEditor> runnable;
+		Display.getDefault().syncExec(runnable = new RunnableWithResult.Impl() {
+
+			public void run() {
+				try {
+					final IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+					IMultiDiagramEditor openedEditor = (IMultiDiagramEditor)IDE.openEditor(page, input, PAPYRUS_EDITOR_ID);
+					setResult(openedEditor);
+					setStatus(Status.OK_STATUS);
+				} catch (PartInitException ex) {
+					setStatus(new Status(IStatus.ERROR, Activator.PLUGIN_ID, ex.getMessage(), ex));
+				}
+			}
+		});
+
+		if(runnable.getStatus().getSeverity() == IStatus.ERROR) {
+			throw (PartInitException)runnable.getStatus().getException();
+		}
+
+		return runnable.getResult();
 	}
 
 }
