6d7fbe0b6607695099102ceedb38f4494dd620de
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-28 16:47:50 +0000

    404564: [Resource] Papyrus shall support optional ecore models https://bugs.eclipse.org/bugs/show_bug.cgi?id=404564
    Improve the LabelProvider for "AnyType" elements

---
+++ plugins/infra/emf/org.eclipse.papyrus.infra.emf/src/org/eclipse/papyrus/infra/emf/providers/AnyTypeLabelProvider.java
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Camille Letavernier (camille.letavernier@cea.fr) - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.infra.emf.providers;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.xml.type.AnyType;
+import org.eclipse.papyrus.infra.emf.utils.EMFHelper;
+import org.eclipse.papyrus.infra.services.labelprovider.service.IFilteredLabelProvider;
+
+/**
+ * A LabelProvider for unresolved "AnyType" elements
+ * 
+ * @author Camille Letavernier
+ * 
+ */
+public class AnyTypeLabelProvider extends EMFLabelProvider implements IFilteredLabelProvider {
+
+	@Override
+	public String getText(Object element) {
+		EObject eObject = EMFHelper.getEObject(element);
+
+		if(!(eObject instanceof AnyType)) {
+			return super.getText(element);
+		}
+
+		AnyType anyType = (AnyType)eObject;
+		EClass eClass = anyType.eClass();
+		if(eClass != null) {
+			return "Missing " + eClass.getName();
+		} else {
+			return "Missing component";
+		}
+	}
+
+	public boolean accept(Object element) {
+		return EMFHelper.getEObject(element) instanceof AnyType;
+	}
+
+}
