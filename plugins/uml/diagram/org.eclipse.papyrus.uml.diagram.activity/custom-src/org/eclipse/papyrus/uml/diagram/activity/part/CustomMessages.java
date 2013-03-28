3e0cca22e7cb0efab8936374592bbfb77186b9ff
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-03-28 13:12:17 +0000

    404119: [Activity Diagram] remove generatedNot https://bugs.eclipse.org/bugs/show_bug.cgi?id=404119

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.activity/custom-src/org/eclipse/papyrus/uml/diagram/activity/part/CustomMessages.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.activity/custom-src/org/eclipse/papyrus/uml/diagram/activity/part/CustomMessages.java
@@ -16,418 +16,176 @@ package org.eclipse.papyrus.uml.diagram.activity.part;
 import org.eclipse.osgi.util.NLS;
 
 /**
- * @generated
+ * custom messages
+ * 
+ * @author PT202707
+ * 
  */
 public class CustomMessages extends NLS {
 
-	/**
-	 * @generated
-	 */
 	static {
 		NLS.initializeMessages("custom-messages", CustomMessages.class); //$NON-NLS-1$
 	}
 
-	/**
-	 * @generated
-	 */
 	private CustomMessages() {
 	}
 
-	/**
-	 * @generated NOT
-	 */
 	public static String UMLValidation_ErrorTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String UMLValidation_WarningTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreatePinsForObjectFlowDialog_Title;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreatePinsForObjectFlowDialog_PinCreationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreatePinsForObjectFlowDialog_PinCreationHelp;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreatePinsForObjectFlowDialog_Name;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreatePinsForObjectFlowDialog_Type;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String PinAndParameterSynchronizer_UndeleteablePinTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String PinAndParameterSynchronizer_UndeleteablePinMessage;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String PinAndParameterSynchronizer_UnauthorizedModificationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String PinAndParameterSynchronizer_UnauthorizedModificationRedirection;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String PinAndParameterSynchronizer_UnauthorizedModification;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ActivityParameterAndParameterSynchronizer_UnauthorizedModificationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ActivityParameterAndParameterSynchronizer_UnauthorizedModificationRedirection;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ConfirmPinAndParameterSync_Title;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ConfirmPinAndParameterSync_FromParameterMsg;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ConfirmActivityParameterAndParameterSync_Title;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ConfirmActivityParameterAndParameterSync_FromParameterMsg;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_OperationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_SelectOperation;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_CreateOperation;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_BehaviorTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_SelectBehavior;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_CreateBehavior;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_SignalTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_SelectSignal;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_CreateSignal;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_ParentLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_NameLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_TypeLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_IsSynchronousTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_IsSynchronousHelp;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_IsSynchronous;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_OperationInvocationCreationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_OperationInvocationCreationHelp;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_OperationInvocationSelectionTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_BehaviorInvocationCreationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_BehaviorInvocationCreationHelp;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_BehaviorInvocationSelectionTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_SignalInvocationCreationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_SignalInvocationCreationHelp;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateCallActionDialog_SignalInvocationSelectionTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_DialogTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_ParameterSelectionTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_ParameterSelectionHelp;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_ParameterSelectionLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_ParameterCreationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_ParameterCreationHelp;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_ParameterCreationLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_NameLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_TypeLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateActivityParameterNodeDialog_DirectionLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_WarnNoImpact;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_WarnImpact;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_Open;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_DialogTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_ParameterCreationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_ParameterCreationHelp;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_NameLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_TypeLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateParameterDialog_DirectionLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateAttributeDialog_WarnNoImpact;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateAttributeDialog_WarnImpact;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateAttributeDialog_Open;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateAttributeDialog_DialogTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateAttributeDialog_CreationTitle;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateAttributeDialog_NameLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateAttributeDialog_EClassLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String CreateAttributeDialog_TypeLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String DiagramsPreferencePage_disableNotification;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String DiagramsPreferencePage_notificationGroup_label;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String DiagramsPreferencePage_confirmPinSyncFromParameter_label;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String DiagramsPreferencePage_confirmActivityParameterNodeSyncFromParameter_label;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ActivityEdgePreferencePage_visibilityGroup;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ActivityEdgePreferencePage_showName;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ForkJoinSegmentSwitchOrientation_actionLabel;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String ForkJoinSegmentSwitchOrientation_helpMessage;
 
-
-	/**
-	 * @generated NOT
-	 */
 	public static String SpecificKeywordStructuredActivityNodeVisibility_visibility;
 
-	/**
-	 * @generated NOT
-	 */
 	public static String StructuredActivityNodePreferencePage_showSpecificKeyword;
 	//TODO: put accessor fields manually	
 }
-
