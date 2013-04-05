abe2aa159f48200ffedbebb37a6cb41105f5c0b1
ptessier <ptessier@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-04-05 14:21:22 +0000

    403808: Validation ignores AppliedStereotypes https://bugs.eclipse.org/bugs/show_bug.cgi?id=403808

--- plugins/uml/org.eclipse.papyrus.uml.service.validation/src/org/eclipse/papyrus/uml/service/validation/Activator.java
+++ plugins/uml/org.eclipse.papyrus.uml.service.validation/src/org/eclipse/papyrus/uml/service/validation/Activator.java
@@ -1,3 +1,15 @@
+/*****************************************************************************
+ * Copyright (c) 2010, 2013 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * CoPatrick Tessier (CEA LIST) Patrick.Tessier@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
 package org.eclipse.papyrus.uml.service.validation;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
