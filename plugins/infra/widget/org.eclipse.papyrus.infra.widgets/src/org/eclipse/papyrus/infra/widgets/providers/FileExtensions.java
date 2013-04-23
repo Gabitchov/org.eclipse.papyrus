e5f6cd80150426a7752e2743f2be8f660eea09f6
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-04-23 11:16:58 +0000

    399869: [Widgets] Papyrus shall implement extension filters on the “import…” UI https://bugs.eclipse.org/bugs/show_bug.cgi?id=399869

--- plugins/infra/widget/org.eclipse.papyrus.infra.widgets/src/org/eclipse/papyrus/infra/widgets/providers/FileExtensions.java
+++ plugins/infra/widget/org.eclipse.papyrus.infra.widgets/src/org/eclipse/papyrus/infra/widgets/providers/FileExtensions.java
@@ -1,9 +1,27 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Camille Letavernier (camille.letavernier@cea.fr) - Initial API and implementation
+ *
+ *****************************************************************************/
 package org.eclipse.papyrus.infra.widgets.providers;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-
+/**
+ * Standard file extensions used in Papyrus
+ * 
+ * @author Camille Letavernier
+ * 
+ */
 public class FileExtensions {
 
 	public static final Map<String, String> umlExtensions = new LinkedHashMap<String, String>();
