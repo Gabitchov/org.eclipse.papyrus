c614dd90688d4a4e39ed588b1f87a9a13882a0a7
rschnekenbu <rschnekenbu@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-05-22 08:50:44 +0000

    408668: [Sysml Requirements Diagram] Drag & Drop between a Model Explorer and a Diagram https://bugs.eclipse.org/bugs/show_bug.cgi?id=408668
    - updated drag 'n' drop edit policy and view provider

--- plugins/sysml/diagram/org.eclipse.papyrus.sysml.diagram.requirement/src/org/eclipse/papyrus/sysml/diagram/requirement/edit/policy/CustomDragDropEditPolicy.java
+++ plugins/sysml/diagram/org.eclipse.papyrus.sysml.diagram.requirement/src/org/eclipse/papyrus/sysml/diagram/requirement/edit/policy/CustomDragDropEditPolicy.java
@@ -15,6 +15,18 @@ package org.eclipse.papyrus.sysml.diagram.requirement.edit.policy;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.papyrus.sysml.diagram.requirement.provider.ElementTypes;
+import org.eclipse.papyrus.sysml.util.SysmlResource;
+import org.eclipse.uml2.uml.Abstraction;
+import org.eclipse.uml2.uml.Comment;
+import org.eclipse.uml2.uml.Constraint;
+import org.eclipse.uml2.uml.Dependency;
+import org.eclipse.uml2.uml.PackageImport;
+import org.eclipse.uml2.uml.Realization;
+
 public class CustomDragDropEditPolicy extends RequirementDiagramDragDropEditPolicy {
 
 	/**
@@ -25,4 +37,78 @@ public class CustomDragDropEditPolicy extends RequirementDiagramDragDropEditPoli
 		Set<Integer> droppableElementsVisualID = new HashSet<Integer>();
 		return droppableElementsVisualID;
 	}
+	
+	@Override
+	public int getNodeVisualID(View containerView, EObject domainElement) {
+		String semanticHint = "-1";
+
+		// Fill the semantic hint during the drop when the top level is the Requirement Diagram
+		if(containerView instanceof Diagram) {
+			if(domainElement instanceof org.eclipse.uml2.uml.Package) {
+				semanticHint = ElementTypes.PACKAGE.getSemanticHint();
+			}
+			if((domainElement instanceof org.eclipse.uml2.uml.Class)) {
+				org.eclipse.uml2.uml.Class domainElementClass = (org.eclipse.uml2.uml.Class)domainElement;
+				if(domainElementClass.getAppliedStereotype(SysmlResource.REQUIREMENT_ID) != null) {
+					semanticHint = ElementTypes.CLASS.getSemanticHint();
+				}
+			}
+			
+			if(domainElement instanceof Comment) {
+				semanticHint = ElementTypes.COMMENT.getSemanticHint();
+			}
+			
+			if(domainElement instanceof Constraint) {
+				semanticHint = ElementTypes.CONSTRAINT.getSemanticHint();
+			}
+						
+		}
+
+		// Fill the semantic hint during the drop when the top level is the an element in the Requirement Diagram  (a Package for instance)
+		else {
+			if(domainElement instanceof org.eclipse.uml2.uml.Package) {
+				semanticHint = ElementTypes.PACKAGE_CN.getSemanticHint();
+			}
+			if((domainElement instanceof org.eclipse.uml2.uml.Class)) {
+				org.eclipse.uml2.uml.Class domainElementClass = (org.eclipse.uml2.uml.Class)domainElement;
+				if(domainElementClass.getAppliedStereotype(SysmlResource.REQUIREMENT_ID) != null) {
+					semanticHint = ElementTypes.CLASS_CN.getSemanticHint();
+				}
+			}
+		}
+
+		// In the case of we want to Drag and drop something else than a package or a Requirement, if this is a NamedElement, 
+		// we can fill the semanticHint with the NamedElement dedicated Value
+		if(semanticHint == "-1") {
+			if((domainElement instanceof org.eclipse.uml2.uml.NamedElement)) {
+				semanticHint = ElementTypes.DEFAULT_NAMED_ELEMENT.getSemanticHint();
+			}
+		}
+
+		return new Integer(semanticHint);
+	}
+	
+	@Override
+	public int getLinkWithClassVisualID(EObject domainElement) {
+		String semanticHint = "-1";
+
+		if(domainElement instanceof Abstraction) {
+			semanticHint = ElementTypes.ABSTRACTION.getSemanticHint();
+		}
+
+		if(domainElement instanceof Dependency) {
+			semanticHint = ElementTypes.DEPENDENCY.getSemanticHint();
+		}
+		
+		if(domainElement instanceof PackageImport) {
+			semanticHint = ElementTypes.PACKAGE_IMPORT.getSemanticHint();
+		}
+		if(domainElement instanceof Realization) {
+			semanticHint = ElementTypes.REALIZATION.getSemanticHint();
+		}
+		
+		return new Integer(semanticHint);
+
+	}
+	
 }
