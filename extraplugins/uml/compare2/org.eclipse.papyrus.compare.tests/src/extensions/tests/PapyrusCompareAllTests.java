134c0fe364c710dd2be263833ff46560b7a331cd
acuccuru <acuccuru@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-06-03 12:16:07 +0000

    409711: [Compare 2] Papyrus shall provide a PostProcessor for Compare 2 https://bugs.eclipse.org/bugs/show_bug.cgi?id=409711

---
+++ extraplugins/uml/compare2/org.eclipse.papyrus.compare.tests/src/extensions/tests/PapyrusCompareAllTests.java
@@ -0,0 +1,58 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package extensions.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>PapyrusCompare</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class PapyrusCompareAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new PapyrusCompareAllTests("PapyrusCompare Tests");
+		suite.addTest(ExtensionsTests.suite());
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PapyrusCompareAllTests(String name) {
+		super(name);
+	}
+
+} //PapyrusCompareAllTests
---
+++ extraplugins/uml/compare2/org.eclipse.papyrus.compare.tests/src/extensions/tests/PapyrusDiffTest.java
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package extensions.tests;
+
+import extensions.PapyrusDiff;
+import junit.framework.TestCase;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Papyrus Diff</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public abstract class PapyrusDiffTest extends TestCase {
+
+	/**
+	 * The fixture for this Papyrus Diff test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected PapyrusDiff fixture = null;
+
+	/**
+	 * Constructs a new Papyrus Diff test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PapyrusDiffTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Papyrus Diff test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(PapyrusDiff fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Papyrus Diff test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected PapyrusDiff getFixture() {
+		return fixture;
+	}
+
+} //PapyrusDiffTest
