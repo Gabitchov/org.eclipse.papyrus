705c3a33943c4e0b51c88cf7dfa714947a4be259
vlorenzo <vlorenzo@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-06-05 13:45:03 +0000

    409933: [General] [Diagram] Rewrite GMF Actions from Edit menu in the Diagram https://bugs.eclipse.org/bugs/show_bug.cgi?id=409933
    Cut/Copy/Paste actions rewritten for Papyrus

---
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.menu/src/org/eclipse/papyrus/infra/gmfdiag/menu/handlers/AbstractDiagramActionHandler.java
@@ -0,0 +1,111 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.infra.gmfdiag.menu.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.gmf.runtime.common.ui.action.ActionManager;
+import org.eclipse.gmf.runtime.common.ui.action.IActionWithProgress;
+import org.eclipse.gmf.runtime.diagram.ui.parts.IDiagramWorkbenchPart;
+import org.eclipse.papyrus.infra.tools.util.EditorHelper;
+import org.eclipse.ui.IEditorPart;
+
+/**
+ * 
+ * @author vl222926
+ * 
+ */
+public abstract class AbstractDiagramActionHandler extends AbstractHandler {
+
+	/**
+	 * 
+	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 * 
+	 * @param event
+	 * @return
+	 * @throws ExecutionException
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final ActionManager manager = getActionManager();
+		if(manager != null) {
+			final IActionWithProgress globalActionHandler = getGlobalActionHandler();
+			if(globalActionHandler != null) {
+				globalActionHandler.refresh();
+				manager.run(globalActionHandler);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @return
+	 *         the action to execute
+	 */
+	private IActionWithProgress getGlobalActionHandler() {
+		final IDiagramWorkbenchPart workbenchPart = getDiagramWorkbenchPart();
+		if(workbenchPart instanceof IEditorPart) {
+			final IEditorPart editor = (IEditorPart)workbenchPart;
+			return (IActionWithProgress)editor.getEditorSite().getActionBars().getGlobalActionHandler(getActionId());
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @return
+	 *         the action manager to use
+	 */
+	private ActionManager getActionManager() {
+		IDiagramWorkbenchPart part = getDiagramWorkbenchPart();
+		if(part != null) {
+			return (ActionManager)part.getAdapter(ActionManager.class);
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @return
+	 *         the current diagram workbench part
+	 */
+	private IDiagramWorkbenchPart getDiagramWorkbenchPart() {
+		final IEditorPart workbench = EditorHelper.getCurrentEditor();
+		if(workbench != null) {
+			return (IDiagramWorkbenchPart)workbench.getAdapter(IDiagramWorkbenchPart.class);
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @return
+	 *         the id of the action to execute
+	 */
+	protected abstract String getActionId();
+
+	/**
+	 * 
+	 * @see org.eclipse.core.commands.AbstractHandler#setEnabled(java.lang.Object)
+	 * 
+	 * @param evaluationContext
+	 */
+	@Override
+	public void setEnabled(Object evaluationContext) {
+		final IActionWithProgress action = getGlobalActionHandler();
+		action.refresh();
+		setBaseEnabled(action.isRunnable());
+	}
+}
---
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.menu/src/org/eclipse/papyrus/infra/gmfdiag/menu/handlers/PasteInDiagramHandler.java
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.infra.gmfdiag.menu.handlers;
+
+import org.eclipse.gmf.runtime.common.ui.action.global.GlobalActionId;
+
+/**
+ * This Paste handler retarget the Paste action to the GMF action, but now this action is declared using the Eclipse Framework
+ * 
+ * @author vl222926
+ * 
+ */
+public class PasteInDiagramHandler extends AbstractDiagramActionHandler {
+
+	/**
+	 * 
+	 * @see org.eclipse.papyrus.infra.gmfdiag.menu.handlers.AbstractDiagramActionHandler#getActionId()
+	 * 
+	 * @return
+	 */
+	@Override
+	protected String getActionId() {
+		return GlobalActionId.PASTE;
+	}
+
+}
