d0abd904c97a0caa9d917db35a8022637ff4c532
cdumoulin <cdumoulin@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-06-10 12:43:33 +0000

    404142: [sasheditor] Rename diagram command from sash tab https://bugs.eclipse.org/bugs/show_bug.cgi?id=404142
      Add missing classes for tests

---
+++ tests/junit/plugins/core/org.eclipse.papyrus.sasheditor.tests/test/org/eclipse/papyrus/infra/core/sasheditor/pagesmodel/IModelExp.java
@@ -0,0 +1,28 @@
+/*****************************************************************************
+ * Copyright (c) 2013 Cedric Dumoulin.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Cedric Dumoulin  Cedric.dumoulin@lifl.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+
+package org.eclipse.papyrus.infra.core.sasheditor.pagesmodel;
+
+/**
+ * A Model Expression represent an expression used to describe a PagesModel.
+ * Term implementing this interface can be used as starting point of expression.
+ * <br>
+ * Following classses are expressions: {@link PanelTerm}, {@link WindowTerm}, {@link PagesModel}.
+ * 
+ * @author cedric dumoulin
+ * TODO Rename to IModelExp
+ */
+public interface IModelExp extends IModelObject {
+
+}
