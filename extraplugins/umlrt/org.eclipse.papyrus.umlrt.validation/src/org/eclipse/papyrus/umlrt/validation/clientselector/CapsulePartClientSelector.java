8017a0b63415856a18c1ee7c090d3b1194b86d7c
cletavernie <cletavernie@d68fad7f-2b57-0410-97a2-f40a37629c47>
2013-06-25 11:19:03 +0000

    411009: [UML RT] Provide tooling support for the UML-RT Profile https://bugs.eclipse.org/bugs/show_bug.cgi?id=411009

---
+++ extraplugins/umlrt/org.eclipse.papyrus.umlrt.validation/src/org/eclipse/papyrus/umlrt/validation/clientselector/CapsulePartClientSelector.java
@@ -0,0 +1,48 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Camille Letavernier (camille.letavernier@cea.fr) - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.umlrt.validation.clientselector;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.model.IClientSelector;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Stereotype;
+import org.eclipse.uml2.uml.util.UMLUtil;
+
+
+public class CapsulePartClientSelector implements IClientSelector {
+
+	public boolean selects(Object object) {
+		if(!(object instanceof EObject)) {
+			return false;
+		}
+
+		if(object instanceof Element) {
+			return false;
+		}
+
+		EObject stereotypeApplication = ((EObject)object);
+		Stereotype stereotype = UMLUtil.getStereotype(stereotypeApplication);
+		if(stereotype == null) {
+			return false;
+		}
+
+		if("CapsulePart".equals(stereotype.getName())) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
+
