4181e7c5d2222cee821e00f45970c4570a94bb4b
Arnaud Cuccuru <arnaud.cuccuru@cea.fr>
2013-09-17 10:50:30 +0200

    Revert "Revert "Bug 315231 - Improvement of xtext integration.""
    This reverts commit a73b1583a1b3c8962c6e5cdfb1620a07a7ac66f0.

--- plugins/uml/xtext/org.eclipse.papyrus.uml.xtext.integration.ui/src/org/eclipse/papyrus/uml/xtext/integration/InvalidSyntaxAdapter.java
+++ plugins/uml/xtext/org.eclipse.papyrus.uml.xtext.integration.ui/src/org/eclipse/papyrus/uml/xtext/integration/InvalidSyntaxAdapter.java
@@ -1,14 +1,13 @@
 package org.eclipse.papyrus.uml.xtext.integration;
 
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EAnnotation;
-import org.eclipse.emf.ecore.EModelElement;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.util.EContentAdapter;
+import org.eclipse.uml2.uml.Comment;
+import org.eclipse.uml2.uml.Element;
 
 /**
  * {@link EContentAdapter} that listens for modifications outside the xtext cell
- * editor to remove the annotation that stores the invalid string. 
+ * editor to remove the text comment that stores the invalid string.
  * 
  * @author muelder
  * 
@@ -17,16 +16,13 @@ public class InvalidSyntaxAdapter extends EContentAdapter {
 	@Override
 	public void notifyChanged(Notification notification) {
 		super.notifyChanged(notification);
-		if (notification.getFeature() != EcorePackage.Literals.EMODEL_ELEMENT__EANNOTATIONS) {
-			Object notifier = notification.getNotifier();
-			if (notifier instanceof EModelElement) {
-				EModelElement eModelElement = (EModelElement) notifier;
-				EAnnotation eAnnotation = eModelElement
-						.getEAnnotation(DefaultXtextDirectEditorConfiguration.ANNOTATION_SOURCE);
-				if (eAnnotation != null) {
-					eModelElement.getEAnnotations().remove(eAnnotation);
-				}
-			}
+		if (notification.getNotifier() instanceof Element) {
+			Element element = (Element) notification.getNotifier();
+			Comment comment = InvalidStringUtil
+					.getTextualRepresentationComment(element);
+			if (comment != null)
+				element.getOwnedComments().remove(comment);
+
 		}
 	}
 }
