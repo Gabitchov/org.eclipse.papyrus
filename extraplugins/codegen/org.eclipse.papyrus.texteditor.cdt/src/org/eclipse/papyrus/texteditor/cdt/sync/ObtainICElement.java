a9c34177780cc04c143d552804955d90f5f27564
Ansgar Radermacher <ansgar.radermacher@cea.fr>
2013-11-04 11:07:51 +0100

    - Fix text selection for classes nested in namespaces. Bug 399864 - [CDT/StateMachine] Papyrus shall enable to see code on transitions

--- extraplugins/codegen/org.eclipse.papyrus.texteditor.cdt/src/org/eclipse/papyrus/texteditor/cdt/sync/ObtainICElement.java
+++ extraplugins/codegen/org.eclipse.papyrus.texteditor.cdt/src/org/eclipse/papyrus/texteditor/cdt/sync/ObtainICElement.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Ansgar Radermacher - ansgar.radermacher@cea.fr CEA LIST - initial API and implementation
+ *
+ *******************************************************************************/
+
 package org.eclipse.papyrus.texteditor.cdt.sync;
 
 import org.eclipse.cdt.core.model.ICElement;
@@ -5,6 +17,7 @@ import org.eclipse.cdt.core.model.IFunctionDeclaration;
 import org.eclipse.cdt.core.model.IParent;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.Namespace;
 import org.eclipse.uml2.uml.Operation;
 
 public class ObtainICElement {
@@ -18,13 +31,13 @@ public class ObtainICElement {
 		try {	
 			for(ICElement child : parent.getChildren()) {
 				if(child instanceof IParent) {
-					getICElement((IParent)child, element);
+					return getICElement((IParent)child, element);
 				}
 				if(child instanceof IFunctionDeclaration) {
 					// IFunctionDeclaration function = (IFunctionDeclaration) child;
 					
 					if (element instanceof Operation) {						
-						if (child.getElementName().endsWith("::" + element.getName())) {
+						if (child.getElementName().endsWith(Namespace.SEPARATOR + element.getName())) {
 							return child;
 						}
 					}
