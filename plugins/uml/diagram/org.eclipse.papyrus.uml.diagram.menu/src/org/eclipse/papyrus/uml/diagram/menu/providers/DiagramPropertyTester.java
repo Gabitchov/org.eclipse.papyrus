838101fc1b61dded15663a3ea7848aa0063cbf14
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2013-11-07 13:03:38 +0100

    421212: [Diagram] Papyrus should provide actions for Show/Hide related links in all diagrams https://bugs.eclipse.org/bugs/show_bug.cgi?id=421212
    Commit some required classes which have been forgotten

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.menu/src/org/eclipse/papyrus/uml/diagram/menu/providers/DiagramPropertyTester.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.menu/src/org/eclipse/papyrus/uml/diagram/menu/providers/DiagramPropertyTester.java
@@ -14,6 +14,8 @@ package org.eclipse.papyrus.uml.diagram.menu.providers;
 import java.util.Iterator;
 
 import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
 import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.jface.viewers.IStructuredSelection;
 
@@ -24,6 +26,11 @@ public class DiagramPropertyTester extends PropertyTester {
 	public static final String IS_DIAGRAM = "isDiagram"; //$NON-NLS-1$
 
 	/**
+	 * property to test if a diagram has the required edit policy
+	 */
+	public static final String DIAGRAM_HAS_REQUIRED_EDIT_POLICY = "hasRequiredEditPolicy";  //$NON-NLS-1$
+
+	/**
 	 * 
 	 * {@inheritDoc}
 	 */
@@ -31,8 +38,42 @@ public class DiagramPropertyTester extends PropertyTester {
 		if(IS_DIAGRAM.equals(property) && receiver instanceof IStructuredSelection) {
 			boolean answer = isDiagram((IStructuredSelection)receiver);
 			return new Boolean(answer).equals(expectedValue);
+		} else if(DIAGRAM_HAS_REQUIRED_EDIT_POLICY.equals(property) && receiver instanceof IStructuredSelection && args.length == 1 && args[0] instanceof String) {
+			boolean answer = hasRequiredEditPolicy((IStructuredSelection)receiver, (String)args[0]);
+			return new Boolean(answer).equals(expectedValue);
 		}
 		return false;
+	}
+
+	/**
+	 * 
+	 * @param selection
+	 *        the selection
+	 * @param wantedEditPolicy
+	 *        the wanted edit policy
+	 * @return
+	 *         <code>true</code> if the diagram edit part has the wanted selection
+	 */
+	protected boolean hasRequiredEditPolicy(final IStructuredSelection selection, final String wantedEditPolicy) {
+		boolean answer = false;
+		if(selection.size() != 0) {
+			final Object first = selection.getFirstElement();
+			if(first instanceof EditPart) {
+				EditPart parent = (EditPart)first;
+				DiagramEditPart diagramEP = null;
+				while(parent != null && diagramEP == null) {
+					if(parent instanceof DiagramEditPart) {
+						diagramEP = (DiagramEditPart)parent;
+					} else {
+						parent = parent.getParent();
+					}
+				}
+				if(diagramEP != null) {
+					answer = diagramEP.getEditPolicy(wantedEditPolicy) != null;
+				}
+			}
+		}
+		return answer;
 	}
 
 	/**
