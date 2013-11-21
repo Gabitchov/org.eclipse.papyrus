f907a79f9a0e882e56c841c9af3944d8816d739f
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2013-11-21 16:46:28 +0100

    418018: [General] Add plugin to releng for Luna AND Kepler SR2 : oep.uml.nattable.xtext.integration https://bugs.eclipse.org/bugs/show_bug.cgi?id=418018
    Share plugin org.eclipse.uml.xtext.integration.core and
    org.eclipse.uml.xtext.integration.ui on maintenance-0.10

---
+++ plugins/uml/xtext/org.eclipse.papyrus.uml.xtext.integration.ui/src/org/eclipse/papyrus/uml/xtext/integration/ICompletionProposalListener.java
@@ -0,0 +1,29 @@
+package org.eclipse.papyrus.uml.xtext.integration;
+
+
+/**
+ * This interface is used to listen to additional notifications from a
+ * {@link CompletionProposalAdapter}.
+ * 
+ * @author patrick.koenemann@itemis.de
+ *
+ */
+public interface ICompletionProposalListener {
+	/**
+	 * A completion proposal popup has been opened.
+	 * 
+	 * @param adapter
+	 *            the CompletionProposalAdapter which is adapting content proposal
+	 *            behavior to a control
+	 */
+	public void proposalPopupOpened(CompletionProposalAdapter adapter);
+
+	/**
+	 * A completion proposal popup has been closed.
+	 * 
+	 * @param adapter
+	 *            the CompletionProposalAdapter which is adapting content proposal
+	 *            behavior to a control
+	 */
+	public void proposalPopupClosed(CompletionProposalAdapter adapter);
+}
