ac7745b1afa53790904ff31fb7487e46d9c2b841
Ansgar Radermacher <ansgar.radermacher@cea.fr>
2013-12-19 13:37:16 +0100

    Bug 424442 - [QDesigner] Container transformation should separate rule application and instance creation

--- extraplugins/qompass-designer/org.eclipse.papyrus.qompass.designer.core/src/org/eclipse/papyrus/qompass/designer/core/transformations/AbstractContainerTrafo.java
+++ extraplugins/qompass-designer/org.eclipse.papyrus.qompass.designer.core/src/org/eclipse/papyrus/qompass/designer/core/transformations/AbstractContainerTrafo.java
@@ -46,16 +46,6 @@ public abstract class AbstractContainerTrafo {
 	 */
 	protected static Map<Class, AbstractContainerTrafo> containers;
 
-	/**
-	 * Only configure the instances, i.e. avoid re-creating elements of the container, but create
-	 * the UML instance specifications
-	 */
-	public void configureOnly() {
-		configureOnly = true;
-	}
-
-	protected boolean configureOnly;
-
 	public abstract void createContainer(Class smComponent, Class tmComponent) throws TransformationException;
 
 	/**
@@ -69,7 +59,7 @@ public abstract class AbstractContainerTrafo {
 	 * @param context
 	 *        Additional information about the container that is used by instance configurators
 	 */
-	public abstract void createContainerInstance(Class tmComponent, InstanceSpecification tmIS, ContainerContext context)
+	public abstract InstanceSpecification createContainerInstance(Class tmComponent, InstanceSpecification tmIS, ContainerContext context)
 		throws TransformationException;
 
 	/**
@@ -88,34 +78,24 @@ public abstract class AbstractContainerTrafo {
 		throws TransformationException;
 
 	/**
-	 * Move a slot from the executor instance specification to an instance
-	 * specification of an extension/interceptor This is required, since the
-	 * users cannot configure containers directly. The modification is done in
-	 * the target model, i.e. the user model is not affected.
-	 * 
-	 * Default implementation is empty
-	 */
-	public void moveSlots() {
-	}
-
-	/**
 	 * The instance specification of the created container
 	 */
-	protected InstanceSpecification containerIS;
+	// protected InstanceSpecification containerIS;
 
 	/**
-	 * Store source and target model
+	 * Copy class from source to target model
 	 */
 	protected Copy copy;
 
 
 	/**
-	 * @return the instance specification of the created container
+	 * Is called after a set of rule applications.
+	 * Does nothing by default
 	 */
-	public InstanceSpecification getContainerIS() {
-		return containerIS;
+	public void finalize() {
 	}
 
+	
 	/**
 	 * The created container implementation (prefixed with sm, since part of
 	 * source model)
@@ -129,12 +109,12 @@ public abstract class AbstractContainerTrafo {
 	protected Class tmClass;
 
 	/**
+	 * deployment plan within source model
+	 */
+	protected Package smCDP;
+	
+	/**
 	 * deployment plan within target model
 	 */
 	protected Package tmCDP;
-
-	/**
-	 * Additional information about the container that is used by instance configurators
-	 */
-	protected ContainerContext context;
 }
