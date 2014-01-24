4d04f02d1fb7d7c9a0efe34c52023c071315f36d
Benoit Maggi <benoit.maggi@cea.fr>
2014-01-24 14:50:50 +0100

    Creation of org.eclipse.papyrus.uml.diagram.composite.tests to test uml diagram composite
    Change-Id: I6b011409de22dd8d8ffbf01de81649ee9b03f48f
    Signed-off-by: Benoit Maggi <benoit.maggi@cea.fr>

---
+++ tests/junit/plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.composite.tests/test/org/eclipse/papyrus/uml/diagram/composite/test/AllTests.java
@@ -0,0 +1,32 @@
+/*****************************************************************************
+ * Copyright (c) 2014 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Benoit Maggi (CEA LIST) benoit.maggi@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.uml.diagram.composite.test;
+
+import org.eclipse.papyrus.uml.diagram.composite.test.canonical.AllCanonicalTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * All tests together.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+// canonical
+AllCanonicalTests.class,
+// load
+//LoadTests.class
+})
+public class AllTests {
+}
