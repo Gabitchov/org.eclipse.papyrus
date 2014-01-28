4889444a92221b1a6584b5d748ff3aa9dc8cb9d5
Ansgar Radermacher <ansgar.radermacher@cea.fr>
2014-01-28 15:27:09 +0100

    425182 - [Use Case Diagram] Actor invisible in use case diagram

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.usecase/custom-src/org/eclipse/papyrus/uml/diagram/usecase/draw2d/StickMan.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.usecase/custom-src/org/eclipse/papyrus/uml/diagram/usecase/draw2d/StickMan.java
@@ -60,9 +60,15 @@ public class StickMan extends ShadowShape implements IPapyrusNodeNamedElementFig
 	@Override
 	protected void outlineShape(Graphics graphics, Rectangle bounds) {
 		PointList pl = setupPoints(bounds);
+		int oldLW = graphics.getLineWidth();
+		if (graphics.getLineWidth() < 1) {
+			// fix for bug 425182 - [Use Case Diagram] Actor invisible in use case diagram
+			graphics.setLineWidth(1);
+		}
 		graphics.drawPolygon(pl);
 		int add = graphics.getLineWidth() / 2;
 		graphics.drawOval(new Rectangle(ovalX, ovalY, ovalD + add, ovalD + add));
+		graphics.setLineWidth(oldLW);
 	}
 
 	/**
@@ -163,98 +169,76 @@ public class StickMan extends ShadowShape implements IPapyrusNodeNamedElementFig
 
 	@Override
 	public void setStereotypePropertiesInBrace(String stereotypeProperties) {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void setStereotypePropertiesInCompartment(String stereotypeProperties) {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public Label getStereotypesLabel() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void setStereotypeDisplay(String stereotypes, Image image) {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void setQualifiedName(String qualifiedName) {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public Label getQualifiedNameLabel() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Label getTaggedLabel() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void setDepth(int depth) {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public WrappingLabel getNameLabel() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void setNameLabelIcon(boolean displayNameLabelIcon) {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public Color getBorderColor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean isShadow() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void setBorderColor(Color borderColor) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void setShadow(boolean shadow) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void setTransparency(int transparency) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void setGradientData(int gradientColor1, int gradientColor2, int gradientStyle) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void setIsUsingGradient(boolean b) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -269,21 +253,17 @@ public class StickMan extends ShadowShape implements IPapyrusNodeNamedElementFig
 
 	@Override
 	public void removeStereotypeLabel() {
-		
 	}
 
 	@Override
 	public void restoreStereotypeLabel() {
-		
 	}
 
 	@Override
 	public void restoreTaggedLabel() {
-		
 	}
 
 	@Override
 	public void removeTaggedLabel() {
-		
 	}
 }
