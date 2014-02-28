9b11d4e998698a1f5d458fbd3edcc5eeb3af78fe
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2014-02-28 16:47:12 +0100

    424942: [Diagram] Papyrus shall ease resizing of model elements owning children https://bugs.eclipse.org/bugs/show_bug.cgi?id=424942

--- plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/editpolicies/PapyrusResizableShapeEditPolicy.java
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/editpolicies/PapyrusResizableShapeEditPolicy.java
@@ -13,27 +13,34 @@
  *****************************************************************************/
 package org.eclipse.papyrus.infra.gmfdiag.common.editpolicies;
 
+import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.requests.ChangeBoundsRequest;
+import org.eclipse.gef.tools.ResizeTracker;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ResizableShapeEditPolicy;
+import org.eclipse.papyrus.infra.gmfdiag.common.snap.ResizeTrackerWithPreferences;
 
 /**
- *
+ * 
  * See Bug 424943 ResizableEditPolicy#getResizeCommand duplicates request ignoring some request values
- * TODO : remove this class when the bug will be fixed
+ * 
  */
 public class PapyrusResizableShapeEditPolicy extends ResizableShapeEditPolicy {
 
 	/**
+	 * See Bug 424943 ResizableEditPolicy#getResizeCommand duplicates request ignoring some request values
+	 * TODO : remove this override when the bug will be fixed
+	 * 
 	 * Returns the command contribution for the given resize request. By
 	 * default, the request is re-dispatched to the host's parent as a {@link org.eclipse.gef.RequestConstants#REQ_RESIZE_CHILDREN}. The
 	 * parent's edit policies determine how to perform the resize based on the
 	 * layout manager in use.
-	 *
+	 * 
 	 * @param request
 	 *        the resize request
 	 * @return the command contribution obtained from the parent
 	 */
+
 	@Override
 	protected Command getResizeCommand(ChangeBoundsRequest request) {
 		ChangeBoundsRequest req = new ChangeBoundsRequest(REQ_RESIZE_CHILDREN);
@@ -54,6 +61,18 @@ public class PapyrusResizableShapeEditPolicy extends ResizableShapeEditPolicy {
 		}
 
 		return getHost().getParent().getCommand(req);
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.gef.editpolicies.ResizableEditPolicy#getResizeTracker(int)
+	 * 
+	 * @param direction
+	 * @return
+	 */
+	@Override
+	protected ResizeTracker getResizeTracker(int direction) {
+		return new ResizeTrackerWithPreferences((GraphicalEditPart)getHost(), direction);
 	}
 
 }
