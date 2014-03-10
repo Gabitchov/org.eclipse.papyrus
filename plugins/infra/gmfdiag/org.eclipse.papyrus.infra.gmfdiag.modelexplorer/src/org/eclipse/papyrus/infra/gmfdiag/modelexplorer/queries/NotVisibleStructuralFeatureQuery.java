908ac148c84a3efdf11147865c12d4773d8a0795
ptessier <patrick.tessier@cea.fr>
2014-03-10 18:52:05 +0100

    Bug 386118 - [EMF Facet] Papyrus should progressively support EMF Facet 0.2 fixe query to display diagram when who diagram is activated

--- plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.modelexplorer/src/org/eclipse/papyrus/infra/gmfdiag/modelexplorer/queries/NotVisibleStructuralFeatureQuery.java
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.modelexplorer/src/org/eclipse/papyrus/infra/gmfdiag/modelexplorer/queries/NotVisibleStructuralFeatureQuery.java
@@ -12,9 +12,11 @@
  /*****************************************************************************/
 package org.eclipse.papyrus.infra.gmfdiag.modelexplorer.queries;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.papyrus.emf.facet.efacet.core.IFacetManager;
 import org.eclipse.papyrus.emf.facet.efacet.core.exception.DerivedTypedElementException;
+import org.eclipse.papyrus.emf.facet.efacet.metamodel.v0_2_0.efacet.ParameterValue;
 import org.eclipse.papyrus.emf.facet.query.java.core.IJavaQuery2;
 import org.eclipse.papyrus.emf.facet.query.java.core.IParameterValueList2;
 
@@ -23,6 +25,14 @@ public class NotVisibleStructuralFeatureQuery implements IJavaQuery2<Diagram, Bo
 			final IParameterValueList2 parameterValues,
 			final IFacetManager facetManager)
 			throws DerivedTypedElementException {
+		ParameterValue parameterValue= (ParameterValue)parameterValues.getParameterValueByName("eStructuralFeature");
+		EStructuralFeature eStructuralFeature=(EStructuralFeature)parameterValue.getValue();
+		//if eStructural feature ==null this is root model explorer.
+		//border effect of  this kind of queries
+		if (eStructuralFeature==null){
+			return true;
+		}
+		
 		return false;
 	}
 }
