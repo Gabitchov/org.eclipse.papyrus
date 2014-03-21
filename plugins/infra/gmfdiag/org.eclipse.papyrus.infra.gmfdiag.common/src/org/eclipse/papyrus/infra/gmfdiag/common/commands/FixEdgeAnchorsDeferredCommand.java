ec6b3b357072ebf68ee673f08de560239c74e3d8
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2014-03-21 13:58:34 +0100

    430702: [Diagram] Moving source of a link moves the target too. https://bugs.eclipse.org/bugs/show_bug.cgi?id=430702  First commit for this bug. It fix anchors in sysml IBD and BDD

---
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/commands/FixEdgeAnchorsDeferredCommand.java
@@ -0,0 +1,141 @@
+/*****************************************************************************
+ * Copyright (c) 2014 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		
+ *		CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+
+
+package org.eclipse.papyrus.infra.gmfdiag.common.commands;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.transaction.RunnableWithResult;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gef.commands.CompoundCommand;
+import org.eclipse.gef.editparts.AbstractConnectionEditPart;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.util.EditPartUtil;
+import org.eclipse.papyrus.infra.gmfdiag.common.Activator;
+
+
+/**
+ * 
+ * see bug 430702: [Diagram] Moving source of a link moves the target too.
+ * 
+ * This class allows to fix the anchors for a collection of connection edit part
+ * 
+ */
+public class FixEdgeAnchorsDeferredCommand extends AbstractFixEdgeAnchorDeferredCommand {
+
+	/**
+	 * the list of the connections to refresh
+	 */
+	private Collection<?> connectionsEditPartToRefresh;
+
+	/**
+	 * 
+	 * Constructor.
+	 * 
+	 * @param editingDomain
+	 *        the editing domain
+	 * @param request
+	 *        the creation request
+	 * @param containerEP
+	 *        the diagram edit part
+	 */
+	public FixEdgeAnchorsDeferredCommand(final TransactionalEditingDomain editingDomain, final IGraphicalEditPart containerEP, final Collection<?> connectionsEditPartToRefresh) {
+		super(editingDomain, "Fix Edge Anchors", containerEP); //$NON-NLS-1$
+		this.connectionsEditPartToRefresh = new ArrayList<Object>(connectionsEditPartToRefresh);
+	}
+
+
+	/**
+	 * Executes a fix anchor command for the created edge
+	 * 
+	 */
+	protected CommandResult doExecuteWithResult(IProgressMonitor progressMonitor, IAdaptable info) throws ExecutionException {
+
+		final RunnableWithResult<Collection<AbstractConnectionEditPart>> refreshRunnable = new RunnableWithResult<Collection<AbstractConnectionEditPart>>() {
+
+			private IStatus status;
+
+			private Collection<AbstractConnectionEditPart> result;
+
+			public Collection<AbstractConnectionEditPart> getResult() {
+				return result;
+			}
+
+			public void setStatus(IStatus status) {
+				this.status = status;
+			}
+
+			public IStatus getStatus() {
+				return status;
+			}
+
+			public void run() {
+				getContainerEP().refresh();
+
+				// We update the figure world 
+				getContainerFigure().invalidate();
+				getContainerFigure().validate();
+				final Iterator<?> iter = connectionsEditPartToRefresh.iterator();
+				final Collection<AbstractConnectionEditPart> connectionsEP = new HashSet<AbstractConnectionEditPart>();
+				this.result = connectionsEP;
+				while(iter.hasNext()) {
+					final Object object = iter.next();
+					if(object instanceof AbstractConnectionEditPart) {
+						connectionsEP.add((AbstractConnectionEditPart)object);
+						refreshConnection((AbstractConnectionEditPart)object);
+					}
+				}
+				setStatus(Status.OK_STATUS);
+			}
+		};
+
+		EditPartUtil.synchronizeRunnableToMainThread(getContainerEP(), refreshRunnable);
+		final Collection<AbstractConnectionEditPart> toRefresh = refreshRunnable.getResult();
+		final Iterator<AbstractConnectionEditPart> iter = toRefresh.iterator();
+		while(iter.hasNext()) {
+			final CompoundCommand cc = new CompoundCommand("Fix connections anchors"); //$NON-NLS-1$
+			final AbstractConnectionEditPart current = iter.next();
+			addFixAnchorCommand(current, cc);
+			if(cc.canExecute()) {
+				cc.execute();
+			}else{
+				Activator.log.warn("Command to fix the anchors is null"); //$NON-NLS-1$
+			}
+		}
+		return CommandResult.newOKCommandResult();
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand#cleanup()
+	 * 
+	 */
+	protected void cleanup() {
+		super.cleanup();
+		this.connectionsEditPartToRefresh.clear();
+	}
+
+
+
+}
