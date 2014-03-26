1b9fc96e2e48c2991b2f1ac82f2e19aa2615c44e
Camille Letavernier <camille.letavernier@cea.fr>
2014-03-26 19:22:08 +0100

    431233: [All Diagrams] duplicate diagram no more available - no workaround https://bugs.eclipse.org/bugs/show_bug.cgi?id=431233

--- plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.modelexplorer/src/org/eclipse/papyrus/infra/gmfdiag/modelexplorer/handlers/AbstractDiagramCommandHandler.java
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.modelexplorer/src/org/eclipse/papyrus/infra/gmfdiag/modelexplorer/handlers/AbstractDiagramCommandHandler.java
@@ -20,6 +20,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.papyrus.infra.emf.utils.EMFHelper;
 import org.eclipse.papyrus.views.modelexplorer.NavigatorUtils;
 import org.eclipse.papyrus.views.modelexplorer.handler.AbstractCommandHandler;
 import org.eclipse.ui.PlatformUI;
@@ -47,10 +48,10 @@ public abstract class AbstractDiagramCommandHandler extends AbstractCommandHandl
 				Object current = iter.next();
 				/**
 				 * Get the diagram object.
-				 * This getElement is used in order to use IAdaptabel mechanisme
+				 * This getElement is used in order to use IAdaptabel mechanism
 				 * For example for Facet Elements
 				 */
-				EObject diag = NavigatorUtils.getElement(current, EObject.class);
+				EObject diag = EMFHelper.getEObject(current);
 				if(diag instanceof Diagram) {
 					diagrams.add((Diagram)diag);
 				}
