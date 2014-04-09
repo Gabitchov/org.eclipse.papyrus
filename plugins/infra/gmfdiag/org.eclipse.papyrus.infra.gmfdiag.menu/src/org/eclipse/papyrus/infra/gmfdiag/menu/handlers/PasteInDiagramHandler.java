6bcbe2ba1ef2b71c983b46ef9103aaedbdbb73cc
Benoit Maggi <benoit.maggi@cea.fr>
2014-04-09 10:57:58 +0200

    [Usability] Papyrus shall provide a copy/paste/cut feature Add property tester to remove text copy from diagram copy/paster handler Signed-off-by: Benoit Maggi <benoit.maggi@cea.fr> Change-Id: I665470e8e9e2b0c87f37d1e84a677519e413ce16

--- plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.menu/src/org/eclipse/papyrus/infra/gmfdiag/menu/handlers/PasteInDiagramHandler.java
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.menu/src/org/eclipse/papyrus/infra/gmfdiag/menu/handlers/PasteInDiagramHandler.java
@@ -15,6 +15,7 @@ package org.eclipse.papyrus.infra.gmfdiag.menu.handlers;
 
 import java.util.List;
 
+import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.UnexecutableCommand;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
@@ -23,10 +24,15 @@ import org.eclipse.papyrus.infra.core.clipboard.PapyrusClipboard;
 import org.eclipse.papyrus.infra.gmfdiag.common.strategy.IStrategy;
 import org.eclipse.papyrus.infra.gmfdiag.common.strategy.paste.IPasteStrategy;
 import org.eclipse.papyrus.infra.gmfdiag.common.strategy.paste.PasteStrategyManager;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 
 /**
- * The handler for the paste with model in Diagram 
+ * The handler for the paste with model in Diagram
  * 
  */
 public class PasteInDiagramHandler extends AbstractGraphicalCommandHandler {
@@ -39,30 +45,54 @@ public class PasteInDiagramHandler extends AbstractGraphicalCommandHandler {
 	 */
 	@Override
 	protected Command getCommand() {
-		
 		PapyrusClipboard.getInstance().resetTarget();
-		
 		List<IGraphicalEditPart> selectedElements = getSelectedElements();
-		if (selectedElements.size() == 1){ // Paste is only enabled on a simple selection
-			//if (PapyrusClipboard.getInstance().size() > 0) { // TODO : Paste is only enabled if there is something inside the clipboard
-			GraphicalEditPart targetOwnerEditPart = (GraphicalEditPart) selectedElements.get(0);
+		if(selectedElements.size() == 1) { // Paste is only enabled on a simple selection
+			GraphicalEditPart targetOwnerEditPart = (GraphicalEditPart)selectedElements.get(0);
 			org.eclipse.gef.commands.CompoundCommand compoundCommand = new org.eclipse.gef.commands.CompoundCommand("Paste all elements"); //$NON-NLS-1$
-			
-			List<IStrategy> allStrategies = PasteStrategyManager.getInstance()
-					.getAllActiveStrategies();			
-			for (IStrategy iStrategy : allStrategies) {
-				IPasteStrategy iPasteStrategy = (IPasteStrategy) iStrategy;
+
+			List<IStrategy> allStrategies = PasteStrategyManager.getInstance().getAllActiveStrategies();
+			for(IStrategy iStrategy : allStrategies) {
+				IPasteStrategy iPasteStrategy = (IPasteStrategy)iStrategy;
 				Command graphicalCommand = iPasteStrategy.getGraphicalCommand(getEditingDomain(), targetOwnerEditPart, PapyrusClipboard.getInstance());
-				if (graphicalCommand!=null) {
+				if(graphicalCommand != null) {
 					compoundCommand.add(graphicalCommand);
 				}
 			}
-			return compoundCommand;	
-		//	}
+			return compoundCommand;
 		}
 		return UnexecutableCommand.INSTANCE;
 	}
 
-	// TODO : setEnable if selection.size == 1 and Papyrus Clipboard  or other is not empty
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.papyrus.infra.gmfdiag.menu.handlers.AbstractGraphicalCommandHandler#setEnabled(java.lang.Object)
+	 */
+	@Override
+	public void setEnabled(Object evaluationContext) {
+		if(!PapyrusClipboard.getInstance().isEmpty()) {
+			if(evaluationContext instanceof IEvaluationContext) {
+				IEvaluationContext iEvaluationContext = (IEvaluationContext)evaluationContext;
+				Object activeFocusControl = iEvaluationContext.getVariable("activeFocusControl"); //$NON-NLS-1$
+				Object activeShell = iEvaluationContext.getVariable("activeShell"); //$NON-NLS-1$
+				Control focusControl = null;
+				if(activeShell instanceof Shell) {
+					Shell shell = (Shell)activeShell;
+					Display display = shell.getDisplay();
+					if(display != null) {
+						focusControl = display.getFocusControl();
+					}
+				}
+				if(activeFocusControl instanceof StyledText || focusControl instanceof Text) { // true if the focus is on an internal xtext editor or a text edit
+					setBaseEnabled(false);
+				} else {
+					super.setEnabled(evaluationContext);
+				}
+			}
+		} else {
+			setBaseEnabled(false);
+		}
+	}
 }
